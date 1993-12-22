00d2f3d387bee2f9e71b608bf2dc94585b778f85
mehl <mehl@ada56829-ad1f-0410-b00f-83cda6628aec>
1993-12-22 10:39:46 +0000

    oz: menus local to oz-buffers     fun added ozstartup: cleaned up, for demos: take your .emacs with you !!!
    git-svn-id: https://gforge.info.ucl.ac.be/svn/mozart@106 ada56829-ad1f-0410-b00f-83cda6628aec

--- share/elisp/ozstartup.el
+++ share/elisp/ozstartup.el
@@ -1,41 +1,25 @@
-;(setq debug-on-error t)
+;; ozstartup.el
+;; ---------------------------------------------------------
+;; a minimal ozstartup file:
+;; setting oz-home, load-path and auto-mode-alist
+;; starting oz
 
-(defvar
-  lucid-emacs 
-  (string-match "Lucid" emacs-version)
-  "Use Lucid-Emacs functions for highlighting for example")
-
-
-(defun ozhome()
-"Return directory where Oz was installed as a string"
-  (or (getenv "OZHOME") ""))
+(defvar oz-home (or (getenv "OZHOME") ".")
+  "directory where Oz was installed"
+  )
 
 ;;; where lemacs searches for elisp files:
 (setq load-path (append (list "." 
-			      (concat (ozhome) "/lib/elisp"))
+			      (concat oz-home "/lib/elisp"))
 		      load-path))
 
-;;; where lemacs searches for online documentation
-(setq Info-directory-list (list (concat (ozhome) "/lemacs/info")))
 
-;;; where lemacs searches for online documentation too
-(setq exec-directory (concat (ozhome) "/lemacs/etc/"))
+;; if we distribute LEMACS with Oz, you need to add the following
+;; where lemacs searches for online documentation
+;(setq Info-directory-list (list (concat (ozhome) "/lemacs/info")))
 
-
-;; 
-;;  Oz Mode
-;;
-;(defvar oz-input-to-oz t)
-
-(autoload 'oz-mode        "oz" "Mode for Oz" t)
-(autoload 'run-oz         "oz" "Mode for Oz" t)
-(autoload 'oz-previous-buffer "oz" "Mode for Oz" t)
-(autoload 'oz-next-buffer "oz" "Mode for Oz" t)
-(autoload 'oz-new-buffer  "oz" "Mode for Oz" t)
-
-(global-set-key "\M-n"   'oz-next-buffer)
-(global-set-key "\M-p"   'oz-previous-buffer)
-(global-set-key "\M-o"   'oz-new-buffer)
+;; where lemacs searches for online documentation too
+;(setq exec-directory (concat (ozhome) "/lemacs/etc/"))
 
 
 ;; automatically switch into Oz-Mode when loading
@@ -44,85 +28,5 @@
 			    auto-mode-alist))
 
 
-(put 'eval-expression 'disabled nil)
-
-
-(defun button3-popup()
-  (interactive)
-  (popup-menu button3-menubar))
-
-(defun buffer-menu-new-window()
-  (interactive)
-  (mybuffer-menu 'switch-to-buffer))
-
-(defun buffer-menu-new-screen()
-  (interactive)
-  (mybuffer-menu 'switch-to-buffer-new-screen))
-
-(defun mybuffer-menu (switcher)
-  (let (name
-	buffers)
-    ;; sort the buffers alphabetically
-    (setq buffers (sort (buffer-list)
-			(function (lambda (x y) (string< (buffer-name x) (buffer-name y))))))
-    (setq buffers
-	  (mapcar (function
-		   (lambda (buffer)
-		     (if (setq name (format-buffers-menu-line buffer))
-			 (vector name
-				 (list switcher (buffer-name buffer))
-				 t))))
-		  buffers))
-    (popup-menu  (cons "" (delq nil buffers)))))
-
-
-(defun line-to-top()
-  (interactive)
-  (recenter 0))
-
-(if lucid-emacs
-    (progn 
-      (global-set-key [(button3)]               'button3-popup)
-      (global-set-key [(control button3)]       'buffer-menu-new-window)
-      (global-set-key [(control shift button3)]       'buffer-menu-new-screen)
-      (global-set-key [(button1)]               'mouse-track)
-      (global-set-key [(f2)]                    'line-to-top)
-      (global-set-key [(f3)]                    'goto-line-selected)
-      (global-set-key [(control c) (g)]         'goto-line)))
-
-(defun goto-line-selected()
-"Goto line selected via X11, or prompt for line-number"
-  (interactive)
-  (let ((line (string-to-int 
-	       (or (condition-case () (x-get-selection) (error ()))
-		   "0"))))
-    (if (= line 0)
-        (call-interactively 'goto-line)
-	(goto-line line))))
-
-(setq server-visit-hook 'find-buffer-other-window)
-
-(defun find-buffer-other-window ()
-  (interactive)
-  (let ((cur (current-buffer)))
-    (x-new-screen)
-    (switch-to-buffer cur)))
-
-
-(defun switch-to-buffer-new-screen (buffer)
-  (interactive "BSwitch to buffer in other screen: ")
-  (let ((screens (screen-list))
-	(screen)
-	(win))
-    (setq win (get-buffer-window buffer t) )
-    (if win
-	(progn
-	  (select-window win)
-	  (make-screen-visible (window-screen win))
-	  (raise-screen  (window-screen win)))
-	(progn
-	  (x-new-screen)
-	  (switch-to-buffer buffer)))))
-
-
+(load "oz")
 (run-oz)
