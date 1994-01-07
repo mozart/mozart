9db6029f3b021c3505cf30c00f4358b5c9212993
mehl <mehl@ada56829-ad1f-0410-b00f-83cda6628aec>
1994-01-07 13:08:19 +0000

    bug fixes

--- elisp/ozstartup.el
+++ elisp/ozstartup.el
@@ -4,22 +4,23 @@
 ;; setting oz-home, load-path and auto-mode-alist
 ;; starting oz
 
-(defvar oz-home (or (getenv "OZHOME") ".")
+(defun oz-home()
   "directory where Oz was installed"
+  (or (getenv "OZHOME") ".")
   )
 
 ;;; where lemacs searches for elisp files:
 (setq load-path (append (list "." 
-			      (concat oz-home "/lib/elisp"))
+			      (concat (oz-home) "/lib/elisp"))
 		      load-path))
 
 
 ;; if we distribute LEMACS with Oz, you need to add the following
 ;; where lemacs searches for online documentation
-;(setq Info-directory-list (list (concat (ozhome) "/lemacs/info")))
+;(setq Info-directory-list (list (concat (oz-home) "/lemacs/info")))
 
 ;; where lemacs searches for online documentation too
-;(setq exec-directory (concat (ozhome) "/lemacs/etc/"))
+;(setq exec-directory (concat (oz-home) "/lemacs/etc/"))
 
 
 ;; automatically switch into Oz-Mode when loading
@@ -29,4 +30,4 @@
 
 
 (load "oz")
-(run-oz)
+(oz-start)
