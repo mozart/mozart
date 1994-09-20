328ad117feffa92d0885067e8f18285e4ff33b15
tmueller <tmueller@ada56829-ad1f-0410-b00f-83cda6628aec>
1994-09-20 17:28:13 +0000

    moved code to icc-file, added MPRINT

--- platform/emulator/genvar.cc
+++ platform/emulator/genvar.cc
@@ -34,18 +34,6 @@ Bool GenCVariable::isLocalVariable(void){
 }
 
 
-void GenCVariable::bind(TaggedRef * vPtr, TaggedRef var,
-			TaggedRef * tPtr, TaggedRef term){
-  if (tagged2CVar(var)->isLocalVariable() == NO)
-    am.trail.pushRef(vPtr, var);
-
-  if (isCVar(term) == OK)
-    term = makeTaggedRef(tPtr);
-  
-  *vPtr = term;
-} // GenCVariable::bind
-
-
 void GenCVariable::bind(TaggedRef * vPtr, TaggedRef var, Bool varIsLocal,
                         TaggedRef * tPtr, TaggedRef term)
 {
@@ -57,6 +45,15 @@ void GenCVariable::bind(TaggedRef * vPtr, TaggedRef var, Bool varIsLocal,
   
   *vPtr = term;
 } 
+
+
+void GenCVariable::bind(TaggedRef * vPtr, TaggedRef var,
+			TaggedRef * tPtr, TaggedRef term)
+{
+  bind(vPtr, var,
+       tagged2CVar(var)->isLocalVariable(),
+       tPtr, term);
+}
 
 
 void GenCVariable::propagate(TaggedRef var, TaggedRef term){
@@ -73,51 +70,6 @@ SuspList * GenCVariable::propagate(TaggedRef var, SuspList * &sl,
 				   TaggedRef term)
 {
   sl = am.checkSuspensionList(tagged2SuspVar(var), var, sl, term);
-}
-
-
-Bool GenCVariable::unify(TaggedRef * tptr1, TaggedRef term1,
-			 TypeOfTerm ttag1,
-			 TaggedRef* tptr2, TaggedRef term2,
-			 TypeOfTerm ttag2)
-{
-  switch (type){
-  case FDVariable:
-    return ((GenFDVariable *)this)->unifyFD(tptr1, term1, ttag1,
-					    tptr2, term2, ttag2);
-  default:
-    error("Unexpected type generic variable at %s:%d.",
-	  __FILE__, __LINE__);
-    break;
-  }
-  return NO;
-} 
-
-
-size_t GenCVariable::getSize(void){
-  switch (type){
-  case FDVariable:
-    return ((GenFDVariable*)this)->getSize();
-  default:
-    error("Unexpected type generic variable at %s:%d.",
-	  __FILE__, __LINE__);
-    break;
-  }
-  return 0;
-} 
-
-
-ProgramCounter GenCVariable::index(ProgramCounter elseLabel, 
-				   IHashTable * table)
-{
-  switch (type){
-  case FDVariable:
-    return ((GenFDVariable*)this)->index(elseLabel, table);
-  default:
-    error("Unexpected type generic variable at %s:%d.",
-	  __FILE__, __LINE__);
-    return NOCODE;
-  }  
 }
 
 
