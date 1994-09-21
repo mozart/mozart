6a90e59708d15f40ff6a9181dce51e52374e64ad
tmueller <tmueller@ada56829-ad1f-0410-b00f-83cda6628aec>
1994-09-21 09:41:26 +0000

    gcc -O works now

--- platform/emulator/genvar.hh
+++ platform/emulator/genvar.hh
@@ -24,7 +24,6 @@
 #endif
 
 #include "term.hh"
-#include "am.hh"
 
 //-----------------------------------------------------------------------------
 //                       Generic Constrained Variable
