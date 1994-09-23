5aa625a1fff95461070c58cd5eb025a92605b4d4
tmueller <tmueller@ada56829-ad1f-0410-b00f-83cda6628aec>
1994-09-23 13:19:43 +0000

    made fd-stuff compiling properly wrt added warnings

--- platform/emulator/var_fd.hh
+++ platform/emulator/var_fd.hh
@@ -63,7 +63,7 @@ public:
   }
       
   void addVirtualConstr(SuspList * elem, FDState state) {
-    DebugCheck((state < det || state > any), error("Unexpected state.")); 
+    DebugCheck(state > any, error("Unexpected state.")); 
     if (state == any)
       addVirtualConstr(elem);
     else
@@ -71,7 +71,7 @@ public:
   }
 
   void addVirtualConstrLocal(SuspList * elem, FDState state) {
-    DebugCheck((state < det || state > any), error("Unexpected state.")); 
+    DebugCheck(state > any, error("Unexpected state.")); 
     if (isLocalVariable() == OK || state == any || state == eqvar)
       addVirtualConstr(elem, state);
     else
