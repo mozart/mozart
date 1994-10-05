6e285711b2ed684efa5769171b26a58e647f336c
tmueller <tmueller@ada56829-ad1f-0410-b00f-83cda6628aec>
1994-10-05 17:59:03 +0000

    added consistency check to prove properness of pointers

--- platform/emulator/tagged.hh
+++ platform/emulator/tagged.hh
@@ -144,6 +144,7 @@ void taggedPrintLong(TaggedRef ref, int depth = 10, int offset = 0);
 #define CHECK_ISVAR(term)  Assert(isAnyVar(term))
 #define CHECK_DEREF(term)  Assert(!isRef(term) && !isAnyVar(term))
 #define CHECK_POINTER(s)   Assert(s != NULL && !((int) s & 3) )
+#define CHECK_POINTERLSB(s)   Assert(!((int) s & 3) )
 #define CHECK_STRPTR(s)    Assert(s != NULL)
 #define CHECKTAG(Tag)      Assert(tagTypeOf(ref) == Tag)
 
