256b1a74a95c1c4d297a82eee65d808b9132f3f1
tmueller <tmueller@ada56829-ad1f-0410-b00f-83cda6628aec>
1994-11-24 20:58:56 +0000

    added function mkTuple
    git-svn-id: https://gforge.info.ucl.ac.be/svn/mozart@519 ada56829-ad1f-0410-b00f-83cda6628aec

--- platform/emulator/tagged.hh
+++ platform/emulator/tagged.hh
@@ -917,4 +917,13 @@ RefsArray resize(RefsArray r, int s){
 } // resize
 
 
+
+inline
+TaggedRef mkTuple(int from, int to){
+  OZ_Term s = OZ_tuple(OZ_CToAtom("#"), 2);
+  OZ_putArg(s, 1, OZ_CToInt(from));
+  OZ_putArg(s, 2, OZ_CToInt(to));
+  return s;
+}
+
 #endif
