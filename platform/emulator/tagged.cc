1250800647aaa3b97e8bb808c5e5213b3da47201
tmueller <tmueller@ada56829-ad1f-0410-b00f-83cda6628aec>
1994-12-07 21:01:10 +0000

    added TypeOfTermString, giving you a string wrt TypeOfTerm

--- platform/emulator/tagged.cc
+++ platform/emulator/tagged.cc
@@ -13,3 +13,22 @@
 #endif
 
 #include "tagged.hh"
+
+char * TypeOfTermString[16] = {
+  "REF", // 0
+  "UVAR", // 1
+  "LTUPLE", // 2
+  "SMALLINT", //3
+  "REF", // 4
+  "CVAR", // 5
+  "STUPLE", // 6
+  "BIGINT", // 7
+  "REF", // 8
+  "SVAR", // 9
+  "CONST", // 10
+  "FLOAT", // 11
+  "REF", // 12
+  "UNUSEDVARIABLE", // 13
+  "SRECORD", // 14
+  "ATOM", // 15
+};
