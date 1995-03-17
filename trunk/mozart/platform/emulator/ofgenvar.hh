6dbe85fe8431678494219a77e26afa3eb641edb4
vanroy <vanroy@ada56829-ad1f-0410-b00f-83cda6628aec>
1995-03-17 11:23:31 +0000

    Debugged and extended open feature structures. Now they can have any label.
    git-svn-id: https://gforge.info.ucl.ac.be/svn/mozart@766 ada56829-ad1f-0410-b00f-83cda6628aec

--- trunk/mozart/platform/emulator/ofgenvar.hh
+++ trunk/mozart/platform/emulator/ofgenvar.hh
@@ -5,6 +5,7 @@
 #pragma interface
 #endif
 
+#include "am.hh"
 #include "genvar.hh"
 #include "tagged.hh"
 #include "term.hh"
@@ -100,7 +101,7 @@ public:
 //                               class DynamicTable
 //-------------------------------------------------------------------------
 
-// A class implementing a hash table that is automatically expanded when
+// A class implementing a hash table that can be expanded when
 // it becomes too full.  Several operations (merge, srecordcheck) are
 // added to facilitate unification of GenOFSVariables, which are built on
 // top of a DynamicTable.
@@ -122,48 +123,77 @@ private:
 //   int Literal::hash()					(term.hh)
 //   Literal* tagged2Literal(TaggedRef ref)			(tagged.hh)
 //   Bool isLiteral(TaggedRef term)				(tagged.hh)
-//   void* freeListMalloc(size_t chunk_size)			(mem.hh)
-//   void freeListDispose(void* addr, size_t chunk_size)	(mem.hh)
-//   void *gcRealloc(void *ptr, size_t sz)			(gc.cc)
+//   void* heapMalloc(size_t chunk_size)	 		(mem.hh)
+//   void* gcRealloc(void* ptr, size_t sz)			(gc.cc)
 //   void gcTagged(TaggedRef &fromTerm, TaggedRef &toTerm)	(gc.cc)
 
 class DynamicTable {
 friend class HashElement;
 
+public:
+    dt_index numelem;
+    dt_index size;
+    HashElement table[1]; // 1 == size of initial table
+
+    USEHEAPMEMORY;
+
+    void print(ostream & = cout, int=0, int=0);
+    void printLong(ostream & = cout, int=0, int=0);
+
+    // Copy the dynamictable from 'from' to 'to' space:
+    DynamicTable* gc(void); // See definition in gc.cc
+    void gcRecurse(void);
+
+    DynamicTable() { error("do not use DynamicTable"); }
+
+    // Create an initially empty dynamictable of size s (default 1)
+    static DynamicTable* newDynamicTable(dt_index s=1);
+
+    // Initialize an elsewhere-allocated dynamictable of size s
+    void init(dt_index s=1);
+
+    // Create a copy of an existing dynamictable
+    DynamicTable* copyDynamicTable();
+
+    // Test whether the current table has too little room for one new element:
+    // ATTENTION: Calls to insert should be preceded by fullTest.
+    Bool fullTest();
+
+    // Return a table that is double the size of the current table and
+    // that contains the same elements:
+    // ATTENTION: Should be called before insert if the table is full.
+    DynamicTable* doubleDynamicTable();
+
+    // Insert val at index id 
+    // Return NULL if val is successfully inserted (id did not exist) 
+    // Return the value of the pre-existing element if id already exists
+    // Test for and increase size of hash table if it becomes too full
+    // ATTENTION: insert must only be done if the table has room for a new element.
+    TaggedRef insert(TaggedRef id, TaggedRef val);
+
+    // Look up val at index id
+    // Return val if it is found
+    // Return NULL if nothing is found
+    TaggedRef lookup(TaggedRef id);
+
+    // Return TRUE iff there are features in an external dynamictable that
+    // are not in the current dynamictable
+    Bool extraFeaturesIn(DynamicTable* dt);
+
+    // Merge the current dynamictable into an external dynamictable
+    // Return a pairlist containing all term pairs with the same feature
+    // The external dynamictable is resized if necessary
+    void merge(DynamicTable* &dt, PairList* &pairs);
+
+    // Check an srecord against the current dynamictable
+    // Return TRUE if all elements of dynamictable exist in srecord.
+    // Return FALSE if there exists element of dynamictable that is not in srecord.
+    // If TRUE, collect pairs of corresponding elements of dynamictable and srecord.
+    // If FALSE, pair list contains a well-terminated but meaningless list.
+    // Neither the srecord nor the dynamictable is modified.
+    Bool srecordcheck(SRecord &sr, PairList* &pairs);
 
 private:
-    /**** Utilities ****/
-  /* should stay here, so they can be inlined */
-
-    // Return true iff argument is a power of two:
-    Bool isPwrTwo(dt_index s)
-    {
-        Assert(s>0);
-        while ((s&1)==0) s=(s>>1);
-        return (s==1);
-    }
-
-    // Test for and double size of internal hash table if necessary
-    void overflowTest() {
-        Assert(isPwrTwo(size));
-        if (numelem>=(size>>1)) { // numelem>=(size>>1)+(size>>2)
-	    Assert(isPwrTwo(size<<1));
-            DynamicTable* dt=new DynamicTable(size<<1);
-            for(dt_index i=0; i<size; i++) {
-                if (table[i].ident!=makeTaggedNULL()) {
-		    Assert(isLiteral(table[i].ident));
-                    dt->insert(table[i].ident, table[i].value);
-                }
-            }
-            // Make current table a reference to dt's table:
-            freeListDispose(table, size*sizeof(HashElement));
-            numelem=dt->numelem;
-            size=dt->size;
-	    Assert(numelem<size);
-            table=dt->table;
-	    Assert(table!=NULL);
-        }
-    }
 
     // Hash and rehash until the element or an empty slot is found
     // Returns index of slot; the slot is empty or contains the element
@@ -182,152 +212,12 @@ private:
         return i;
     }
 
-
-
-public:
-    dt_index numelem;
-    dt_index size;
-    HashElement *table;
-
-    USEHEAPMEMORY;
-
-    void print(ostream & = cout, int = 0) const;
-    void printLong(ostream & = cout, int = 0) const;
-
-    // Copy the dynamictable from 'from' to 'to' space:
-    void gc(void); // Definition in gc.cc
-
-    // Create an initially empty dynamictable of size s (default 1) 
-    // (a future optimization: change the default to some more reasonable value)
-    DynamicTable(dt_index s=1) {
-        Assert(isPwrTwo(s));
-	this->init(s);
-    }
-
-    // Initialize an elsewhere-allocated dynamictable
-    void init(dt_index s=1) {
-        Assert(isPwrTwo(s));
-	numelem=0;
-        size=s;
-        table=(HashElement *)freeListMalloc(s*sizeof(HashElement));
-        for (dt_index i=0; i<s; i++) table[i].ident=makeTaggedNULL();
-    }
-
-    // Create a copy of an existing dynamictable
-    DynamicTable(DynamicTable &dt) {
-        numelem=dt.numelem;
-        size=dt.size;
-        Assert(isPwrTwo(size));
-	Assert(numelem<size);
-	Assert(size>0);
-        table=(HashElement *)freeListMalloc(size*sizeof(HashElement));
-	Assert(table!=NULL);
-        for (dt_index i=0; i<size; i++) table[i]=dt.table[i];
-    }
-
-    // Insert val at index id 
-    // Return NULL if val is successfully inserted (id did not exist) 
-    // Return the value of the pre-existing element if id already exists
-    // Test for and increase size of hash table if it becomes too full
-    TaggedRef insert(TaggedRef id, TaggedRef val) {
-        Assert(isPwrTwo(size));
-        Assert(isLiteral(id));
-        overflowTest();
-        dt_index i=fullhash(id);
-	Assert(i<size);
-        if (table[i].ident!=makeTaggedNULL()) {
-	    Assert(isLiteral(table[i].ident));
-            // Ident exists already; return value & don't insert
-            return table[i].value;
-        } else {
-            // Ident doesn't exist; insert value
-            numelem++;
-	    Assert(numelem<size);
-            table[i].ident=id;
-            table[i].value=val;
-            return makeTaggedNULL();
-        }
-    }
-
-    // Look up val at index id
-    // Return val if it is found
-    // Return NULL if nothing is found
-    TaggedRef lookup(TaggedRef id) {
-        Assert(isPwrTwo(size));
-        Assert(isLiteral(id));
-        dt_index i=fullhash(id);
-	Assert(i<size);
-        if (table[i].ident==id) {
-	    Assert(isLiteral(table[i].ident));
-            // Val is found
-            return table[i].value;
-        } else {
-            // Val is not found
-            return makeTaggedNULL();
-        }
-    }
-
-    // Return TRUE iff there are features in an external dynamictable that
-    // are not in the current dynamictable
-    Bool extraFeaturesIn(DynamicTable dt) {
-        Assert(isPwrTwo(size));
-        for (dt_index i=0; i<dt.size; i++) {
-	    if (dt.table[i].ident!=makeTaggedNULL()) {
-		Assert(isLiteral(dt.table[i].ident));
-		Bool exists=lookup(dt.table[i].ident);
-		if (!exists) return TRUE;
-	    }
-	}
-	return FALSE;
-    }
-
-    // Merge an external dynamictable into the current dynamictable
-    // Return a pairlist containing all term pairs with the same feature
-    void merge(DynamicTable &dt, PairList* &pairs) {
-        Assert(isPwrTwo(size));
-        pairs=new PairList();
-	Assert(pairs->isempty());
-        for (dt_index i=0; i<dt.size; i++) {
-            if (dt.table[i].ident!=makeTaggedNULL()) {
-		Assert(isLiteral(dt.table[i].ident));
-        	TaggedRef val=insert(dt.table[i].ident, dt.table[i].value);
-        	if  (val!=makeTaggedNULL()) {
-                    // Two terms have this feature; don't insert
-                    // Add the terms to the list of pairs:
-                    pairs->addpair(val, dt.table[i].value);
-		    Assert(!pairs->isempty());
-        	} else {
-                    // Element successfully inserted
-        	}
-            }
-        }
-    }
-
-    // Check an srecord against the current dynamictable
-    // Return TRUE if all elements of dynamictable exist in srecord.
-    // Return FALSE if there exists element of dynamictable that is not in srecord.
-    // If TRUE, collect pairs of corresponding elements of dynamictable and srecord.
-    // If FALSE, pair list contains a well-terminated but meaningless list.
-    // Neither the srecord nor the dynamictable is modified.
-    Bool srecordcheck(SRecord &sr, PairList* &pairs) {
-        Assert(isPwrTwo(size));
-	pairs=new PairList();
-	Assert(pairs->isempty());
-	for (dt_index i=0; i<size; i++) {
-	    if (table[i].ident!=makeTaggedNULL()) {
-		Assert(isLiteral(table[i].ident));
-		TaggedRef val=sr.getFeature(table[i].ident);
-		if (val!=makeTaggedNULL()) {
-		    // Feature found in srecord; add corresponding terms to list of pairs:
-		    pairs->addpair(val, table[i].value);
-		    Assert (!pairs->isempty());
-		} else {
-		    // Feature not found in srecord; failure of unification
-		    return FALSE;
-		}
-	    }
-	}
-	return TRUE;
+    // Return true iff argument is a power of two:
+    static Bool isPwrTwo(dt_index s)
+    {
+        Assert(s>0);
+        while ((s&1)==0) s=(s>>1);
+        return (s==1);
     }
 };
 
@@ -340,30 +230,32 @@ public:
 class GenOFSVariable: public GenCVariable {
 
 private:
-    // Attention: This is the _record itself_, not a pointer to it:
-    DynamicTable dynamictable;
+    TaggedRef label;
+    DynamicTable* dynamictable;
 
 public:
     GenOFSVariable(DynamicTable &dt, TaggedRef pn = AtomVoid)
     : GenCVariable(OFSVariable, pn) {
-        dynamictable=dt;
+        label=makeTaggedRef(newTaggedUVar(am.currentBoard));
+        dynamictable= &dt;
     }
 
     GenOFSVariable(TaggedRef pn = AtomVoid)
     : GenCVariable(OFSVariable, pn) {
-        dynamictable.init();
+        label=makeTaggedRef(newTaggedUVar(am.currentBoard));
+        dynamictable=DynamicTable::newDynamicTable();
     }
 
     // Methods relevant for term copying (gc and solve)
     void gc(void);
     size_t getSize(void) { return sizeof(GenOFSVariable); }
 
-    Literal* getLabel(void) {
-	return tagged2Literal(AtomOpen);
+    TaggedRef getLabel(void) {
+	return label;
     }
 
     DynamicTable* getTable(void) {
-	return &dynamictable;
+	return dynamictable;
     }
 
     Bool unifyOFS(TaggedRef *, TaggedRef, TaggedRef *, TaggedRef, Bool);
@@ -371,15 +263,17 @@ public:
     // Return the feature value if feature exists, return NULL if it doesn't exist
     TaggedRef getFeatureValue(TaggedRef feature) {
 	Assert(isLiteral(feature));
-        return dynamictable.lookup(feature);
+        return dynamictable->lookup(feature);
     }
 
     // Add the feature and its value
     // If the feature already exists, do not insert anything
     // Return TRUE if feature successfully inserted, FALSE otherwise
+    // ATTENTION: only use this for terms that do not have to be trailed
     Bool addFeatureValue(TaggedRef feature, TaggedRef term) {
 	Assert(isLiteral(feature));
-        TaggedRef prev=dynamictable.insert(feature,term);
+        if (dynamictable->fullTest()) dynamictable=dynamictable->doubleDynamicTable();
+        TaggedRef prev=dynamictable->insert(feature,term);
         // (a future optimization: a second suspList only waiting on features)
         if (prev==makeTaggedNULL()) {
             // propagate(makeTaggedCVar(this), suspList, makeTaggedCVar(this), pc_propagator);
@@ -400,7 +294,7 @@ public:
     }
 
     int getNumOfFeatures(void) {
-        return (int) dynamictable.numelem;
+        return (int) dynamictable->numelem;
     }
 
     // These procedures exist as well in the class GenFDVariable,
