81859bc12712c566fa37dd33c6dad6e8d5df8736
scheidhr <scheidhr@ada56829-ad1f-0410-b00f-83cda6628aec>
1995-04-27 10:03:59 +0000

    alpha port

---
+++ platform/emulator/machine.hh
@@ -0,0 +1,58 @@
+/*
+  Hydra Project, DFKI Saarbruecken,
+  Stuhlsatzenhausweg 3, D-66123 Saarbruecken, Phone (+49) 681 302-5312
+  Author: scheidhr
+  Last modified: $Date$ from $Author$
+  Version: $Revision$
+  State: $State$
+
+  ------------------------------------------------------------------------
+*/
+
+
+#ifndef __MACHINEHH
+#define __MACHINEHH
+
+
+/*
+ * This file contains machine specific settings
+ */
+
+
+/* a 32 bit integer, the same on alpha and other machines */
+#define int32 int
+
+
+#ifdef MIPS
+const int mallocBase = 0x10000000;
+#else
+#ifdef AIX3_RS6000
+const int mallocBase = 0x20000000;
+#else
+#ifdef HPUX_700
+const int mallocBase = 0x40000000;
+#else
+#ifdef OSF1_ALPHA
+const long int mallocBase = 0x140000000;
+#else
+const int mallocBase = 0x0;
+#endif
+#endif
+#endif
+#endif
+
+
+/* convert an int32 to a pointer and vice versa */
+inline void* ToPointer(int32 i) { 
+#ifdef OSF1_ALPHA
+  return (void*) (mallocBase|i);
+#else
+  return (void*) i;
+#endif
+}
+
+inline int32 ToInt32(void *p)   { return (int32)(p); } ;
+
+
+#endif
+
