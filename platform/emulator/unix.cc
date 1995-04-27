c90d1b02140f64fd438f4c9db82785e85463f58c
scheidhr <scheidhr@ada56829-ad1f-0410-b00f-83cda6628aec>
1995-04-27 09:43:09 +0000

    use 	#ifdef LINUX
    on both m68k and i486
    
    
    git-svn-id: https://gforge.info.ucl.ac.be/svn/mozart@805 ada56829-ad1f-0410-b00f-83cda6628aec

--- platform/emulator/unix.cc
+++ platform/emulator/unix.cc
@@ -18,7 +18,7 @@
 #include <unistd.h>
 #include <netdb.h>
 
-#if defined(LINUX_I486) || defined(HPUX_700)
+#if defined(LINUX) || defined(HPUX_700)
 extern int h_errno;
 #endif
 
@@ -128,7 +128,7 @@ while ((RET = CALL) < 0) {                          \
 { RETURN_ANY_ERROR(OUT,errno,OZ_unixError(errno),"unix"); }
 
 
-#if defined(SUNOS_SPARC) || defined(SOLARIS_SPARC) || defined(LINUX_I486) || defined(HPUX_700) || defined(IRIX5_MIPS) || defined(AIX3_RS6000) || defined(OSF1_ALPHA)
+#if defined(SUNOS_SPARC) || defined(SOLARIS_SPARC) || defined(LINUX) || defined(HPUX_700) || defined(IRIX5_MIPS) || defined(AIX3_RS6000) || defined(OSF1_ALPHA)
 
 static char* h_strerror(const int err) {
   switch (err) {
@@ -138,7 +138,7 @@ static char* h_strerror(const int err) {
     return "Retry later again.";
   case NO_RECOVERY:
     return "Unexpected non-recoverable server failure.";
-#if defined(SOLARIS_SPARC) || defined(LINUX_I486)
+#if defined(SOLARIS_SPARC) || defined(LINUX)
   case NO_ADDRESS:
 #endif
 #if defined(SUNOS_SPARC)
