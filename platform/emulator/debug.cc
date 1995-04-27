090dda3c2fc98672c40726fa3ec80b5d2eb632d7
scheidhr <scheidhr@ada56829-ad1f-0410-b00f-83cda6628aec>
1995-04-27 09:47:49 +0000

    more 64bit clean
    git-svn-id: https://gforge.info.ucl.ac.be/svn/mozart@808 ada56829-ad1f-0410-b00f-83cda6628aec

--- platform/emulator/debug.cc
+++ platform/emulator/debug.cc
@@ -541,7 +541,7 @@ Bool trace(char *s,Board *board,Actor *actor,
 	sscanf(&command[1],"%x %d",&from,&size);
 	printf("%x:",from);
 	for (int i = 0; i < 20; i++)
-	  printf(" %d",CodeArea::getShort(from+i));
+	  printf(" %d",CodeArea::getWord(from+i));
 	printf("\n%x:\n",from+20);
       }
       break;
