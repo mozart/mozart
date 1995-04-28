0c89234dbb143303621759020f15539330f8b20b
tmueller <tmueller@ada56829-ad1f-0410-b00f-83cda6628aec>
1995-04-28 14:18:34 +0000

    fixed bug in union of domains

--- platform/emulator/fdomn.cc
+++ platform/emulator/fdomn.cc
@@ -1270,7 +1270,11 @@ FiniteDomain &FiniteDomain::operator | (const FiniteDomain &y) const
 
   FiniteDomain &z = return_var; z.setEmpty();
 
-  if (max(max_elem, y.max_elem) > fd_bv_max_elem) {
+  if (*this == fd_empty) {
+    z = y;
+  } else if (y == fd_empty) {
+    z = *this;
+  } else if (max(max_elem, y.max_elem) > fd_bv_max_elem) {
     FDIntervals * x_v = asIntervals();
     FDIntervals * y_v = y.asIntervals();
     FDIntervals * z_v;
