fb1b680c48375f6d11e525154eb3388c277c6c7a
scheidhr <scheidhr@ada56829-ad1f-0410-b00f-83cda6628aec>
1995-05-02 07:57:51 +0000

    link with -n on AXP, see comment in machine.hh

--- platform/emulator/machine.hh
+++ platform/emulator/machine.hh
@@ -24,6 +24,22 @@
 #define intlong long
 
 
+/*
+ * Note for DEC Alpha:
+ * by default the text segment on the alpha starts at adress 
+ * 0x120000000 and the data segment at 0x140000000 (i.e. a bit in the upper
+ * 4 bytes of addresses is set).
+ * This makes converting an int32 to a void* and converting an
+ * int32 to a program adress (used in threaded code) more expensive.
+ * So we can either use the -T and -D option of the linker or create an
+ * NMAGIC file using the -n link option, which lets start segments at
+ * 0x20000000 and 0x40000000. We choose to use the -n option.
+ * See also man page of ld(1)
+ */
+
+
+
+
 #ifdef MIPS
 const intlong mallocBase = 0x10000000;
 #else
@@ -34,7 +50,7 @@ const intlong mallocBase = 0x20000000;
 const intlong mallocBase = 0x40000000;
 #else
 #ifdef OSF1_ALPHA
-const intlong mallocBase = 0x140000000;
+const intlong mallocBase = 0x40000000;
 #else
 const intlong mallocBase = 0x0;
 #endif
@@ -47,19 +63,11 @@ const intlong mallocBase = 0x0;
  * needed for threaded code
  */
 
-#ifdef OSF1_ALPHA
-const intlong textBase = 0x100000000;
-#else
 const intlong textBase = 0x0;
-#endif
 
 /* convert an int32 to a pointer and vice versa */
 inline void* ToPointer(int32 i) { 
-#ifdef OSF1_ALPHA
-  return (void*) (mallocBase|i);
-#else
   return (void*) i;
-#endif
 }
 
 inline int32 ToInt32(void *p) { return (int32)(p); }
