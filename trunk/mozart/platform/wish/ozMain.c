c0394d8a5cb38eb2cc9604a21d0b51ac1f51591b
mehl <mehl@ada56829-ad1f-0410-b00f-83cda6628aec>
1995-07-12 12:16:00 +0000

    initialize BLT removed rc_init...
    git-svn-id: https://gforge.info.ucl.ac.be/svn/mozart@940 ada56829-ad1f-0410-b00f-83cda6628aec

--- trunk/mozart/platform/wish/ozMain.c
+++ trunk/mozart/platform/wish/ozMain.c
@@ -57,13 +57,6 @@ static Tk_Window mainWindow;	/* The main window for the application.  If
 				 * NULL then the application no longer
 				 * exists. */
 static Tcl_Interp *interp;	/* Interpreter for this application. */
-#ifndef Tk4
-char *tcl_RcFileName = NULL;	/* Name of a user-specific startup script
-				 * to source if the application is being run
-				 * interactively (e.g. "~/.wishrc").  Set
-				 * by Tcl_AppInit.  NULL means don't source
-				 * anything ever. */
-#endif
 static Tcl_DString command;	/* Used to assemble lines of terminal input
 				 * into Tcl commands. */
 static int tty;			/* Non-zero means standard input is a
@@ -257,28 +250,6 @@ main(argc, argv)
 	 * device is a terminal.
 	 */
 
-	if (tcl_RcFileName != NULL) {
-	    Tcl_DString buffer;
-	    char *fullName;
-	    FILE *f;
-    
-	    fullName = Tcl_TildeSubst(interp, tcl_RcFileName, &buffer);
-	    if (fullName == NULL) {
-		fprintf(stdout, "w %s\n.\n", interp->result);
-		fflush(stdout); /* added mm */
-	    } else {
-		f = fopen(fullName, "r");
-		if (f != NULL) {
-		    code = Tcl_EvalFile(interp, fullName);
-		    if (code != TCL_OK) {
-			fprintf(stdout, "w %s\n.\n", interp->result);
-			fflush(stdout); /* added mm */
-		    }
-		    fclose(f);
-		}
-	    }
-	    Tcl_DStringFree(&buffer);
-	}
 	Tk_CreateFileHandler(0, TK_READABLE, StdinProc, (ClientData) 0);
 	if (tty) {
 	    Prompt(interp, 0);
