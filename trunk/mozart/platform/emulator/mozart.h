c683ed72d109b362bed75f92807bc4fc972f8c33
tmueller <tmueller@ada56829-ad1f-0410-b00f-83cda6628aec>
1995-11-22 10:38:25 +0000

    add OZ_getM{in,ax}Int to foreign function interface
    git-svn-id: https://gforge.info.ucl.ac.be/svn/mozart@1257 ada56829-ad1f-0410-b00f-83cda6628aec

--- trunk/mozart/platform/emulator/mozart.h
+++ trunk/mozart/platform/emulator/mozart.h
@@ -75,6 +75,8 @@ extern char *   OZ_atomToC    _PROTOTYPE((OZ_Term));
 extern char *   OZ_literalToC _PROTOTYPE((OZ_Term));
 extern OZ_Term  OZ_CToAtom    _PROTOTYPE((char *));
 
+extern int      OZ_getMinInt     _PROTOTYPE((void));
+extern int      OZ_getMaxInt     _PROTOTYPE((void));
 extern OZ_Term  OZ_CToInt        _PROTOTYPE((int));
 extern int      OZ_intToC        _PROTOTYPE((OZ_Term));
 extern OZ_Term  OZ_CStringToInt  _PROTOTYPE((char *str));
