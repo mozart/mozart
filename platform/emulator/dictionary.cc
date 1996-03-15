d827338ddf7663b1b4da336f3797e7822fb2acee
scheidhr <scheidhr@ada56829-ad1f-0410-b00f-83cda6628aec>
1996-03-15 10:20:38 +0000

    added dictionaries

---
+++ platform/emulator/dictionary.cc
@@ -0,0 +1,15 @@
+/*
+ * FBPS Saarbr"ucken
+ * Author: mehl
+ * Last modified: $Date$ from $Author$
+ * Version: $Revision$
+ * State: $State$
+ */
+
+#if defined(INTERFACE)
+#pragma implementation "dictionary.hh"
+#endif
+
+#include "am.hh"
+#include "ofgenvar.hh"
+#include "dictionary.hh"
