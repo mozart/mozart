59f06fbecb774e724b343e6d8c88e7b96e4598d3
tmueller <tmueller@ada56829-ad1f-0410-b00f-83cda6628aec>
1996-07-31 10:01:27 +0000

    added set variables

---
+++ platform/emulator/fset.hh
@@ -0,0 +1,76 @@
+/*
+  Hydra Project, DFKI Saarbruecken,
+  Stuhlsatzenhausweg 3, D-66123 Saarbruecken, Phone (+49) 681 302-5312
+  Author: tmueller
+  Last modified: $Date$ from $Author$
+  Version: $Revision$
+  State: $State$
+
+  ------------------------------------------------------------------------
+*/
+
+#ifndef __FSET_HH__
+#define __FSET_HH__
+
+#if defined(INTERFACE)
+#pragma interface
+#endif
+
+#include <iostream.h>
+
+#include "oz.h"
+
+const int fset_high = 2;
+
+class FSetValue {
+friend class OZ_FSetImpl;
+private:
+  int _card;
+  int _in[fset_high];
+public:
+  FSetValue(void) {}
+  FSetValue(OZ_FSetImpl &);
+  FSetValue(OZ_Term);
+
+  ostream &print(ostream &) const;
+
+  FSetValue * gc(void);
+
+  OZ_Boolean unify(OZ_Term t);
+  OZ_Boolean operator == (const FSetValue &) const;
+};
+
+inline
+ostream &operator << (ostream &ofile, const FSetValue &fs) {
+  return fs.print(ofile);
+}
+
+class OZ_FSetImpl {
+friend class FSetValue;
+private:
+  int _card; /* is -1 if the set is not valid */
+  int _known_not_in, _known_in;
+  int _in[fset_high], _not_in[fset_high];
+public:
+  OZ_FSetImpl(void) {};
+  OZ_FSetImpl(int c, OZ_Term, OZ_Term);
+
+  OZ_FSetImpl unify (const OZ_FSetImpl &) const;
+  OZ_Boolean unify (const FSetValue &) const;
+
+  int getCard(void) const { return _card; }
+  OZ_Boolean isValidSet(void) { return _card != -1; }
+  OZ_Boolean isWeakerThan(const OZ_FSetImpl &) const;
+  OZ_Boolean isFSetValue(void) { return _card == _known_in; }
+  ostream &print(ostream &) const;
+};
+
+inline
+ostream &operator << (ostream &ofile, const OZ_FSetImpl &fs) {
+  return fs.print(ofile);
+}
+
+typedef OZ_FSetImpl OZ_FSet;
+
+
+#endif // __FSET_HH__
---
+++ platform/emulator/fsetcore.cc
@@ -0,0 +1,47 @@
+/*
+  Hydra Project, DFKI Saarbruecken,
+  Stuhlsatzenhausweg 3, D-66123 Saarbruecken, Phone (+49) 681 302-5312
+  Author: tmueller
+  Last modified: $Date$ from $Author$
+  Version: $Revision$
+  State: $State$
+
+  ------------------------------------------------------------------------
+*/
+
+
+#include "am.hh"
+#include "genvar.hh"
+
+//-----------------------------------------------------------------------------
+// Introduce FD Built-ins to the Emulator
+
+OZ_C_proc_begin(BImkFSetValue, 2) 
+{
+  return OZ_unify(OZ_getCArg(1),
+		  makeTaggedFSetValue(new FSetValue(OZ_getCArg(0))));
+}
+OZ_C_proc_end
+
+OZ_C_proc_begin(BImkFSetVar, 4) 
+{
+  OZ_FSetImpl fset(OZ_intToC(OZ_getCArg(0)), OZ_getCArg(1), OZ_getCArg(2));
+
+  return OZ_unify(OZ_getCArg(3), makeTaggedRef(newTaggedCVar(new GenFSetVariable(fset))));
+}
+OZ_C_proc_end
+
+static
+BIspec fdSpec[] = {
+
+// fsetcore.cc
+  {"mkFSetValue", 2, BImkFSetValue},
+  {"mkFSetVar", 4, BImkFSetVar},
+
+  {0,0,0,0}
+};
+
+void BIinitFSet(void)
+{
+  BIaddSpec(fdSpec);
+}
