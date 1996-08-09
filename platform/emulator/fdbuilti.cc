1beec5b40aea653f393144e2fe6d1cc0be419099
wuertz <wuertz@ada56829-ad1f-0410-b00f-83cda6628aec>
1996-08-09 06:46:27 +0000

    stream-based firstLasts added

--- platform/emulator/fdbuilti.cc
+++ platform/emulator/fdbuilti.cc
@@ -1217,6 +1217,7 @@ BIspec fdSpec[] = {
   {"counter", 2, fdtest_counter},
   {"firstFail", 2, fdtest_firstFail},
   {"sched_taskIntervalsProof", 5, sched_taskIntervalsProof},
+  {"sched_firstsLasts", 4, sched_firstsLasts},
   {"spawnLess", 2, fdtest_spawnLess},
   {"dplus", 3, fdtest_plus},
 
