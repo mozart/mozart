719d91cc7a4f70d50dfc37c9d7bd865927694143
mehl <mehl@ada56829-ad1f-0410-b00f-83cda6628aec>
1996-10-01 11:37:23 +0000

    new file structure
    git-svn-id: https://gforge.info.ucl.ac.be/svn/mozart@2240 ada56829-ad1f-0410-b00f-83cda6628aec

---
+++ platform/emulator/libfd/count.hh
@@ -0,0 +1,69 @@
+/*
+  Hydra Project, DFKI Saarbruecken,
+  Stuhlsatzenhausweg 3, D-66123 Saarbruecken, Phone (+49) 681 302-5312
+  Author: tmueller
+  Last modified: $Date$ from $Author$
+  Version: $Revision$
+  State: $State$
+
+  ------------------------------------------------------------------------
+*/
+
+#ifndef __COUNT_HH__
+#define __COUNT_HH__
+
+#include "std.hh"
+
+//-----------------------------------------------------------------------------
+
+class ExactlyPropagator : public Propagator_D_VD_I {
+private:
+  static OZ_CFun spawner;
+public:
+  ExactlyPropagator(OZ_Term n, OZ_Term l, OZ_Term v)
+    : Propagator_D_VD_I(n, l, v) {}
+
+  virtual OZ_Return run(void);
+  virtual OZ_CFun getSpawner(void) const { return spawner; }
+};
+
+//-----------------------------------------------------------------------------
+
+class AtLeastPropagator : public Propagator_D_VD_I {
+private:
+  static OZ_CFun spawner;
+public:
+  AtLeastPropagator(OZ_Term n, OZ_Term l, OZ_Term v)
+    : Propagator_D_VD_I(n, l, v) {}
+
+  virtual OZ_Return run(void);
+  virtual OZ_CFun getSpawner(void) const { return spawner; }
+};
+
+//-----------------------------------------------------------------------------
+
+class AtMostPropagator : public Propagator_D_VD_I {
+private:
+  static OZ_CFun spawner;
+public:
+  AtMostPropagator(OZ_Term n, OZ_Term l, OZ_Term v)
+    : Propagator_D_VD_I(n, l, v) {}
+
+  virtual OZ_Return run(void);
+  virtual OZ_CFun getSpawner(void) const { return spawner; }
+};
+
+//-----------------------------------------------------------------------------
+
+class ElementPropagator : public Propagator_D_VI_D {
+private:
+  static OZ_CFun spawner;
+public:
+  ElementPropagator(OZ_Term n, OZ_Term l, OZ_Term v)
+    : Propagator_D_VI_D(n, l, v) {}
+  
+  virtual OZ_Return run(void);
+  virtual OZ_CFun getSpawner(void) const { return spawner; }
+};
+
+#endif // __COUNT_HH__
