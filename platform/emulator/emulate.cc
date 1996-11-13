cb350e243512dac736eb3e9f4b61cc91cb99437a
scheidhr <scheidhr@ada56829-ad1f-0410-b00f-83cda6628aec>
1996-11-13 10:05:14 +0000

    minor performance improvements under win

--- platform/emulator/emulate.cc
+++ platform/emulator/emulate.cc
@@ -431,13 +431,11 @@ Bool AM::hookCheckNeeded()
 
 
 /* define REGOPT if you want the into register optimization for GCC */
-#if defined(REGOPT) &&__GNUC__ >= 2 && (defined(LINUX_I486) || defined(MIPS) || defined(OSF1_ALPHA) || defined(SPARC)) && !defined(DEBUG_CHECK)
+#if defined(REGOPT) &&__GNUC__ >= 2 && (defined(GNUWIN32) || defined(LINUX_I486) || defined(MIPS) || defined(OSF1_ALPHA) || defined(SPARC)) && !defined(DEBUG_CHECK)
 #define Into(Reg) asm(#Reg)
 
-#ifdef LINUX_I486
-/* This does NOT pay off */
+#if defined(LINUX_I486) || defined(GNUWIN32)
 #define Reg1 asm("%esi")
-#define XReg1
 #define Reg2
 #define Reg3
 #define Reg4
