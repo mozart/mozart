0213d691040e1618e4daf479f050028de17acd73
wuertz <wuertz@ada56829-ad1f-0410-b00f-83cda6628aec>
1996-12-05 08:29:54 +0000

    stream based distinct added

---
+++ platform/emulator/libfd/streamProps.cc
@@ -0,0 +1,436 @@
+/*
+  Hydra Project, DFKI Saarbruecken,
+  Stuhlsatzenhausweg 3, D-66123 Saarbruecken, Phone (+49) 681 302-5312
+  Author: wuertz
+  Last modified: $Date$ from $Author$
+  Version: $Revision$
+  State: $State$
+
+  ------------------------------------------------------------------------
+*/
+
+#include "streamProps.hh"
+#include "rel.hh"
+#include "auxcomp.hh"
+#include <stdlib.h>
+
+//-----------------------------------------------------------------------------
+
+//--------------------------------------------------------------
+
+class Min_max {
+public:
+  int min, max;
+};
+
+//////////
+// CONSTRUCTOR
+//////////
+DisjunctivePropagatorStream::DisjunctivePropagatorStream(OZ_Term fds, OZ_Term durs, OZ_Term st) 
+{
+  stream = st;
+  reg_durs = vectorToInts(durs, reg_size);
+  reg_fds = vectorToOzTerms(fds, reg_size);
+}
+
+
+//////////
+// DESTRUCTOR
+//////////
+DisjunctivePropagatorStream::~DisjunctivePropagatorStream()
+{
+  OZ_hfreeCInts(reg_durs, reg_size);
+  OZ_hfreeOzTerms(reg_fds, reg_size);
+
+}
+
+
+//////////
+// BUILTIN
+//////////
+OZ_C_proc_begin(sched_disjunctiveStream, 3)
+{
+  OZ_EXPECTED_TYPE(OZ_EM_VECT OZ_EM_FD "," OZ_EM_VECT OZ_EM_INT "," OZ_EM_STREAM);
+
+  PropagatorExpect pe;
+
+  OZ_EXPECT(pe, 0, expectVectorIntVarAny);
+  OZ_EXPECT(pe, 1, expectVectorInt);
+  OZ_EXPECT(pe, 2, expectStream);
+
+  return pe.spawn(new DisjunctivePropagatorStream(OZ_args[0], OZ_args[1], OZ_args[2]));
+}
+OZ_C_proc_end
+
+
+//////////
+// COPYING
+//////////
+void DisjunctivePropagatorStream::updateHeapRefs(OZ_Boolean duplicate)
+{
+  int * new_reg_durs    = OZ_hallocCInts(reg_size);
+  OZ_Term * new_reg_fds = OZ_hallocOzTerms(reg_size);
+
+  for (int i = reg_size; i--; ) {
+    new_reg_durs[i] = reg_durs[i];
+    new_reg_fds[i] = reg_fds[i];
+    OZ_updateHeapTerm(new_reg_fds[i]);
+  }
+  reg_durs = new_reg_durs;
+  reg_fds  = new_reg_fds;
+
+}
+
+
+//////////
+// RUN METHOD
+//////////
+
+OZ_Return DisjunctivePropagatorStream::run(void)
+{
+  OZ_Stream st(stream);
+
+  int i, j;
+  int &ts  = reg_size;
+
+
+  int disjFlag = 0;
+  int new_items = 0;
+  struct MyList 
+  {
+    OZ_Term fd;
+    int         dur;
+    struct MyList * next;
+  };
+  struct MyList * my_list;
+  int only_stream = 1;
+
+
+  while (!st.isEostr()) {
+    OZ_Term e = st.get();
+    if (OZ_isTuple(e)) {
+      char * label = OZ_atomToC(OZ_label(e));
+      if (! strcmp("#", label)) {
+	OZ_Term new_fd  = OZ_getArg(e, 0);
+	int new_dur = OZ_intToC(OZ_getArg(e, 1));
+	MyList * newItem = new MyList;
+	newItem->fd = new_fd;
+	newItem->dur = new_dur;
+	if (new_items == 0)
+	  {
+	  newItem->next = NULL;
+	  my_list = newItem;
+	  }
+	else {
+	  newItem->next = my_list;
+	  my_list = newItem;
+	}
+	new_items++;
+      }
+      else {
+	st.fail(); 
+	
+	return FAILED;
+      }
+    }
+  }
+
+  
+  int new_ts = ts + new_items;
+  DECL_DYN_ARRAY(OZ_FDIntVar, x, new_ts);
+
+  if (new_items > 0) {
+    int * new_reg_durs    = OZ_hallocCInts(new_ts);
+    OZ_Term * new_reg_fds = OZ_hallocOzTerms(new_ts);
+
+    for (i = ts; i--; ) {
+      new_reg_durs[i] = reg_durs[i];
+      new_reg_fds[i] = reg_fds[i];
+    }
+
+    OZ_hfreeOzTerms(reg_fds,ts);
+    OZ_hfreeCInts(reg_durs,ts);
+
+    for (i = 0; i<new_items; i++ ) {
+      new_reg_durs[ts+i] = my_list->dur;
+      new_reg_fds[ts+i] = my_list->fd;
+      postOn(my_list->fd);
+      :: delete my_list;
+      my_list = my_list->next;
+    }
+    reg_durs = new_reg_durs;
+    reg_fds  = new_reg_fds;
+  }
+
+  ts = new_ts;
+
+  for (i = ts; i--; )
+    x[i].read(reg_fds[i]);
+
+  int * dur = reg_durs;
+
+  DECL_DYN_ARRAY(Min_max, MinMax, ts);
+  for (i=ts; i--;){
+    MinMax[i].min = x[i]->getMinElem();
+    MinMax[i].max = x[i]->getMaxElem();
+  }
+
+
+reifiedloop:
+
+   for (i=0; i<ts; i++)
+     for (j=i+1; j<ts; j++) {
+       int xui = MinMax[i].max, di = dur[i], xlj = MinMax[j].min;
+       if (xui + di <= xlj) continue;
+       int xuj = MinMax[j].max, dj = dur[j], xli = MinMax[i].min;
+       if (xuj + dj <= xli) continue;
+       if (xli + di > xuj) {
+	 if (xuj > xui - dj) {
+	   disjFlag = 1;
+	   FailOnEmpty(*x[j] <= xui - dj);
+	   MinMax[j].max = x[j]->getMaxElem();
+	 }
+	 if (xli < xlj + dj) {
+	   disjFlag = 1;
+	   FailOnEmpty(*x[i] >= xlj + dj);
+	   MinMax[i].min = x[i]->getMinElem();
+	 }
+       }
+       if (xlj + dj > xui) {
+	 if (xui > xuj - di) {
+	   disjFlag = 1;
+	   FailOnEmpty(*x[i] <= xuj - di);
+	   MinMax[i].max = x[i]->getMaxElem();
+	 }
+	 if (xlj < xli + di) {
+	   disjFlag = 1;
+	   FailOnEmpty(*x[j] >= xli + di);
+	   MinMax[j].min = x[j]->getMinElem();
+	 }
+       }
+     }
+
+  if (disjFlag == 1) {
+    disjFlag = 0;
+    goto reifiedloop;
+  }
+
+  if (!st.isValid()) 
+    goto failure;
+  
+
+  stream = st.getTail();
+
+  for (i = ts; i--; ) {
+    if (*x[i] != fd_singl) 
+      only_stream = 0;
+    x[i].leave();
+  }
+  
+  if (only_stream)
+    return st.leave() ? SLEEP : PROCEED;
+  else {
+    st.leave();
+    return SLEEP;
+  }
+
+failure:
+  st.fail(); 
+
+  for (i = ts; i--; )
+    x[i].fail();
+
+  return FAILED;
+
+}
+
+//-----------------------------------------------------------------------------
+
+//////////
+// CONSTRUCTOR
+//////////
+DistinctPropagatorStream::DistinctPropagatorStream(OZ_Term fds, OZ_Term st) 
+{
+  stream = st;
+  reg_fds = vectorToOzTerms(fds, reg_size);
+}
+
+
+//////////
+// DESTRUCTOR
+//////////
+DistinctPropagatorStream::~DistinctPropagatorStream()
+{
+  OZ_hfreeOzTerms(reg_fds, reg_size);
+}
+
+
+//////////
+// BUILTIN
+//////////
+OZ_C_proc_begin(fdp_distinctStream, 2)
+{
+  OZ_EXPECTED_TYPE(OZ_EM_VECT OZ_EM_FD "," OZ_EM_STREAM);
+
+  PropagatorExpect pe;
+
+  OZ_EXPECT(pe, 0, expectVectorIntVarAny);
+  OZ_EXPECT(pe, 1, expectStream);
+
+  return pe.spawn(new DistinctPropagatorStream(OZ_args[0], OZ_args[1]));
+}
+OZ_C_proc_end
+
+
+//////////
+// COPYING
+//////////
+void DistinctPropagatorStream::updateHeapRefs(OZ_Boolean duplicate)
+{
+  OZ_Term * new_reg_fds = OZ_hallocOzTerms(reg_size);
+  for (int i = reg_size; i--; ) {
+    new_reg_fds[i] = reg_fds[i];
+    OZ_updateHeapTerm(new_reg_fds[i]);
+  }
+  reg_fds  = new_reg_fds;
+}
+
+
+//////////
+// RUN METHOD
+//////////
+
+OZ_Return DistinctPropagatorStream::run(void)
+{
+  OZ_Stream st(stream);
+
+  int i;
+  int &ts  = reg_size;
+  OZ_FiniteDomain u(fd_empty);
+
+  int new_items = 0;
+  struct MyList 
+  {
+    OZ_Term fd;
+    struct MyList * next;
+  };
+  struct MyList * my_list;
+  int only_stream = 1;
+
+
+  while (!st.isEostr()) {
+    OZ_Term e = st.get();
+    if (OZ_isVariable(e)) {
+      MyList * newItem = new MyList;
+      newItem->fd = e;
+      if (new_items == 0)
+	{
+	  newItem->next = NULL;
+	  my_list = newItem;
+	}
+      else {
+	newItem->next = my_list;
+	my_list = newItem;
+      }
+      new_items++;
+    }
+    else {
+      st.fail(); 
+      
+      return FAILED;
+    }
+  }
+
+  
+  int new_ts = ts + new_items;
+  DECL_DYN_ARRAY(OZ_FDIntVar, x, new_ts);
+
+  if (new_items > 0) {
+    OZ_Term * new_reg_fds = OZ_hallocOzTerms(new_ts);
+
+    for (i = ts; i--; ) 
+      new_reg_fds[i] = reg_fds[i];
+
+    OZ_hfreeOzTerms(reg_fds,ts);
+
+    for (i = 0; i<new_items; i++ ) {
+      new_reg_fds[ts+i] = my_list->fd;
+      postOn(my_list->fd);
+      :: delete my_list;
+      my_list = my_list->next;
+    }
+    reg_fds  = new_reg_fds;
+  }
+
+  ts = new_ts;
+
+  for (i = ts; i--; )
+    x[i].read(reg_fds[i]);
+
+
+  // here comes the propagation
+  if (mayBeEqualVars()) {
+    int * is = OZ_findEqualVars(ts, reg_fds);
+    
+    for (i = ts; i--; )
+      if (is[i] != -1 && is[i] != i) 
+	goto failure;
+  }    
+  
+  for  (i = ts; i--; )
+    if (*x[i] == fd_singl) {
+      int s = x[i]->getSingleElem();
+      if (u.isIn(s)) {
+	goto failure;
+      } else {
+	u += s;
+      }
+    }
+
+ loop:
+  for (i = ts; i--; ) {
+    if (*x[i] != fd_singl) {
+      FailOnEmpty(*x[i] -= u);
+      
+      if (*x[i] == fd_singl) {
+	u += x[i]->getSingleElem();
+	goto loop;
+      }
+    }
+  }
+
+
+  if (!st.isValid()) 
+    goto failure;
+  
+
+  stream = st.getTail();
+
+  for (i = ts; i--; ) {
+    if (*x[i] != fd_singl) 
+      only_stream = 0;
+    x[i].leave();
+  }
+  
+  if (only_stream)
+    return st.leave() ? SLEEP : PROCEED;
+  else {
+    st.leave();
+    return SLEEP;
+  }
+
+failure:
+  st.fail(); 
+
+  for (i = ts; i--; )
+    x[i].fail();
+
+  return FAILED;
+
+}
+
+//-----------------------------------------------------------------------------
+// static member
+
+OZ_CFun DisjunctivePropagatorStream::spawner = sched_disjunctiveStream;
+OZ_CFun DistinctPropagatorStream::spawner = fdp_distinctStream;
