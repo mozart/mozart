9defd9b3f131937f560cd75b99da3c962afd7348
tmueller <tmueller@ada56829-ad1f-0410-b00f-83cda6628aec>
1997-01-20 09:24:13 +0000

    added threadstack for testing; is NOT activated, use LOCAL_THREAD_STACK
    git-svn-id: https://gforge.info.ucl.ac.be/svn/mozart@2815 ada56829-ad1f-0410-b00f-83cda6628aec

--- trunk/mozart/platform/emulator/thrqueue.hh
+++ trunk/mozart/platform/emulator/thrqueue.hh
@@ -52,7 +52,7 @@ public:
   int getSize () { return (size); }
   Bool isAllocated () { return (maxsize); }
 
-  int suggestNewSize() {
+  int suggestNewSize(void) {
     return max(min(size * 2,(maxsize + size) >> 1), QUEUEMINSIZE);
   }
 
@@ -75,6 +75,10 @@ public:
   Board * getHighestSolveDebug(void); // TMUELLER
   int getRunnableNumber();
   void deleteThread(Thread *th);
+
+  void disposePool () {
+    freeListDispose (queue, (size_t) (maxsize * sizeof (Thread *)));
+  }
 };
 
 class ThreadQueue : public ThreadQueueImpl {
@@ -86,18 +90,86 @@ public:
   void printThreads(void);
 };
 
-class LocalThreadQueue : public ThreadQueueImpl {
+class ThreadStackImpl {
+private:
+  int tos, size, maxsize;
+  Thread ** stack;
+
+  static int fromBos;
+
+  void resize(void);
+public:
+  
+  ThreadStackImpl(void) : tos(0), size(0), maxsize(0), stack(NULL) {}
+  ~ThreadStackImpl(void) {}
+
+  USEFREELISTMEMORY;
+ 
+  void allocate(int initsize) {
+    maxsize = initsize;
+    tos = size = 0;
+    // in the Oz heap;
+    stack = 
+      (Thread **) heapMalloc ((size_t) (sizeof(Thread *) * initsize));
+  }
+
+  Bool isEmpty () { return (size == 0); }
+  int getSize () { return (size); }
+  Bool isAllocated () { return (maxsize); }
+
+  // push
+  void enqueue (Thread * th) {
+    if (size == maxsize) resize();
+    stack[tos++] = th;
+    size++;
+  }
+
+  // pop
+  Thread * dequeue(void) {
+    Assert(!isEmpty());
+    Thread * th = stack[--tos];
+    size--;
+    return th;
+  }
+
+  Thread * readOutFromBottom(void) {
+    return (fromBos == tos) ? (Thread *) NULL : stack[fromBos++];
+  }
+  void initReadOutFromBottom(void) { fromBos = 0; }
+
+  void print(void) {}
+
+  int suggestNewSize(void) {
+    return max(min(size * 2,(maxsize + size) >> 1), QUEUEMINSIZE);
+  }
+
+  void disposePool () {
+    freeListDispose (stack, (size_t) (maxsize * sizeof (Thread *)));
+  }
+}; // class ThreadStackImpl
+ 
+//-----------------------------------------------------------------------------
+//#define LOCAL_THREAD_STACK
+
+
+#ifdef LOCAL_THREAD_STACK
+typedef ThreadStackImpl LocalThreadImpl;
+#else
+typedef ThreadQueueImpl LocalThreadImpl;
+#endif
+
+class LocalThreadQueue : public LocalThreadImpl {
 private:
   // needed when merging spaces to unpack threads in local thread queue
   Thread * ltq_thr;
 public:
   LocalThreadQueue(Thread * lthr, Thread * thr) 
-    : ltq_thr(lthr), ThreadQueueImpl() 
+    : ltq_thr(lthr), LocalThreadImpl() 
   {
     allocate(QUEUEMINSIZE);
     enqueue(thr);
   }
-  LocalThreadQueue(int sz) : ThreadQueueImpl(){
+  LocalThreadQueue(int sz) : LocalThreadImpl(){
     allocate(sz);
   }
   ~LocalThreadQueue() { error("never destroy LTQ"); }
@@ -105,13 +177,13 @@ public:
   LocalThreadQueue * gc(void);
 
   void dispose () {
-    freeListDispose (queue, (size_t) (maxsize * sizeof (Thread *)));
+    LocalThreadImpl::disposePool();
     freeListDispose (this, sizeof(LocalThreadQueue));
   }
 
   Thread * getLTQThread(void) { return ltq_thr; }
 };
 
-#endif
+#endif /* __THREADQUEUEH */
 
 
