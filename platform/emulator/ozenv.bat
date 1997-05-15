1f26447ad2a893b9d66aed97b1acd03c12820f0f
scheidhr <scheidhr@ada56829-ad1f-0410-b00f-83cda6628aec>
1997-05-15 08:16:29 +0000

    needed for Win development

---
+++ platform/emulator/ozenv.bat
@@ -0,0 +1,19 @@
+@echo off
+
+set OZHOME=y:\oz-devel
+
+set PATH=%PATH%;%OZHOME%\platform\win32-i486;C:\WINDOWS;C:\WINDOWS\COMMAND;C:\BIN;C:\DOS;C:\MWW\DLL;e:\WATCOM\BINNT;e:\WATCOM\BINW
+
+set OZPATH=.;%OZHOME%\lib;%OZHOME%\demo
+rem set OZRC=ozrc
+
+set TCL_LIBRARY=%OZHOME%\lib\wish\tcl
+set TK_LIBRARY=%OZHOME%\lib\wish\tk
+
+rem set TCL_LIBRARY=%OZHOME%\ozwish-win\lib\tcl
+rem set TK_LIBRARY=%OZHOME%\ozwish-win\lib\tk
+
+rem set TCL_LIBRARY=d:\tcl\lib\tcl7.5
+rem set TK_LIBRARY=d:\tcl\lib\tk4.1
+
+rem set OZBOOT=./ozboot.ql
