1e5bd5a17bd5b60faed1a094b004fea7beb3a1ac
mehl <mehl@ada56829-ad1f-0410-b00f-83cda6628aec>
1997-05-22 08:27:12 +0000

    if x=y bug fixed
    git-svn-id: https://gforge.info.ucl.ac.be/svn/mozart@3392 ada56829-ad1f-0410-b00f-83cda6628aec

--- platform/emulator/am.cc
+++ platform/emulator/am.cc
@@ -690,10 +690,9 @@ void AM::genericBind(TaggedRef *varPtr, TaggedRef var,
   if (prop) {
     if (isSVar(var)) {
       checkSuspensionList(var, pc_std_unif);
-
-      if (isSVar(term)) {
-	checkSuspensionList(term, pc_std_unif);
-      }
+    }
+    if (isSVar(term)) {
+      checkSuspensionList(term, pc_std_unif);
     }
   }
 
