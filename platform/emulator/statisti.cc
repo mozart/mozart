f2e9ffa1c97a1f06786d28f739517960fb1403a0
scheidhr <scheidhr@ada56829-ad1f-0410-b00f-83cda6628aec>
1997-05-26 11:37:45 +0000

    FreeBSD Port

--- platform/emulator/statisti.cc
+++ platform/emulator/statisti.cc
@@ -24,7 +24,7 @@
 #include "fdomn.hh"
 
 
-#if !defined(OSF1_ALPHA) && !defined(WINDOWS)
+#if !defined(OSF1_ALPHA) && !defined(WINDOWS) && !defined(FREEBSD_I486)
 extern "C" void *sbrk(int incr);
 #endif
 
