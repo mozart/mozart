527586db1711c8b38091deb520fa3e4b1608abc0
duchier <duchier@ada56829-ad1f-0410-b00f-83cda6628aec>
1997-06-26 12:54:09 +0000

    the procedure supplied to a lazy var can now also be a tuple where the 1st element is an integer that determines the interpretation of the remainder of the spec. 1#P is like just P. 2#X simply means to request X - somehow the computation of X should then normaly bind the current variable as a side effect. 3#URL is a lazyload of the URL.

--- platform/emulator/lazyvar.cc
+++ platform/emulator/lazyvar.cc
@@ -6,15 +6,72 @@
 #include "genvar.hh"
 #include "runtime.hh"
 
+// if `function' is a procedure or an object, we simply call it
+// with the variable itself as argument.
+//
+// for extension, `function' may also be a tuple, where typically
+// the first element is a small integer that determines the
+// operational interpretation:
+//
+//      1#P     ==> thread {P ME} end
+//      2#X     ==> force request of X
+//      3#URL   ==> thread {Load URL ME} end
+
+OZ_C_proc_proto(BIload);
+
 void
 GenLazyVariable::kickLazy()
 {
   if (function!=0) {
-    Thread* thr = am.mkRunnableThread(DEFAULT_PRIORITY,home);
-    thr->pushCall(function,&result,1);
-    am.scheduleThread(thr);
+    if (OZ_isProcedure(function)||OZ_isObject(function))
+      {
+        Thread* thr = am.mkRunnableThread(DEFAULT_PRIORITY,home);
+        thr->pushCall(function,&result,1);
+        am.scheduleThread(thr);
+      }
+    else if (OZ_isCons(function) ||
+             (OZ_isTuple(function)&&OZ_width(function)>1))
+      {
+        OZ_Term fst = OZ_getArg(function,0);
+        OZ_Term snd = OZ_getArg(function,1);
+        if (OZ_isSmallInt(fst))
+          switch (OZ_intToC(fst)) {
+          case 1:
+            // 1#P ==> thread {P ME} end
+            {
+              Thread* thr = am.mkRunnableThread(DEFAULT_PRIORITY,home);
+              thr->pushCall(function,&result,1);
+              am.scheduleThread(thr);
+              break;
+            }
+          case 2:
+            // 2#X ==> force request of X
+            snd = OZ_deref(snd);
+            if (tagTypeOf(snd)==CVAR &&
+                tagged2CVar(snd)->getType()==LazyVariable)
+              ((GenLazyVariable*)tagged2CVar(snd))->kickLazy();
+            break;
+          case 3:
+            // 3#URL ==> thread {Load URL ME} end
+            {
+              Thread* thr = am.mkRunnableThread(DEFAULT_PRIORITY,home);
+              TaggedRef args[] = { snd , result };
+              thr->pushCFun(BIload,args,2,TRUE);
+              am.scheduleThread(thr);
+              break;
+            }
+          default:
+            goto illegal;
+          }
+        else goto illegal;
+      }
+    else goto illegal;
     function=0;
   }
+  return;
+  illegal:
+  function=0;
+  OZ_warning("Lazy variable contains illegal spec");
 }
 
 void
@@ -56,8 +113,10 @@ OZ_C_proc_begin(BILazyNew,2)
 {
   OZ_Term oz_fun = OZ_getCArg(0);
   OZ_Term oz_res = OZ_getCArg(1);
-  if (!OZ_isProcedure(oz_fun))
-    return OZ_typeError(0,"Unary Procedure");
+  if (!OZ_isProcedure(oz_fun) &&
+      !OZ_isObject(oz_fun)    &&
+      !OZ_isTuple(oz_fun))
+    return OZ_typeError(0,"Unary Procedure|Object|Tuple");
   GenLazyVariable *lazy = new GenLazyVariable(oz_fun,oz_res);
   return OZ_unify(oz_res,(OZ_Term)newTaggedCVar((GenCVariable*)lazy));
 }
