d338c28b559dafa6102e1f9e38984733f0d34136
tmueller <tmueller@ada56829-ad1f-0410-b00f-83cda6628aec>
1997-06-27 11:36:29 +0000

    profile support for propagators
    git-svn-id: https://gforge.info.ucl.ac.be/svn/mozart@3522 ada56829-ad1f-0410-b00f-83cda6628aec

---
+++ trunk/mozart/platform/emulator/cpi_profile.cc
@@ -0,0 +1,16 @@
+/*
+  Hydra Project, DFKI Saarbruecken,
+  Stuhlsatzenhausweg 3, D-66123 Saarbruecken, Phone (+49) 681 302-5312
+  Author: tmueller
+  Last modified: $Date$ from $Author$
+  Version: $Revision$
+  State: $State$
+
+  ------------------------------------------------------------------------
+*/
+
+
+#include "oz_cpi.hh"
+
+
+OZ_CFunHeader * OZ_CFunHeader::_all_headers = NULL;
