88c1ce865af7c6878fe3cf3ea03c2830389400b9
mehl <mehl@ada56829-ad1f-0410-b00f-83cda6628aec>
1997-07-21 10:02:59 +0000

    minor changes; #define PERDIO removed; #define #NEW_STACK removed
    git-svn-id: https://gforge.info.ucl.ac.be/svn/mozart@3643 ada56829-ad1f-0410-b00f-83cda6628aec

--- platform/emulator/taskstk.hh
+++ platform/emulator/taskstk.hh
@@ -10,15 +10,6 @@
   ------------------------------------------------------------------------
 */
 
-/*
- * NEW_STACK is an experimental feature.
- * If defined the thread stack is represented as linked list
- * (Michael)
- *
- * usage 'ozmkmf -DNEW_STACK'
- */
-
-
 #ifndef __TASKSTACKH
 #define __TASKSTACKH
 
@@ -41,184 +32,6 @@ extern ProgramCounter
   C_EMPTY_STACK        ;
 
 
-#ifdef NEW_STACK
-class TaskStack;
-
-class Frame {
-public:
-  friend class TaskStack;
-private:
-  Frame *next;
-  ProgramCounter pc;
-  void *y;
-  void *g;
-
-public:
-  USEFREELISTMEMORY;
-  Frame() {}
-  Frame(ProgramCounter pc,void *y, void *g, Frame *next) 
-    : next(next), pc(pc), y(y), g(g) {}
-
-  ProgramCounter getPC() const { return pc; }
-  Frame *getNext() const       { return next; }
-  RefsArray getY() const       { return (RefsArray) y; }
-  RefsArray getG() const       { return (RefsArray) g; }
-
-  Frame *dispose() {
-    Frame *ret = getNext();
-    freeListDispose(this,sizeof(Frame));
-    return ret;
-  }
-  void disposeAll() {
-    for (Frame *f=this; f; f = f->dispose()) ;
-  }
-
-  int count() const {
-    int i=0;
-    for (const Frame *f=this; f; f=f->getNext()) {
-      i++;
-    }
-    return i;
-  }
-};
-
-inline
-int isEmpty(Frame *f) { return f->getPC()==C_EMPTY_STACK; }
-
-class TaskStack {
-private:
-  Frame *tos;
-
-public:
-  USEFREELISTMEMORY;
-
-  ~TaskStack() { Assert(0); }
-
-  Frame *getTop() { return tos; }
-  void setTop(Frame *t) { tos=t; }
-
-  void pushFrame(ProgramCounter pc,void *y, void *g)
-  {
-    Frame *fr = new Frame();
-    fr->next=tos;
-    tos=fr;
-    fr->pc=pc;
-    fr->y=y;
-    fr->g=g;
-    // mm2 CountMax(maxStackDepth,(tos-array)*sizeof(StackEntry));
-  }
-
-  Bool checkFrame(ProgramCounter pc) 
-  { 
-    return tos->getPC()==pc;
-  }
-
-  void discardFrame(ProgramCounter pc) 
-  { 
-    Assert(pc==NOCODE || checkFrame(pc));
-    tos = tos->dispose();
-  }
-
-  void pushEmpty() {
-    Assert(!tos);
-    pushFrame(C_EMPTY_STACK,0,0);
-  }
-
-  TaskStack() {
-    tos=0;
-    pushEmpty();
-  }
-
-  void makeEmpty()
-  {
-    if (tos) {
-      tos->disposeAll();
-    }
-    init();
-  }
-  void init()
-  {
-    tos=0;
-    pushEmpty();
-  }
-  Bool isEmpty() { return ::isEmpty(tos); }
-
-  void printTaskStack(ProgramCounter pc = NOCODE,
-		      Bool verbose = NO, int depth = 1000);
-  TaggedRef dbgGetTaskStack(ProgramCounter pc = NOCODE, int depth = 1000);
-  TaggedRef dbgFrameVariables(int frameId);
-
-  void gc(TaskStack *);
-
-  Bool findCatch(TaggedRef *traceBack=0, Bool verbose=0);
-
-  void pushCFun(OZ_CFun f, RefsArray  x, int i, Bool copy)
-  {
-    DebugCheckT(for (int ii = 0; ii < i; ii++) CHECK_NONVAR(x[ii]));
-
-    Assert(MemChunks::areRegsInHeap(x, i));
-
-    pushFrame(C_CFUNC_CONT_Ptr,(void*)f,
-	      i>0 ? (copy ? copyRefsArray(x, i) : x) : NULL);
-  }
-  
-  void pushCont(ProgramCounter pc,RefsArray y,RefsArray g,RefsArray x)
-  {
-    Assert(!isFreedRefsArray(y));
-
-    Assert(!x || MemChunks::areRegsInHeap(x,getRefsArraySize(x)));
-    Assert(!y || MemChunks::areRegsInHeap(y,getRefsArraySize(y)));
-    Assert(!g || MemChunks::areRegsInHeap(g,getRefsArraySize(g)));
-	       
-    pushFrame(pc,y,g);
-    if (x)
-      pushFrame(C_XCONT_Ptr,x,0);
-  }
-
-  void pushCall(TaggedRef pred, RefsArray  x, int i)
-  {
-    DebugCheckT(for (int ii = 0; ii < i; ii++) CHECK_NONVAR(x[ii]));
-
-    pushFrame(C_CALL_CONT_Ptr, ToPointer(pred), i>0 ? copyRefsArray(x, i) : NULL);
-  }
-
-  void pushCallNoCopy(TaggedRef pred, RefsArray  x)
-  {
-    pushFrame(C_CALL_CONT_Ptr, ToPointer(pred), x);
-  }
-
-  void pushLTQ(SolveActor *sa)   { pushFrame(C_LTQ_Ptr,sa,0); }
-  void pushLock(OzLock *lck)     { pushFrame(C_LOCK_Ptr,lck,0); }
-  void pushCatch()               { pushFrame(C_CATCH_Ptr,0,0); }
-  void pushDebug(OzDebug *deb)   { pushFrame(C_DEBUG_CONT_Ptr,deb,0); }
-  void pushSelf(Object *o)       { pushFrame(C_SET_SELF_Ptr,o,NULL); }
-  void pushActor(Actor *aa)      { pushFrame(C_ACTOR_Ptr,aa,0); }
-
-  int tasks();
-};
-
-#define GetFrameNoDecl(tos,pc,y,g)		\
-{						\
-  pc   = tos->getPC();				\
-  y    = tos->getY();				\
-  g    = tos->getG();				\
-						\
-  tos  = tos->getNext();			\
-}
-
-#define PopFrameNoDecl(ts,pc,y,g)		\
-{						\
-  TaskStack *__ts = ts;				\
-  Frame *__top = __ts->getTop();		\
-  __ts->setTop(__top->dispose());		\
-  pc   = __top->getPC();			\
-  y    = __top->getY();				\
-  g    = __top->getG();				\
-}
-
-#endif
-
-#ifndef NEW_STACK
 typedef StackEntry Frame;
 
 inline
@@ -303,6 +116,20 @@ public:
 
   Bool findCatch(TaggedRef *traceBack=0, Bool verbose=0);
 
+
+  void pushX(RefsArray X) {
+    Assert(X);
+    pushFrame(C_XCONT_Ptr,X,0);
+  }
+
+  void pushX(RefsArray X, int I) {
+    int i=I;
+    RefsArray x=i>0?copyRefsArray(X,i):0;
+    Assert(!x || MemChunks::areRegsInHeap(x,getRefsArraySize(x)));
+    if (x)
+      pushX(x);
+  }
+
   void pushCFun(OZ_CFun f, RefsArray  x, int i, Bool copy)
   {
     DebugCheckT(for (int ii = 0; ii < i; ii++) CHECK_NONVAR(x[ii]));
@@ -313,17 +140,14 @@ public:
 	      i>0 ? (copy ? copyRefsArray(x, i) : x) : NULL);
   }
   
-  void pushCont(ProgramCounter pc,RefsArray y,RefsArray g,RefsArray x)
+  void pushCont(ProgramCounter pc,RefsArray y,RefsArray g)
   {
     Assert(!isFreedRefsArray(y));
 
-    Assert(!x || MemChunks::areRegsInHeap(x,getRefsArraySize(x)));
     Assert(!y || MemChunks::areRegsInHeap(y,getRefsArraySize(y)));
     Assert(!g || MemChunks::areRegsInHeap(g,getRefsArraySize(g)));
 	       
     pushFrame(pc,y,g);
-    if (x)
-      pushFrame(C_XCONT_Ptr,x,0);
   }
 
   void pushCall(TaggedRef pred, RefsArray  x, int i)
@@ -377,4 +201,3 @@ public:
     RefsArray y,g;				\
     GetFrameNoDecl(top,pc,y,g)
 
-#endif
