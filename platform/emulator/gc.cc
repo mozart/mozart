473d377cb557506120c53da2feb38517df71be47
lorenz <lorenz@ada56829-ad1f-0410-b00f-83cda6628aec>
1997-09-18 14:17:32 +0000

    Speedup of 1400% when running with debugmode Avoid compiler warnings Add thread state to Debug.dumpThreads output
    Index: emulate.cc
    ===================================================================
    RCS file: /home/ps-home/hydra/CVS/Oz/Emulator/emulate.cc,v
    retrieving revision 1.533
    diff -p -u -d -r1.533 emulate.cc
    --- emulate.cc	1997/09/02 09:28:02	1.533
    +++ emulate.cc	1997/09/16 15:07:27
    @@ -300,8 +300,11 @@ Bool AM::emulateHookOutline(ProgramCount
         return TRUE;
       }
    
    -  if (def && debugmode() && !currentSolveBoard && currentThread != rootThread
    -                         && CodeArea::existVarNames(PC)) {
    +  if (def && debugmode() && !currentSolveBoard && currentThread != rootThread
    +                         &&
    +      (CodeArea::getOpcode(PC+sizeOf(CodeArea::getOpcode(PC)))==DEBUGINFO ||
    +       CodeArea::getOpcode(PC)==DEBUGINFO)) {
    +
         OzDebug *dbg;
         int frameId   = ++lastFrameID % MAX_ID;
    
    @@ -554,7 +557,8 @@ void pushDummyDebug(TaskStack *stk, Prog
    
     #ifdef FASTREGACCESS
     #define RegAccess(Reg,Index) (*(RefsArray)((intlong) Reg + Index))
    -#define LessIndex(Index,CodedIndex) (Index <= CodedIndex/sizeof(TaggedRef))
    +#define LessIndex(Index,CodedIndex) \
    +                       (Index <= (int)(CodedIndex/sizeof(TaggedRef)))
     #else
     #define RegAccess(Reg,Index) (Reg[Index])
     #define LessIndex(I1,I2) (I1<=I2)
    Index: gc.cc
    ===================================================================
    RCS file: /home/ps-home/hydra/CVS/Oz/Emulator/gc.cc,v
    retrieving revision 1.341
    diff -p -u -d -r1.341 gc.cc
    --- gc.cc	1997/09/02 10:25:13	1.341
    +++ gc.cc	1997/09/16 15:48:40
    @@ -269,9 +269,15 @@ public:
       static void print()
       {
         for (ThreadList *aux = allthreads; aux; aux=aux->next) {
    -      printf("\n\n");
    -      message("Thread: id=%d\n",aux->elem->getID());
    -      message("----------------\n",aux->elem->getID());
    +      char *s;
    +      if (aux->elem->isDeadThread())
    +	s = "terminated";
    +      else if (aux->elem->isRunnable())
    +	s = "ready";
    +      else
    +	s = "blocked";
    +      message("Thread: id = %d, state: %s\n",aux->elem->getID(),s);
    +      message("----------------------------------------\n");
           aux->elem->printTaskStack(NOCODE,NO);
         }
       }
    cvs diff: Diffing FDLib
    cvs diff: Diffing FSetLib

--- platform/emulator/gc.cc
+++ platform/emulator/gc.cc
@@ -269,9 +269,15 @@ public:
   static void print()
   {
     for (ThreadList *aux = allthreads; aux; aux=aux->next) {
-      printf("\n\n");
-      message("Thread: id=%d\n",aux->elem->getID());
-      message("----------------\n",aux->elem->getID());
+      char *s;
+      if (aux->elem->isDeadThread())
+	s = "terminated";
+      else if (aux->elem->isRunnable())
+	s = "ready";
+      else
+	s = "blocked";
+      message("Thread: id = %d, state: %s\n",aux->elem->getID(),s);
+      message("----------------------------------------\n");
       aux->elem->printTaskStack(NOCODE,NO);
     }
   }
