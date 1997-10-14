0b9a1139fed809dffb51fe78cafa0daa78d3ba47
kornstae <kornstae@ada56829-ad1f-0410-b00f-83cda6628aec>
1997-10-14 09:58:10 +0000

    Adapted to changes of instrDefs; Added isNumber to foreign function interface.

--- platform/emulator/assemble.cc
+++ platform/emulator/assemble.cc
@@ -1,7 +1,6 @@
 /*
   Hydra Project, DFKI Saarbruecken,
   Stuhlsatzenhausweg 3, D-66123 Saarbruecken, Phone (+49) 681 302-5312
-  Author: popow
   */
 
 
@@ -15,17 +14,54 @@
 #include "indexing.hh"
 
 
+static
+SRecordArity getArity(TaggedRef arity)
+{
+  arity = deref(arity);
+  if (isSmallInt(arity)) {
+    return mkTupleWidth(smallIntValue(arity));
+  }
 
-OZ_C_proc_begin(BIopInfo,2)
+  int len = length(arity);
+  arity = sortlist(arity,len);
+  Arity *ari = aritytable.find(arity);
+  return (ari->isTuple())? mkTupleWidth(ari->getWidth()): mkRecordArity(ari);
+}
+
+
+inline
+Bool getBool(TaggedRef t)
+{
+  return literalEq(deref(t),NameTrue);
+}
+
+
+OZ_C_proc_begin(BIgetOpcode,2)
 {
   OZ_declareAtomArg(0,opname);
   OZ_declareArg(1,ret);
 
   Opcode oc = CodeArea::stringToOp(opname);
   if (oc == OZERROR) {
-    return oz_raise(E_ERROR,OZ_atom("assembler"),"unknownInstruction",1,opname);
+    return oz_raise(E_ERROR,OZ_atom("assembler"),
+		    "unknownInstruction",1,opname);
   }
-  return OZ_unify(ret, OZ_pair2(OZ_int(oc), OZ_int(sizeOf(oc))));
+  return OZ_unify(ret, OZ_int(oc));
+}
+OZ_C_proc_end
+
+
+OZ_C_proc_begin(BIgetInstructionSize,2)
+{
+  OZ_declareAtomArg(0,opname);
+  OZ_declareArg(1,ret);
+
+  Opcode oc = CodeArea::stringToOp(opname);
+  if (oc == OZERROR) {
+    return oz_raise(E_ERROR,OZ_atom("assembler"),
+		    "unknownInstruction",1,opname);
+  }
+  return OZ_unify(ret, OZ_int(sizeOf(oc)));
 }
 OZ_C_proc_end
 
@@ -52,9 +88,10 @@ OZ_C_proc_begin(BImakeProc,3)
   OZ_declareNonvarArg(1,globals);
   OZ_declareArg(2,ret);
   globals = deref(globals);
-  
+
   int numglobals = length(globals);
-  RefsArray gRegs = (numglobals == 0) ? (RefsArray) NULL : allocateRefsArray(numglobals);
+  RefsArray gRegs =
+    numglobals? allocateRefsArray(numglobals): (RefsArray) NULL;
 
   for (int i = 0; i < numglobals; i++) {
     gRegs[i] = head(globals);
@@ -83,28 +120,8 @@ OZ_C_proc_begin(BIaddDebugInfo,3)
 }
 OZ_C_proc_end
 
-   
-OZ_C_proc_begin(BIstoreInt,2)
-{
-  declareCodeBlock(0,code);
-  OZ_declareIntArg(1,i);
-  code->writeInt(i);
-  return PROCEED;
-}
-OZ_C_proc_end
 
-
-OZ_C_proc_begin(BIstoreReg,2)
-{
-  declareCodeBlock(0,code);
-  OZ_declareIntArg(1,i);
-  code->writeReg(i);
-  return PROCEED;
-}
-OZ_C_proc_end
-
-
-OZ_C_proc_begin(BIstoreOp,2)
+OZ_C_proc_begin(BIstoreOpcode,2)
 {
   declareCodeBlock(0,code);
   OZ_declareIntArg(1,i);
@@ -115,7 +132,43 @@ OZ_C_proc_begin(BIstoreOp,2)
 OZ_C_proc_end
 
 
-OZ_C_proc_begin(BIstoreTagged,2)
+OZ_C_proc_begin(BIstoreNumber,2)
+{
+  declareCodeBlock(0,code);
+  OZ_declareNonvarArg(1,arg);
+  arg = deref(arg);
+  Assert(OZ_isNumber(arg));
+  code->writeTagged(arg);
+  return PROCEED;
+}
+OZ_C_proc_end
+
+
+OZ_C_proc_begin(BIstoreLiteral,2)
+{
+  declareCodeBlock(0,code);
+  OZ_declareNonvarArg(1,arg);
+  arg = deref(arg);
+  Assert(OZ_isLiteral(arg));
+  code->writeTagged(arg);
+  return PROCEED;
+}
+OZ_C_proc_end
+
+
+OZ_C_proc_begin(BIstoreFeature,2)
+{
+  declareCodeBlock(0,code);
+  OZ_declareNonvarArg(1,arg);
+  arg = deref(arg);
+  Assert(OZ_isFeature(arg));
+  code->writeTagged(arg);
+  return PROCEED;
+}
+OZ_C_proc_end
+
+
+OZ_C_proc_begin(BIstoreConstant,2)
 {
   declareCodeBlock(0,code);
   OZ_declareNonvarArg(1,arg);
@@ -138,6 +191,38 @@ OZ_C_proc_begin(BIstoreBuiltinname,2)
 OZ_C_proc_end
 
 
+OZ_C_proc_begin(BIstoreVariablename,2)
+{
+  declareCodeBlock(0,code);
+  OZ_declareNonvarArg(1,arg);
+  arg = deref(arg);
+  Assert(OZ_isAtom(arg));
+  code->writeTagged(arg);
+  return PROCEED;
+}
+OZ_C_proc_end
+
+
+OZ_C_proc_begin(BIstoreRegisterIndex,2)
+{
+  declareCodeBlock(0,code);
+  OZ_declareIntArg(1,i);
+  code->writeReg(i);
+  return PROCEED;
+}
+OZ_C_proc_end
+
+
+OZ_C_proc_begin(BIstoreInt,2)
+{
+  declareCodeBlock(0,code);
+  OZ_declareIntArg(1,i);
+  code->writeInt(i);
+  return PROCEED;
+}
+OZ_C_proc_end
+
+
 OZ_C_proc_begin(BIstoreLabel,2)
 {
   declareCodeBlock(0,code);
@@ -146,42 +231,6 @@ OZ_C_proc_begin(BIstoreLabel,2)
   return PROCEED;
 }
 OZ_C_proc_end
-
-
-static 
-SRecordArity getArity(TaggedRef arity)
-{
-  arity = deref(arity);
-  if (isSmallInt(arity)) {
-    return mkTupleWidth(smallIntValue(arity));
-  } 
-
-  int len=length(arity);
-  arity = sortlist(arity,len);
-  Arity *ari = aritytable.find(arity);
-  return (ari->isTuple()) ? mkTupleWidth(ari->getWidth()) :  mkRecordArity(ari);
-}
-
-
-OZ_C_proc_begin(BIstoreRecordArity,2)
-{
-  declareCodeBlock(0,code);
-  OZ_declareNonvarArg(1,arity);
-  code->writeSRecordArity(getArity(arity));
-  return PROCEED;
-}
-OZ_C_proc_end
-
-
-OZ_C_proc_begin(BIstoreCache,2)
-{
-  declareCodeBlock(0,code);
-  OZ_declareNonvarArg(1,ignored);
-  code->writeCache();  
-  return PROCEED;
-}
-OZ_C_proc_end
-
 
 
 OZ_C_proc_begin(BIstorePredicateRef,2)
@@ -202,79 +251,8 @@ OZ_C_proc_begin(BIstorePredId,5)
   OZ_declareNonvarArg(2,arity);
   OZ_declareNonvarArg(3,file); file = deref(file);
   OZ_declareIntArg(4,line);
-  PrTabEntry *pte  = new PrTabEntry(name,getArity(arity),file,line);
+  PrTabEntry *pte = new PrTabEntry(name,getArity(arity),file,line);
   code->writeAddress(pte);
-  return PROCEED;
-}
-OZ_C_proc_end
-
-
-inline 
-Bool getBool(TaggedRef t)
-{
-  return literalEq(deref(t),NameTrue);
-}
-
-OZ_C_proc_begin(BIstoreGenCallInfo,6)
-{
-  declareCodeBlock(0,code);
-  OZ_declareIntArg(1,regindex);
-  OZ_declareNonvarArg(2,isMethod);
-  OZ_declareNonvarArg(3,name); name = deref(name);
-  OZ_declareNonvarArg(4,isTail);
-  OZ_declareNonvarArg(5,arity);
-
-  GenCallInfoClass *gci = new GenCallInfoClass(regindex,getBool(isMethod),name, 
-					       getBool(isTail),getArity(arity));
-  code->writeAddress(gci);
-  return PROCEED;
-}
-OZ_C_proc_end
-
-
-OZ_C_proc_begin(BIstoreApplMethInfo,3)
-{
-  declareCodeBlock(0,code);
-  OZ_declareNonvarArg(1,name); name = deref(name);
-  OZ_declareNonvarArg(2,arity);
-
-  ApplMethInfoClass *ami = new ApplMethInfoClass(name,getArity(arity));
-  code->writeAddress(ami);
-  return PROCEED;
-}
-OZ_C_proc_end
-
-
-OZ_C_proc_begin(BIstoreGRegRef,2)
-{
-  declareCodeBlock(0,code);
-  OZ_declareNonvarArg(1,globals);
-  globals = deref(globals);
-  int numglobals = length(globals);
-
-  AssRegArray *gregs = new AssRegArray(numglobals);
-
-  for (int i = 0; i < numglobals; i++) {
-    SRecord *rec = tagged2SRecord(deref(head(globals)));
-    globals = deref(tail(globals));
-
-    const char *label = rec->getLabelLiteral()->getPrintName();
-    KindOfReg regType;
-    if (strcmp(label,"x")==0) {
-      regType = XReg;
-    } else if (strcmp(label,"y")==0) {
-      regType = YReg;
-    } else {
-      Assert(strcmp(label,"g")==0);
-      regType = GReg;
-    }
-    (*gregs)[i].kind = regType;
-    (*gregs)[i].number = smallIntValue(deref(rec->getArg(0)));
-  }
-
-  Assert(isNil(globals));
-
-  code->writeAddress(gregs);
   return PROCEED;
 }
 OZ_C_proc_end
@@ -288,7 +266,7 @@ OZ_C_proc_begin(BInewHashTable,4)
   OZ_declareArg(3,ret);
 
   IHashTable *ht = new IHashTable(size,code->getStart()+elseLabel);
-  
+
   code->writeAddress(ht);
   return OZ_unifyInt(ret, ToInt32(ht));
 }
@@ -345,7 +323,7 @@ OZ_C_proc_begin(BIstoreHTRecord,5)
   SRecordArity ar   = getArity(arity);
   reclabel          = deref(reclabel);
   ByteCode *absaddr = code->getStart() + label;
-  
+
   if (literalEq(reclabel,AtomCons) && sraIsTuple(ar) && getTupleWidth(ar)==2) {
     ht->addList(absaddr);
   } else {
@@ -357,30 +335,120 @@ OZ_C_proc_begin(BIstoreHTRecord,5)
 OZ_C_proc_end
 
 
+OZ_C_proc_begin(BIstoreRecordArity,2)
+{
+  declareCodeBlock(0,code);
+  OZ_declareNonvarArg(1,arity);
+  code->writeSRecordArity(getArity(arity));
+  return PROCEED;
+}
+OZ_C_proc_end
+
+
+OZ_C_proc_begin(BIstoreGenCallInfo,6)
+{
+  declareCodeBlock(0,code);
+  OZ_declareIntArg(1,regindex);
+  OZ_declareNonvarArg(2,isMethod);
+  OZ_declareNonvarArg(3,name); name = deref(name);
+  OZ_declareNonvarArg(4,isTail);
+  OZ_declareNonvarArg(5,arity);
+
+  GenCallInfoClass *gci =
+    new GenCallInfoClass(regindex,getBool(isMethod),name,
+			 getBool(isTail),getArity(arity));
+  code->writeAddress(gci);
+  return PROCEED;
+}
+OZ_C_proc_end
+
+
+OZ_C_proc_begin(BIstoreApplMethInfo,3)
+{
+  declareCodeBlock(0,code);
+  OZ_declareNonvarArg(1,name); name = deref(name);
+  OZ_declareNonvarArg(2,arity);
+
+  ApplMethInfoClass *ami = new ApplMethInfoClass(name,getArity(arity));
+  code->writeAddress(ami);
+  return PROCEED;
+}
+OZ_C_proc_end
+
+
+OZ_C_proc_begin(BIstoreGRegRef,2)
+{
+  declareCodeBlock(0,code);
+  OZ_declareNonvarArg(1,globals);
+  globals = deref(globals);
+  int numglobals = length(globals);
+
+  AssRegArray *gregs = new AssRegArray(numglobals);
+
+  for (int i = 0; i < numglobals; i++) {
+    SRecord *rec = tagged2SRecord(deref(head(globals)));
+    globals = deref(tail(globals));
+
+    const char *label = rec->getLabelLiteral()->getPrintName();
+    KindOfReg regType;
+    if (strcmp(label,"x")==0) {
+      regType = XReg;
+    } else if (strcmp(label,"y")==0) {
+      regType = YReg;
+    } else {
+      Assert(strcmp(label,"g")==0);
+      regType = GReg;
+    }
+    (*gregs)[i].kind = regType;
+    (*gregs)[i].number = smallIntValue(deref(rec->getArg(0)));
+  }
+
+  Assert(isNil(globals));
+
+  code->writeAddress(gregs);
+  return PROCEED;
+}
+OZ_C_proc_end
+
+
+OZ_C_proc_begin(BIstoreCache,2)
+{
+  declareCodeBlock(0,code);
+  OZ_declareNonvarArg(1,ignored);
+  code->writeCache();
+  return PROCEED;
+}
+OZ_C_proc_end
+
+
 static
 BIspec biSpec[] = {
-  {"opInfo",           2, BIopInfo,           0},
-  {"newCodeBlock",     2, BInewCodeBlock,     0},
-  {"makeProc",         3, BImakeProc,         0},
-  {"addDebugInfo",     3, BIaddDebugInfo,     0},
-  {"storeOp",          2, BIstoreOp,          0},
-  {"storeInt",         2, BIstoreInt,         0},
-  {"storeReg",         2, BIstoreReg,         0},
-  {"storeTagged",      2, BIstoreTagged,      0},
-  {"storeBuiltinname", 2, BIstoreBuiltinname, 0},
-  {"storeLabel",       2, BIstoreLabel,       0},
-  {"storeRecordArity", 2, BIstoreRecordArity, 0},
-  {"storeCache",       2, BIstoreCache,       0},
-  {"storePredicateRef",2, BIstorePredicateRef,0},
-  {"storePredId",      5, BIstorePredId,      0},
-  {"storeGenCallInfo", 6, BIstoreGenCallInfo, 0},
-  {"storeApplMethInfo",3, BIstoreApplMethInfo, 0},
-  {"storeGRegRef",     2, BIstoreGRegRef, 0},
-
-  {"newHashTable",     4, BInewHashTable, 0},
-  {"storeHTVarLabel",  3, BIstoreHTVarLabel, 0},
-  {"storeHTScalar",    4, BIstoreHTScalar, 0},
-  {"storeHTRecord",    5, BIstoreHTRecord, 0},
+  {"getOpcode",         2, BIgetOpcode,         0},
+  {"getInstructionSize",2, BIgetInstructionSize,0},
+  {"newCodeBlock",      2, BInewCodeBlock,      0},
+  {"makeProc",          3, BImakeProc,          0},
+  {"addDebugInfo",      3, BIaddDebugInfo,      0},
+  {"storeOpcode",       2, BIstoreOpcode,       0},
+  {"storeNumber",       2, BIstoreNumber,       0},
+  {"storeLiteral",      2, BIstoreLiteral,      0},
+  {"storeFeature",      2, BIstoreFeature,      0},
+  {"storeConstant",     2, BIstoreConstant,     0},
+  {"storeBuiltinname",  2, BIstoreBuiltinname,  0},
+  {"storeVariablename", 2, BIstoreVariablename, 0},
+  {"storeRegisterIndex",2, BIstoreRegisterIndex,0},
+  {"storeInt",          2, BIstoreInt,          0},
+  {"storeLabel",        2, BIstoreLabel,        0},
+  {"storePredicateRef", 2, BIstorePredicateRef, 0},
+  {"storePredId",       5, BIstorePredId,       0},
+  {"newHashTable",      4, BInewHashTable,      0},
+  {"storeHTVarLabel",   3, BIstoreHTVarLabel,   0},
+  {"storeHTScalar",     4, BIstoreHTScalar,     0},
+  {"storeHTRecord",     5, BIstoreHTRecord,     0},
+  {"storeRecordArity",  2, BIstoreRecordArity,  0},
+  {"storeGenCallInfo",  6, BIstoreGenCallInfo,  0},
+  {"storeApplMethInfo", 3, BIstoreApplMethInfo, 0},
+  {"storeGRegRef",      2, BIstoreGRegRef,      0},
+  {"storeCache",        2, BIstoreCache,        0},
 
   {0,0,0,0}
 };
@@ -389,10 +457,3 @@ void BIinitAssembler()
 {
   BIaddSpec(biSpec);
 }
-
-
-
-
-
-
-
