e415c042fe275288496d4d47eea53e9dfd8b06a8
mehl <mehl@ada56829-ad1f-0410-b00f-83cda6628aec>
1997-12-19 15:49:32 +0000

    old header removed

--- platform/emulator/builtins.hh
+++ platform/emulator/builtins.hh
@@ -25,14 +25,6 @@
  *  WARRANTIES.
  *
  */
-/*
-  Hydra Project, DFKI Saarbruecken,
-  Stuhlsatzenhausweg 3, D-66123 Saarbruecken, Phone (+49) 681 302-5312
-  Author: mehl
-  Last modified: $Date$ from $Author$
-  Version: $Revision$
-  State: $State$
-  */
 
 #ifndef __BUILTINSH
 #define __BUILTINSH
