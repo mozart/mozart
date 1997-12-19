8a42361798231b85018444d4064c675f8bd222e9
mehl <mehl@ada56829-ad1f-0410-b00f-83cda6628aec>
1997-12-19 15:49:32 +0000

    old header removed
    git-svn-id: https://gforge.info.ucl.ac.be/svn/mozart@4447 ada56829-ad1f-0410-b00f-83cda6628aec

--- platform/emulator/perdio_debug.hh
+++ platform/emulator/perdio_debug.hh
@@ -23,19 +23,6 @@
  *  WARRANTIES.
  *
  */
-/*
-  Perdio Project, DFKI & SICS,
-  Universit"at des Saarlandes
-  Postfach 15 11 59, D-66041 Saarbruecken, Phone (+49) 681 302-5312
-  SICS
-  Box 1263, S-16428 Sweden, Phone (+46) 8 7521500
-  Author: perbrand, scheidhr, mehl
-  Last modified: $Date$ from $Author$
-  Version: $Revision$
-  State: $State$
-
-  ------------------------------------------------------------------------
-*/
 
 #ifndef __PERDIO_DEBUG_HH
 #define __PERDIO_DEBUG_HH
