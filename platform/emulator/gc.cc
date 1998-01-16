eebd58cf7ddaee18502ef1112636e6a85baf5611
duchier <duchier@ada56829-ad1f-0410-b00f-83cda6628aec>
1998-01-16 18:30:54 +0000

    christian added a couple of assertions

--- platform/emulator/gc.cc
+++ platform/emulator/gc.cc
@@ -2073,11 +2073,15 @@ update:
       
       Board * bb = tagged2VarHome(aux)->derefBoard();
       
+      Assert(bb);
+
       if (opMode == IN_TC && !isLocalBoard(bb)) {
 	to = makeTaggedRef(aux_ptr);
       } else {
 	(void) bb->gcDerefedBoard();
       
+	Assert(bb->gcIsAlive());
+
 	updateStack.push(&to);
       
 	to = makeTaggedRefToFromSpace(aux_ptr);
