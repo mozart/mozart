a4c37c10c98d3bdb3883db54083e7b55cd84bbb4
scheidhr <scheidhr@ada56829-ad1f-0410-b00f-83cda6628aec>
1998-01-23 09:41:54 +0000

    make bootstrapping a bit easier

---
+++ share/lib/ozbatch.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#
+# use this for bootstrapping the system:
+#
+#    make OZBATCH= ./ozbatch.sh
+#
+
+
+: ${OZEMULATOR=$HOME/Oz/Emulator/oz.emulator.bin}
+
+exec $OZEMULATOR -b ./ozbatch.ozm -a "$@"
