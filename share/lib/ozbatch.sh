2007e1ce59a4d81c641f93ff648596eeef724415
kornstae <kornstae@ada56829-ad1f-0410-b00f-83cda6628aec>
1998-01-27 11:40:11 +0000

    Added -quiet to emulator arguments.

--- share/lib/ozbatch.sh
+++ share/lib/ozbatch.sh
@@ -2,10 +2,9 @@
 #
 # use this for bootstrapping the system:
 #
-#    make OZBATCH= ./ozbatch.sh
+#    make OZBATCH=./ozbatch.sh
 #
-
 
 : ${OZEMULATOR=$HOME/Oz/Emulator/oz.emulator.bin}
 
-exec $OZEMULATOR -b ./ozbatch.ozm -a "$@"
+exec $OZEMULATOR -quiet -b ./ozbatch.ozm -a "$@"
