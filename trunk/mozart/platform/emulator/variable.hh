2975dd1a518bff471bebbffd87233c4010b332b8
schulte <schulte@ada56829-ad1f-0410-b00f-83cda6628aec>
1998-02-16 18:43:23 +0000

    Removed deallocation upon failure, was just an experiment. Turned out that it is not worth it.
    git-svn-id: https://gforge.info.ucl.ac.be/svn/mozart@4996 ada56829-ad1f-0410-b00f-83cda6628aec

--- trunk/mozart/platform/emulator/variable.hh
+++ trunk/mozart/platform/emulator/variable.hh
@@ -78,8 +78,6 @@ public:
 
   SVariable(Board * h) : suspList(NULL), home(h) {}
 
-  Bool dealloc(void);
-
   void dispose(void) {
     suspList->disposeList();
     freeListDispose(this,sizeof(*this));
