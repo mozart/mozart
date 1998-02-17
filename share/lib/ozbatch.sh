adbffaebc298d2ccf89c2db8f405a823fac1d219
scheidhr <scheidhr@ada56829-ad1f-0410-b00f-83cda6628aec>
1998-02-17 13:03:48 +0000

    use ../bin/ozplatform instead of platform

--- share/lib/ozbatch.sh
+++ share/lib/ozbatch.sh
@@ -5,10 +5,13 @@
 #    make OZBATCH=./ozbatch.sh
 #
 
+: ${SRCDIR=.}
+: ${OZPLATFORM=`$SRCDIR/../bin/ozplatform`}
+
 if test -z "$OZ_EMULATOR_DIR"
 then
-    for d in ../Emulator ../Emulator/`platform` \
-            /usr/local/oz/platform/`platform`
+    for d in ../Emulator ../Emulator/$OZPLATFORM \
+            /usr/local/oz/platform/$OZPLATFORM
     do
         if test -r $d/ozma.so && test -x $d/oz.emulator.bin
         then
@@ -21,7 +24,6 @@ fi
 : ${OZEMULATOR=$OZ_EMULATOR_DIR/oz.emulator.bin}
 : ${OZMA_LIB="-B $OZ_EMULATOR_DIR/ozma.so"}
 : ${OZQUIET=-quiet}
-: ${SRCDIR=.}
 OZINIT=${OZMAINIT}
 export OZINIT
 
