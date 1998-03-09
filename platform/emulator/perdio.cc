f4ed7d386f8ce7b04e14b9bf3acd481fd916c472
erik <erik@ada56829-ad1f-0410-b00f-83cda6628aec>
1998-03-09 18:53:42 +0000

    Memory And credit recovery in portsend of noogods tmp removed

--- platform/emulator/perdio.cc
+++ platform/emulator/perdio.cc
@@ -3887,11 +3887,12 @@ OZ_Return portSend(Tertiary *p, TaggedRef msg, Thread *th) {
 
   OZ_Term nogoods = bs->getNoGoods();
   if (!literalEq(nil(),nogoods)) {
+  /*
     int portIndex;
     OZ_Term t;
     unmarshal_M_PORT_SEND(bs,portIndex,t);
     dumpRemoteMsgBuffer(bs);
-
+    */
     return oz_raise(E_ERROR,OZ_atom("dp"),"portSend",3,
                     oz_atom("nogoods"),
                     makeTaggedTert(p),
