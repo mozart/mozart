993e17c1827ecad515a3ae100c04249385c00962
scheidhr <scheidhr@ada56829-ad1f-0410-b00f-83cda6628aec>
1998-03-25 14:50:59 +0000

    exporting noggods gives more hints
    git-svn-id: https://gforge.info.ucl.ac.be/svn/mozart@5496 ada56829-ad1f-0410-b00f-83cda6628aec

--- trunk/mozart/platform/emulator/perdio.cc
+++ trunk/mozart/platform/emulator/perdio.cc
@@ -3732,7 +3732,7 @@ OZ_Return remoteSend(Tertiary *p, char *biName, TaggedRef msg) {
   MsgBuffer *bs = msgBufferManager->getMsgBuffer(site);
   b->getOneMsgCredit();
   marshal_M_REMOTE_SEND(bs,index,biName,msg);
-  CheckNogoods(bs,"send",);
+  CheckNogoods(msg,bs,"send",);
   SendTo(site,bs,M_REMOTE_SEND,site,index);
   return PROCEED;
 }
@@ -3891,7 +3891,7 @@ OZ_Return sendSurrender(BorrowEntry *be,OZ_Term val){
   NetAddress *na = be->getNetAddress();  
   MsgBuffer *bs=msgBufferManager->getMsgBuffer(na->site);
   marshal_M_SURRENDER(bs,na->index,mySite,val);
-  CheckNogoods(bs,"unify",);
+  CheckNogoods(val,bs,"unify",);
   SendTo(na->site,bs,M_SURRENDER,na->site,na->index);
   return PROCEED;
 }
@@ -3900,7 +3900,7 @@ OZ_Return sendRedirect(Site* sd,int OTI,TaggedRef val){
   MsgBuffer *bs=msgBufferManager->getMsgBuffer(sd);
   OT->getOwner(OTI)->getOneCreditOwner();
   marshal_M_REDIRECT(bs,mySite,OTI,val);
-  CheckNogoods(bs,"unify",);
+  CheckNogoods(val,bs,"unify",);
   SendTo(sd,bs,M_REDIRECT,mySite,OTI);
   return PROCEED;
 }
@@ -3939,10 +3939,11 @@ void PerdioVar::redirect(OZ_Term val) {
 OZ_Return sendRedirect(ProxyList *pl,OZ_Term val, Site* ackSite, int OTI){
   OZ_Return ret = PROCEED;
   OwnerEntry *oe = OT->getOwner(OTI);
-  if (pl==NULL && !oe->hasFullCredit()) {
+  //  if (pl==NULL && !oe->hasFullCredit()) {
+  if (pl==NULL) {
     MsgBuffer *bs=msgBufferManager->getMsgBuffer(NULL);
     marshal_M_REDIRECT(bs,mySite,OTI,val);
-    CheckNogoods(bs,"unify",);
+    CheckNogoods(val,bs,"unify",);
   }
   while (pl) {
     Site* sd=pl->sd;
