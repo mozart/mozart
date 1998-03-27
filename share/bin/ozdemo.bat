b676eb423d55f5ca3bc3d090c2cead24145e9391
scheidhr <scheidhr@ada56829-ad1f-0410-b00f-83cda6628aec>
1998-03-27 08:11:11 +0000

    no longer needed
    git-svn-id: https://gforge.info.ucl.ac.be/svn/mozart@5529 ada56829-ad1f-0410-b00f-83cda6628aec

--- share/bin/ozdemo.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-rem gnu-win32 cannot understand real dos names starting with C:\...  
-echo Starting Oz demo.
-echo This may take some time...
-rem %OZHOME%\platform\win32-i486\ozemulator -f - < %OZHOME%\demo\rundemo
-%OZHOME%\platform\win32-i486\ozemulator -f %OZHOME%\demo\rundemo
-
