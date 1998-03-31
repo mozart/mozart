2da96ae4a832a28d673bb8478a2d8ef63466d74f
duchier <duchier@ada56829-ad1f-0410-b00f-83cda6628aec>
1998-03-31 12:05:07 +0000

    updated regex/posix to OZ_BI_define and Foreign.load

--- contrib/regex/posix.cc
+++ contrib/regex/posix.cc
@@ -12,6 +12,10 @@
 #define PREFIX(m) regex_ ## m
 #endif
 
+#ifndef SPREFIX
+#define SPREFIX(m) "regex_" # m
+#endif
+
 #ifndef ERRBUFSIZE
 #define ERRBUFSIZE 200
 #endif
@@ -31,31 +35,29 @@ OZ_Return re_error(int code,regex_t* re,int must_free)
   return OZ_raiseErrorC(MODULE,1,OZ_string(errbuf));
 }
 
-OZ_C_proc_begin(PREFIX(compile),3)
+OZ_BI_define(PREFIX(compile),2,1)
 {
-  OZ_declareVirtualStringArg(0,RE);
-  OZ_declareIntArg(1,CFLAGS);
-  OZ_declareArg(2,RESULT);
+  OZ_declareVirtualStringIN(0,RE);
+  OZ_declareIntIN(1,CFLAGS);
   int errcode;
   regex_t *preg = new regex_t;
   errcode = regcomp(preg,RE,CFLAGS);
   if (errcode) return re_error(errcode,preg,1);
-  return OZ_unify(RESULT,OZ_makeForeignPointer((void*)preg));
+  OZ_RETURN(OZ_makeForeignPointer((void*)preg));
 }
-OZ_C_proc_end
+OZ_BI_end
 
-OZ_C_proc_begin(PREFIX(execute),5)
+OZ_BI_define(PREFIX(execute),4,1)
 {
-  OZ_declareForeignPointerArg(0,RE);
-  OZ_declareVirtualStringArg(1,TXT);
-  OZ_declareIntArg(2,IDX);
-  OZ_declareIntArg(3,EFLAGS);
-  OZ_declareArg(4,RESULT);
+  OZ_declareForeignPointerIN(0,RE);
+  OZ_declareVirtualStringIN(1,TXT);
+  OZ_declareIntIN(2,IDX);
+  OZ_declareIntIN(3,EFLAGS);
   regex_t *preg = (regex_t*) RE;
   int errcode;
   regmatch_t pmatch[MAXMATCH];
   errcode = regexec(preg,&(TXT[IDX]),preg->re_nsub+1,pmatch,EFLAGS);
-  if (errcode==REG_NOMATCH) return OZ_unify(RESULT,OZ_false());
+  if (errcode==REG_NOMATCH) OZ_RETURN(OZ_false());
   if (errcode) return re_error(errcode,preg,0);
   OZ_Term tuple;
   tuple = OZ_tupleC("match",preg->re_nsub);
@@ -64,19 +66,19 @@ OZ_C_proc_begin(PREFIX(execute),5)
                                  OZ_int(pmatch[i].rm_eo+IDX)));
   tuple = OZ_adjoinAt(tuple,OZ_int(0),OZ_pair2(OZ_int(pmatch[0].rm_so+IDX),
                                                OZ_int(pmatch[0].rm_eo+IDX)));
-  return OZ_unify(RESULT,tuple);
+  OZ_RETURN(tuple);
 }
-OZ_C_proc_end
+OZ_BI_end
 
-OZ_C_proc_begin(PREFIX(free),1)
+OZ_BI_define(PREFIX(free),1,0)
 {
-  OZ_declareForeignPointerArg(0,RE);
+  OZ_declareForeignPointerIN(0,RE);
   delete (regex_t*) RE;
   return PROCEED;
 }
-OZ_C_proc_end
+OZ_BI_end
 
-OZ_C_proc_begin(PREFIX(flags),1)
+OZ_BI_define(PREFIX(flags),0,1)
 {
   OZ_Term tuple;
   tuple = OZ_tupleC("flag",6);
@@ -86,6 +88,14 @@ OZ_C_proc_begin(PREFIX(flags),1)
   OZ_putArg(tuple,3,OZ_int(REG_NOSUB   ));
   OZ_putArg(tuple,4,OZ_int(REG_NOTBOL  ));
   OZ_putArg(tuple,5,OZ_int(REG_NOTEOL  ));
-  return OZ_unify(OZ_getCArg(0),tuple);
+  OZ_RETURN(tuple);
 }
-OZ_C_proc_end
+OZ_BI_end
+
+OZ_C_proc_interface oz_interface[] = {
+  {SPREFIX(compile),2,1},
+  {SPREFIX(execute),4,1},
+  {SPREFIX(free),1,0},
+  {SPREFIX(flags),0,1},
+  {0,0,0}
+};
