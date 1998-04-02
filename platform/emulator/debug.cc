277a414165f5ef16eec40be29b665331d1ef1021
lorenz <lorenz@ada56829-ad1f-0410-b00f-83cda6628aec>
1998-04-02 14:08:17 +0000

    edited copyright info reformatted

--- platform/emulator/debug.cc
+++ platform/emulator/debug.cc
@@ -1,36 +1,26 @@
 /*
  *  Authors:
- *    Leif Kornstaedt (kornstae@ps.uni-sb.de)
- *    Benjamin Lorenz (lorenz@ps.uni-sb.de)
- *    Michael Mehl (mehl@dfki.de)
- *    Ralf Scheidhauer (Ralf.Scheidhauer@ps.uni-sb.de)
- *
- *  Contributors:
- *    optional, Contributor's name (Contributor's email address)
+ *    Leif Kornstaedt <kornstae@ps.uni-sb.de>
+ *    Benjamin Lorenz <lorenz@ps.uni-sb.de>
+ *    Michael Mehl <mehl@dfki.de>
+ *    Ralf Scheidhauer <Ralf.Scheidhauer@ps.uni-sb.de>
  *
  *  Copyright:
- *    Organization or Person (Year(s))
+ *    Benjamin Lorenz <lorenz@ps.uni-sb.de>
  *
  *  Last change:
  *    $Date$ by $Author$
  *    $Revision$
  *
- *  This file is part of Mozart, an implementation
- *  of Oz 3:
+ *  This file is part of Mozart, an implementation of Oz 3:
  *     $MOZARTURL$
  *
  *  See the file "LICENSE" or
  *     $LICENSEURL$
  *  for information on usage and redistribution
- *  of this file, and for a DISCLAIMER OF ALL
- *  WARRANTIES.
+ *  of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
  */
-
-/*
- *  famous debug support
- */
-
 
 #if defined(INTERFACE) && !defined(PEANUTS)
 #pragma implementation "debug.hh"
