f278263d3808397ed1fba8dbe96c038c1d84021b
tmueller <tmueller@ada56829-ad1f-0410-b00f-83cda6628aec>
1998-06-19 12:48:59 +0000

    improved builtin `getConstraints`
    git-svn-id: https://gforge.info.ucl.ac.be/svn/mozart@6332 ada56829-ad1f-0410-b00f-83cda6628aec

--- platform/emulator/cpi.cc
+++ platform/emulator/cpi.cc
@@ -68,21 +68,35 @@ OZ_Return constraintsSuspendOnVar(OZ_CFun f, int a, OZ_Term * x,
 #undef inline
 #endif
 
-static OZ_Term var_atom     = OZ_atom("var");  
-static OZ_Term any_atom     = OZ_atom("any");  
-static OZ_Term type_atom    = OZ_atom("type");  
-static OZ_Term fd_atom      = OZ_atom("fd");  
-static OZ_Term fs_atom      = OZ_atom("fs");  
-static OZ_Term bool_atom    = OZ_atom("bool");  
-static OZ_Term bounds_atom  = OZ_atom("bounds");  
-static OZ_Term val_atom     = OZ_atom("val");  
-static OZ_Term glb_atom     = OZ_atom("glb");  
-static OZ_Term lub_atom     = OZ_atom("lub");  
-static OZ_Term ask_atom     = OZ_atom("ask actor");  
-static OZ_Term wait_atom    = OZ_atom("wait actor");  
-static OZ_Term waittop_atom = OZ_atom("waittop actor");  
-static OZ_Term oops_atom    = OZ_atom("oops");  
+static OZ_Term atom_var     = OZ_atom("var");  
+static OZ_Term atom_any     = OZ_atom("any");  
+static OZ_Term atom_type    = OZ_atom("type");  
+static OZ_Term atom_fd      = OZ_atom("fd");  
+static OZ_Term atom_fs      = OZ_atom("fs");  
+static OZ_Term atom_bool    = OZ_atom("bool");  
+static OZ_Term atom_bounds  = OZ_atom("bounds");  
+static OZ_Term atom_val     = OZ_atom("val");  
+static OZ_Term atom_glb     = OZ_atom("glb");  
+static OZ_Term atom_lub     = OZ_atom("lub");  
+static OZ_Term atom_flat    = OZ_atom("flat actor");  
+static OZ_Term atom_local   = OZ_atom("home");  
+static OZ_Term atom_ask     = OZ_atom("ask actor");  
+static OZ_Term atom_wait    = OZ_atom("wait actor");  
+static OZ_Term atom_waittop = OZ_atom("waittop actor");  
+static OZ_Term atom_oops    = OZ_atom("oops");  
+static OZ_Term atom_prop    = OZ_atom("propagator");  
+static OZ_Term atom_params  = OZ_atom("params");  
+static OZ_Term atom_name    = OZ_atom("name");  
+static OZ_Term atom_space   = OZ_atom("space");  
+static OZ_Term atom_susp    = OZ_atom("suspension");  
+static OZ_Term atom_thread  = OZ_atom("thread");  
   
+#define MKARITY(Arity, ArityDef)			\
+OZ_Term Arity = OZ_nil();				\
+for (int i = 0; ArityDef[i] != (OZ_Term) 0; i += 1)	\
+  Arity = OZ_cons(ArityDef[i], Arity);
+
+
 OZ_Term getConstraintsList(SuspList * sl) 
 {
   OZ_Term cl = OZ_nil();
@@ -90,27 +104,58 @@ OZ_Term getConstraintsList(SuspList * sl)
   for (SuspList * p = sl; p != NULL; p = p->getNext()) {
     Suspension susp = p->getSuspension();
     
-    OZ_Term r = oops_atom;
+    OZ_Term r = atom_oops;
 
     if (susp.isPropagator()) {
       OZ_Propagator * p = susp.getPropagator()->getPropagator();
+      Board * b = GETBOARD(susp.getPropagator());
+
+      OZ_Term space = atom_oops;
+      if (b == am.currentBoard()) {
+	space = atom_local;
+      } else if (b->isAsk()) {
+	space = atom_ask;
+      } else if (b->isWait()) {
+	space = atom_wait;
+      } else if (b->isWaitTop()) {
+	space = atom_waittop;
+      }
+
+      OZ_Term arity_def[] = {
+	{OZ_pair2(atom_type, atom_prop)},
+	{OZ_pair2(atom_params, p->getParameters())},
+	{OZ_pair2(atom_name, OZ_atom(p->getProfile()->getPropagatorName()))},
+	{OZ_pair2(atom_space, space)},
+	{(OZ_Term) 0}
+      };
       
-      const char * func_name = p->getProfile()->getPropagatorName();
-      OZ_Term params = p->getParameters();
-
-      r = OZ_pair2(OZ_atom(func_name), params);
-
+      MKARITY(arity, arity_def);
+      
+      r = OZ_recordInit(atom_susp, arity);
     } else {
       Board * b = GETBOARD(susp.getThread());
+      OZ_Term space = atom_oops;
       
-      if (b->isAsk()) {
-	r = ask_atom;
+      if (b == am.currentBoard()) {
+	space = atom_flat;
+      } else if (b->isAsk()) {
+	space = atom_ask;
       } else if (b->isWait()) {
-	r = wait_atom;
+	space = atom_wait;
       } else if (b->isWaitTop()) {
-	r = waittop_atom;
+	space = atom_waittop;
       }
  
+      OZ_Term arity_def[] = {
+	{OZ_pair2(atom_type, atom_thread)},
+	{OZ_pair2(atom_space, space)},
+	{(OZ_Term) 0}
+      };
+      
+      MKARITY(arity, arity_def);
+      
+      r = OZ_recordInit(atom_susp, arity);
+
     }
 
     cl = OZ_cons(r, cl);
@@ -118,12 +163,6 @@ OZ_Term getConstraintsList(SuspList * sl)
 
   return cl;
 }
-
-#define mkArity(Arity, ArityDef)			\
-OZ_Term Arity = OZ_nil();				\
-for (int i = 0; ArityDef[i] != (OZ_Term) 0; i += 1)	\
-  Arity = OZ_cons(ArityDef[i], Arity);
-
 
 OZ_C_proc_begin(BIgetConstraints, 2)
 {
@@ -139,65 +178,63 @@ OZ_C_proc_begin(BIgetConstraints, 2)
 
   if (isNotCVar(vartag)) {
     OZ_Term arity_def[] = {
-      {OZ_pair2(var_atom,  var_itself)},
-      {OZ_pair2(type_atom, any_atom)},      
+      {OZ_pair2(atom_var,  var_itself)},
+      {OZ_pair2(atom_type, atom_any)},      
+      {OZ_pair2(atom_any, (isSVar(vartag) ? 
+			   getConstraintsList(tagged2SVar(var)->getSuspList()) 
+			   : OZ_nil()))},      
       {(OZ_Term) 0}
     };
     
-    mkArity(arity, arity_def);
+    MKARITY(arity, arity_def);
 
-    if (isSVar(vartag)) {
-      arity = OZ_cons(OZ_pair2(any_atom, 
-			       getConstraintsList(tagged2SVar(var)->getSuspList())), arity);
-    }
-
-    r = OZ_recordInit(var_atom, arity);
+    r = OZ_recordInit(atom_var, arity);
   } else if (isGenFDVar(var,vartag)) {
     OZ_Term arity_def[] = {
-      {OZ_pair2(var_atom,  var_itself)},
-      {OZ_pair2(type_atom, fd_atom)},      
-      {OZ_pair2(any_atom, 
+      {OZ_pair2(atom_var,  var_itself)},
+      {OZ_pair2(atom_type, atom_fd)},      
+      {OZ_pair2(atom_any, 
 		getConstraintsList(tagged2CVar(var)->getSuspList()))},
-      {OZ_pair2(bounds_atom, 
+      {OZ_pair2(atom_bounds, 
 		getConstraintsList(tagged2GenFDVar(var)->getSuspList(fd_prop_bounds)))},
-      {OZ_pair2(val_atom, 
+      {OZ_pair2(atom_val, 
 		getConstraintsList(tagged2GenFDVar(var)->getSuspList(fd_prop_singl)))},
       {(OZ_Term) 0}
     };
     
-    mkArity(arity, arity_def);
+    MKARITY(arity, arity_def);
 
-    r = OZ_recordInit(var_atom, arity);
+    r = OZ_recordInit(atom_var, arity);
   } else if (isGenBoolVar(var,vartag)) {
     OZ_Term arity_def[] = {
-      {OZ_pair2(var_atom,  var_itself)},
-      {OZ_pair2(type_atom, bool_atom)},      
-      {OZ_pair2(any_atom, 
+      {OZ_pair2(atom_var,  var_itself)},
+      {OZ_pair2(atom_type, atom_bool)},      
+      {OZ_pair2(atom_any, 
 		getConstraintsList(tagged2CVar(var)->getSuspList()))},
       {(OZ_Term) 0}
     };
     
-    mkArity(arity, arity_def);
+    MKARITY(arity, arity_def);
 
-    r = OZ_recordInit(var_atom, arity);
+    r = OZ_recordInit(atom_var, arity);
   } else if (isGenFSetVar(var,vartag)) {
     OZ_Term arity_def[] = {
-      {OZ_pair2(var_atom,  var_itself)},
-      {OZ_pair2(type_atom, fs_atom)},      
-      {OZ_pair2(any_atom, 
+      {OZ_pair2(atom_var,  var_itself)},
+      {OZ_pair2(atom_type, atom_fs)},     
+      {OZ_pair2(atom_any, 
 		getConstraintsList(tagged2CVar(var)->getSuspList()))},
-      {OZ_pair2(glb_atom, 
+      {OZ_pair2(atom_glb, 
 		getConstraintsList(tagged2GenFSetVar(var)->getSuspList(fs_prop_glb)))},
-      {OZ_pair2(lub_atom, 
+      {OZ_pair2(atom_lub, 
 		getConstraintsList(tagged2GenFSetVar(var)->getSuspList(fs_prop_lub)))},
-      {OZ_pair2(val_atom, 
+      {OZ_pair2(atom_val, 
 		getConstraintsList(tagged2GenFSetVar(var)->getSuspList(fs_prop_val)))},
       {(OZ_Term) 0}
     };
     
-    mkArity(arity, arity_def);
+    MKARITY(arity, arity_def);
 
-    r = OZ_recordInit(var_atom, arity);
+    r = OZ_recordInit(atom_var, arity);
   } 
 
 #ifdef DEBUG_CHECK
