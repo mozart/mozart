c6c194ec8afa1ea10f224e407aad13ee46bdd8a6
duchier <duchier@ada56829-ad1f-0410-b00f-83cda6628aec>
1998-07-29 14:40:16 +0000

    t2p.sh p2t.sh: text2pickle and pickle2text that works locally for booting boot-ozc.sh: run the boot compiler with boot init file boot-init.txt:	text pickle version of boot init file boot-ozc.txt:	text pickle version of boot compiler

---
+++ share/lib/boot-ozc.sh
@@ -0,0 +1,42 @@
+#! /bin/sh
+#
+: ${BUILDTOP=../..}
+: ${SRCDIR=.}
+: ${SRCTOP=$SRCDIR/../..}
+: ${OZPLATFORM=`$SRCTOP/share/bin/ozplatform`}
+: ${OZPREFIX=/usr/local/oz}
+: ${OZBOOTINIT="$BUILDTOP/share/lib/boot-init"}
+: ${OZBOOTOZC="$BUILDTOP/share/lib/boot-ozc"}
+
+if test -z "$OZEMULATOR"
+then
+    for d in \
+        $BUILDTOP/platform/emulator \
+        $BUILDTOP/platform/emulator/$OZPLATFORM \
+        $OZPREFIX/platform/$OZPLATFORM
+    do
+        if test -x $d/oz.emulator.bin
+        then
+            OZEMULATOR=$d/oz.emulator.bin
+            break
+        fi
+    done
+fi
+
+if test -z "$OZEMULATOR"
+then
+    echo Fatal error: oz.emulator.bin not found
+    exit 1
+fi
+
+OZINIT=${OZMAINIT}
+export OZINIT
+
+if test "x$OZMAKE_VERBOSE" = xyes
+then
+    echo "Using OZEMULATOR: $OZEMULATOR"
+    echo "Using OZBOOTINIT: $OZBOOTINIT"
+    echo "Using OZBOOTOZC:  $OZBOOTOZC"
+fi
+
+exec $OZEMULATOR -init $OZBOOTINIT -u $OZBOOTOZC -- "$@"
