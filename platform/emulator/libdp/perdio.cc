306fc99764a1e95dec0bd68bb8ec38db8607cb0a
erik <erik@ada56829-ad1f-0410-b00f-83cda6628aec>
1998-09-24 07:32:01 +0000

    SPP model: marshaling info for dp

--- platform/emulator/libdp/perdio.cc
+++ platform/emulator/libdp/perdio.cc
@@ -52,6 +52,7 @@
 #include "fail.hh"
 #include "protocolCredit.hh"
 #include "port.hh"
+#include "dpResource.hh"
 #include "protocolState.hh"
 #include "protocolFail.hh"
 #include "dpMarshaler.hh"
@@ -243,6 +244,7 @@ void gcPerdioFinal()
   if (isPerdioInitialized()) {
     BT->gcBorrowTableFinal();
     OT->gcOwnerTableFinal();
+    RHT->gcResourceTable();
     gcDSiteTable();
   }
 }
@@ -1073,6 +1075,7 @@ void initPerdio()
   creditSiteOut = NULL;
   ownerTable = new OwnerTable(DEFAULT_OWNER_TABLE_SIZE);
   borrowTable = new BorrowTable(DEFAULT_BORROW_TABLE_SIZE);
+  resourceTable = new ResourceHashTable(RESOURCE_HASH_TABLE_DEFAULT_SIZE);
   msgBufferManager = new MsgBufferManager();
 
   BI_probe = makeTaggedConst(new Builtin("probe", 1, 0, BIprobe, OK));
