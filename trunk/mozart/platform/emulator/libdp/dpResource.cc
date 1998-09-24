b6e2a6b3c3be232c5a68a89bf372e072abf3621d
erik <erik@ada56829-ad1f-0410-b00f-83cda6628aec>
1998-09-24 07:30:41 +0000

    SPP resource stuff
    git-svn-id: https://gforge.info.ucl.ac.be/svn/mozart@8157 ada56829-ad1f-0410-b00f-83cda6628aec

---
+++ trunk/mozart/platform/emulator/libdp/dpResource.cc
@@ -0,0 +1,83 @@
+/*
+ *  Authors:
+ *    Erik Klintskog (erik@sics.se)
+ * 
+ *  Contributors:
+ *    optional, Contributor's name (Contributor's email address)
+ * 
+ *  Copyright:
+ *    Organization or Person (Year(s))
+ * 
+ *  Last change:
+ *    $Date$ by $Author$
+ *    $Revision$
+ * 
+ *  This file is part of Mozart, an implementation 
+ *  of Oz 3:
+ *     http://mozart.ps.uni-sb.de
+ * 
+ *  See the file "LICENSE" or
+ *     http://mozart.ps.uni-sb.de/LICENSE.html
+ *  for information on usage and redistribution 
+ *  of this file, and for a DISCLAIMER OF ALL 
+ *  WARRANTIES.
+ *
+ */
+
+#if defined(INTERFACE)
+#pragma implementation "dpResource.hh"
+#endif
+
+#include "base.hh"
+#include "builtins.hh"
+#include "value.hh"
+#include "dpBase.hh"
+#include "perdio.hh"
+#include "table.hh"
+#include "controlvar.hh"
+#include "dpMarshaler.hh"
+#include "dpInterface.hh"
+#include "dpResource.hh"
+
+ResourceHashTable *resourceTable;
+
+/* 
+   This must be done after the ownerTable has
+   been gced. The resource will stay in the resource table
+   if there is an entry for it in the ownertable.
+   */
+
+void ResourceHashTable::gcResourceTable(){
+  PD((GC,"Resource gc"));
+  int index;
+  int  OTI;
+  OwnerEntry *oe;
+  GenHashNode *aux = getFirst(index);
+  while (aux!=NULL) {
+    TaggedRef entity = (TaggedRef) aux->getBaseKey();
+    if(!oz_isBuiltin(entity)){
+      entity = oz_deref(entity);
+      OTI =  (int) aux->getEntry();
+      if(htSub(index,aux)) ;
+      oe = OT->getEntry(OTI);
+      if(oe && oe->getRef()==entity)
+	add(entity, OTI);}
+    else{
+      OTI =  (int) aux->getEntry();
+      oe = OT->getEntry(OTI);
+      if(oe==NULL || oe->getRef()!=entity)
+	if(htSub(index,aux)) ;
+    }
+    aux = getNext(aux,index);
+  }
+}
+
+ConstTerm* gcDistResource(ConstTerm* term){
+  term = (ConstTerm *) gcRealloc((void*)term,sizeof(DistResource));
+  gcProxyRecurse((Tertiary *)term);
+  return term;
+}
+
+
+
+
