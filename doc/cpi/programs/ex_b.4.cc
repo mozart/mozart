ba393a09c62166db54a26acccb9222a91f71a1f9
tmueller <tmueller@ada56829-ad1f-0410-b00f-83cda6628aec>
1998-10-30 07:59:24 +0000

    program examples for cpi tutorial

---
+++ doc/cpi/programs/ex_b.4.cc
@@ -0,0 +1,13 @@
+OZ_C_proc_begin(fd_twice, 2)
+{
+  OZ_EXPECTED_TYPE(OZ_EM_FD","OZ_EM_FD);
+
+  OZ_Expect pe;
+
+  OZ_EXPECT(pe, 0, expectIntVar);
+  OZ_EXPECT(pe, 1, expectIntVar);
+
+  return pe.impose(new TwiceProp(OZ_args[0],
+                                 OZ_args[1]));
+}
+OZ_C_proc_end
