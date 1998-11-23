054e38517d1b18e817307a0c53c189e3e59e3c4d
tmueller <tmueller@ada56829-ad1f-0410-b00f-83cda6628aec>
1998-11-23 16:44:12 +0000

    improvement on the way
    git-svn-id: https://gforge.info.ucl.ac.be/svn/mozart@9570 ada56829-ad1f-0410-b00f-83cda6628aec

--- doc/cpitut/programs/ozpropdef.hh
+++ doc/cpitut/programs/ozpropdef.hh
@@ -1,9 +1,9 @@
 class OZ_Propagator {
 public:
   OZ_Propagator(void);
-  virtual OZ_Term getParameters(void) const =0;
-  virtual size_t sizeOf(void)               =0;
-  virtual void updateHeapRefs(void)         =0;
-  virtual OZ_Return propagate(void)         =0;
-  virtual OZ_PropagatorProfile *getProfile(void) const =0;
+  virtual OZ_Term getParameters(void) const             = 0;
+  virtual size_t sizeOf(void)                           = 0;
+  virtual void updateHeapRefs(void)                     = 0;
+  virtual OZ_Return propagate(void)                     = 0;
+  virtual OZ_PropagatorProfile * getProfile(void) const = 0;
 }
