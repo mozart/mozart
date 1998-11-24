6004c9ef57162544922ac4cccea4580107c0bdb1
tmueller <tmueller@ada56829-ad1f-0410-b00f-83cda6628aec>
1998-11-24 15:57:57 +0000

    adding stuff
    git-svn-id: https://gforge.info.ucl.ac.be/svn/mozart@9624 ada56829-ad1f-0410-b00f-83cda6628aec

---
+++ doc/cpitut/programs/sync.class.cc
@@ -0,0 +1,29 @@
+class ConnectProp : public OZ_Propagator {
+private:
+  static OZ_PropagatorProfile profile;
+protected:
+  OZ_Term _fs;
+  OZ_Term _fd;
+public:
+  ConnectProp(OZ_Term fsvar, OZ_Term fdvar) 
+    : _fs(fsvar), _fd(fdvar)  {}
+ 
+  virtual void updateHeapRefs(OZ_Boolean) {
+    OZ_updateHeapTerm(_fd);
+    OZ_updateHeapTerm(_fs);
+  }
+
+  virtual size_t sizeOf(void) {
+    return sizeof(ConnectProp);
+  }
+
+  virtual OZ_Term getParameters(void) const {
+    return OZ_cons(_fs, (OZ_cons(_fd, OZ_nil())));
+  }
+
+  virtual OZ_PropagatorProfile *getProfile(void) const {
+    return &profile;
+  }
+
+  virtual OZ_Return propagate();
+};
