c50f882f63aef655de07f340b88fa52302a9dab1
mehl <mehl@ada56829-ad1f-0410-b00f-83cda6628aec>
1998-11-25 13:35:37 +0000

    some copyrights updated old files removed

--- platform/emulator/text2pickle.cc
+++ platform/emulator/text2pickle.cc
@@ -1,3 +1,28 @@
+/*
+ *  Authors:
+ *    Ralf Scheidhr (scheidhr@ps.uni-sb.de)
+ *
+ *  Contributors:
+ *
+ *  Copyright:
+ *    Organization or Person (Year(s))
+ *
+ *  Last change:
+ *    $Date$ by $Author$
+ *    $Revision$
+ *
+ *  This file is part of Mozart, an implementation
+ *  of Oz 3:
+ *     http://mozart.ps.uni-sb.de
+ *
+ *  See the file "LICENSE" or
+ *     http://mozart.ps.uni-sb.de/LICENSE.html
+ *  for information on usage and redistribution
+ *  of this file, and for a DISCLAIMER OF ALL
+ *  WARRANTIES.
+ *
+ */
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdio.h>
