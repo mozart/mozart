5d19ef64b4c41d5d12173631379cc437f21ab529
scheidhr <scheidhr@ada56829-ad1f-0410-b00f-83cda6628aec>
1998-11-28 18:10:42 +0000

    oztool replaces ozdynwinld

--- platform/mswindows/ozwrapper.cc
+++ platform/mswindows/ozwrapper.cc
@@ -1,4 +1,31 @@
 /*
+ *  Authors:
+ *    Ralf Scheidhauer (scheidhr@dfki.de)
+ *
+ *  Contributors:
+ *    optional, Contributor's name (Contributor's email address)
+ *
+ *  Copyright:
+ *    Organization or Person (Year(s))
+ *
+ *  Last change:
+ *    $Date$ by $Author$
+ *    $Revision$
+ *
+ *  This file is part of Mozart, an implementation
+ *  of Oz 3:
+ *     http://mozart.ps.uni-sb.de
+ *
+ *  See the file "LICENSE" or
+ *     http://mozart.ps.uni-sb.de/LICENSE.html
+ *  for information on usage and redistribution
+ *  of this file, and for a DISCLAIMER OF ALL
+ *  WARRANTIES.
+ *
+ */
+
+
+/*
  * wrapper for syslets. Does something like
  *     "exec ozengine $0 $@"
  */
