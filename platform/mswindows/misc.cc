33c644d6ede6cbd57ac0188aff747c93a52464dd
scheidhr <scheidhr@ada56829-ad1f-0410-b00f-83cda6628aec>
1998-11-28 18:10:42 +0000

    oztool replaces ozdynwinld
    git-svn-id: https://gforge.info.ucl.ac.be/svn/mozart@9741 ada56829-ad1f-0410-b00f-83cda6628aec

--- platform/mswindows/misc.cc
+++ platform/mswindows/misc.cc
@@ -1,9 +1,35 @@
+/*
+ *  Authors:
+ *    Ralf Scheidhauer (scheidhr@dfki.de)
+ * 
+ *  Contributors:
+ *    optional, Contributor's name (Contributor's email address)
+ * 
+ *  Copyright:
+ *    Organization or Person (Year(s))
+ * 
+ *  Last change:
+ *    $Date$ by $Author$
+ *    $Revision$
+ * 
+ *  This file is part of Mozart, an implementation 
+ *  of Oz 3:
+ *     http://mozart.ps.uni-sb.de
+ * 
+ *  See the file "LICENSE" or
+ *     http://mozart.ps.uni-sb.de/LICENSE.html
+ *  for information on usage and redistribution 
+ *  of this file, and for a DISCLAIMER OF ALL 
+ *  WARRANTIES.
+ *
+ */
+
 #include <windows.h>
 #include <ddeml.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <process.h>
 #include <io.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <errno.h>
 
