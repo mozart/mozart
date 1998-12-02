9c7a44f40142516081e0ca1452175026e358f8cc
scheidhr <scheidhr@ada56829-ad1f-0410-b00f-83cda6628aec>
1998-12-02 10:29:52 +0000

    windowed versions of their friends
    git-svn-id: https://gforge.info.ucl.ac.be/svn/mozart@9887 ada56829-ad1f-0410-b00f-83cda6628aec

---
+++ trunk/mozart/platform/mswindows/ozenginew.cc
@@ -0,0 +1,29 @@
+/*
+ *  Authors:
+ *    Ralf Scheidhauer (scheidhr@dfki.de)
+ * 
+ *  Contributors:
+ *    optional, Contributor's name (Contributor's email address)
+ * 
+ *  Copyright:
+ *    Organization or Person (Year(s))
+ * 
+ *  Last change:
+ *    $Date$ by $Author$
+ *    $Revision$
+ * 
+ *  This file is part of Mozart, an implementation 
+ *  of Oz 3:
+ *     http://mozart.ps.uni-sb.de
+ * 
+ *  See the file "LICENSE" or
+ *     http://mozart.ps.uni-sb.de/LICENSE.html
+ *  for information on usage and redistribution 
+ *  of this file, and for a DISCLAIMER OF ALL 
+ *  WARRANTIES.
+ *
+ */
+
+#define OZENGINEW
+
+#include "oz.cc"
