57ee32019622402e60ceb1a9f0e1342807ea2bb2
popow <popow@ada56829-ad1f-0410-b00f-83cda6628aec>
1998-12-03 13:55:06 +0000

    timeout for remote servers has a default value which is vprop; some cleanups;
    git-svn-id: https://gforge.info.ucl.ac.be/svn/mozart@9941 ada56829-ad1f-0410-b00f-83cda6628aec

--- trunk/mozart/platform/emulator/libdp/network.cc
+++ trunk/mozart/platform/emulator/libdp/network.cc
@@ -1539,6 +1539,7 @@ case ETIMEDOUT:{
 default:{
   PD((TCP_HERROR,"Unhandled error: %d please inform erik@sics.se",
       ossockerrno()));
+  fprintf(stderr,"default interpreted as perm:%d \n",ossockerrno());
   return IP_PERM_BLOCK;}}
 return IP_TEMP_BLOCK;
 }
@@ -3148,6 +3149,7 @@ retry:
     return IP_TEMP_BLOCK;}
   if(ossockerrno() == ECONNREFUSED || ossockerrno() == EADDRNOTAVAIL       ){
     r->connectionLost();
+    fprintf(stderr,"cannot open - interpreted as perm:%d \n",ossockerrno());
     return IP_PERM_BLOCK;}
   addr.sin_port = htons(aport);
   goto retry;}
