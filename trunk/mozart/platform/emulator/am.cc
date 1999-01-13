1bb58aaaeeec0093743a7bc0fefde181bf3cc5c3
scheidhr <scheidhr@ada56829-ad1f-0410-b00f-83cda6628aec>
1999-01-13 14:37:33 +0000

    - alarm handler no longer does a lot of stuff (like calling   select(2)). It now just sets a flag, which is checked by the   emulator.
      I also removed a optimziation: if there is only one runnable thread
      and preemption should take place control was not handed to the
      scheduler.
    
    
    git-svn-id: https://gforge.info.ucl.ac.be/svn/mozart@10417 ada56829-ad1f-0410-b00f-83cda6628aec

--- trunk/mozart/platform/emulator/am.cc
+++ trunk/mozart/platform/emulator/am.cc
@@ -566,6 +566,9 @@ void AM::checkStatus(Bool block)
     osBlockSignals();
   }
 
+  unsetSFlag(TimerInterrupt);
+  handleAlarm(); // must be done first since it might set other flags
+
   if (isSetSFlag(StartGC))
     doGC();
 
@@ -744,7 +747,12 @@ void handlerCHLD(int)
 // Signal handler;
 void handlerALRM(int)
 {
-  am.handleAlarm(CLOCK_TICK/1000);
+  am.emulatorClock += CLOCK_TICK/1000;
+
+  if (am.isCritical()) /* wait for next ALRM signal */
+    return;
+
+  am.setSFlag(TimerInterrupt);
 }
 
 //
@@ -758,9 +766,10 @@ void handlerUSR2(int)
  * Alarm handling
  * -------------------------------------------------------------------------*/
 
-void AM::handleAlarm(unsigned int ms)
+void AM::handleAlarm(int ms)
 {
-  emulatorClock = emulatorClock + (unsigned long) ms;
+  if (ms>0) 
+    emulatorClock += (unsigned long) ms;
 
   if (ozstat.currPropagator) {
     ozstat.currPropagator->incSamples();
@@ -768,10 +777,6 @@ void AM::handleAlarm(unsigned int ms)
     ozstat.currAbstr->samples++;
   }
 
-  if (isCritical()) {  /* wait for next ALRM signal */
-    return;
-  }
-  
   if (threadSwitchCounter > 0) {
     if (--threadSwitchCounter == 0) {
       setSFlag(ThreadSwitch);
