538929c4fac4fad6d4e709671ca89c92cd3a62e8
schulte <schulte@ada56829-ad1f-0410-b00f-83cda6628aec>
1999-03-25 23:56:13 +0000

    Use new fd distributors

--- platform/emulator/libfd/distribute.cc
+++ platform/emulator/libfd/distribute.cc
@@ -27,6 +27,7 @@
 #include "var_bool.hh"
 #include "distributor.hh"
 #include "thr_int.hh"
+#include "fdomn.hh"
 
 // ---------------------------------------------------------------------
 //                  Finite Domains Distribution Built-ins
@@ -63,189 +64,11 @@ int getConstraints(TaggedRef var) {
 }
 
 
-inline
-static int discard_determined(SRecord * vec) {
-  // Discard all elements which are small ints already
-  register int j = 0;
-  register int w = vec->getWidth();
-
-  for (int i = 0; i<w; i++) {
-    TaggedRef var   = vec->getArg(i);
-    TaggedRef d_var = oz_deref(var);
-
-    if (!oz_isSmallInt(d_var)) {
-      Assert(isGenFDVar(d_var) || isGenBoolVar(d_var));
-      vec->setArg(j++, var);
-    }
-
-  }
-
-  if (j)
-    vec->downSize(j);
-
-  return j;
-}
-
-
-OZ_BI_define(BIfdd_selVarNaive, 1, 1) {
-  oz_declareSTupleIN(0, vec);
-
-  int w = discard_determined(vec);
-
-  // No elements left
-  if (!w)
-    return OZ_raise(makeTaggedSmallInt(-1));
-
-  OZ_RETURN(vec->getArg(0));
-} OZ_BI_end
-
-
-OZ_BI_define(BIfdd_selVarSize, 1, 1) {
-  oz_declareSTupleIN(0, vec);
-
-  int w = discard_determined(vec);
-
-  // No elements left
-  if (!w)
-    return OZ_raise(makeTaggedSmallInt(-1));
-
-
-  TaggedRef var = vec->getArg(0);
-
-  int minsize = getSize(oz_deref(var));
-
-  for (int i=1; i<w; i++) {
-    TaggedRef arg   = vec->getArg(i);
-    TaggedRef d_arg = oz_deref(arg);
-
-    int cursize = getSize(d_arg);
-
-    if (cursize < minsize) {
-      minsize = cursize;
-      var     = arg;
-    }
-
-  }
-
-  OZ_RETURN(var);
-} OZ_BI_end
-
-
-OZ_BI_define(BIfdd_selVarMax, 1, 1) {
-  oz_declareSTupleIN(0, vec);
-
-  int w = discard_determined(vec);
-
-  // No elements left
-  if (!w)
-    return OZ_raise(makeTaggedSmallInt(-1));
-
-  TaggedRef var = vec->getArg(0);
-  int maxmax    = getMax(oz_deref(var));
-
-  for (int i=1; i<w; i++) {
-    TaggedRef arg   = vec->getArg(i);
-    TaggedRef d_arg = oz_deref(arg);
-
-    int curmax = getMax(d_arg);
-
-    if (curmax > maxmax) {
-      maxmax = curmax;
-      var    = arg;
-    }
-
-  }
-
-  OZ_RETURN(var);
-} OZ_BI_end
-
-
-OZ_BI_define(BIfdd_selVarMin, 1, 1) {
-  oz_declareSTupleIN(0, vec);
-
-  int w = discard_determined(vec);
-
-  // No elements left
-  if (!w)
-    return OZ_raise(makeTaggedSmallInt(-1));
-
-  TaggedRef var = vec->getArg(0);
-  int minmin    = getMin(oz_deref(var));
-
-  for (int i=1; i<w; i++) {
-    TaggedRef arg   = vec->getArg(i);
-    TaggedRef d_arg = oz_deref(arg);
-
-    int curmin = getMin(d_arg);
-
-    if (curmin < minmin) {
-      minmin = curmin;
-      var    = arg;
-    }
-
-  }
-
-  OZ_RETURN(var);
-} OZ_BI_end
-
-
-OZ_BI_define(BIfdd_selVarNbSusps, 1, 1) {
-  oz_declareSTupleIN(0, vec);
-
-  int w = discard_determined(vec);
-
-  // No elements left
-  if (!w)
-    return OZ_raise(makeTaggedSmallInt(-1));
-
-  TaggedRef var   = vec->getArg(0);
-  TaggedRef d_var = oz_deref(var);
-
-  int minsize = getSize(d_var);
-  int maxcon  = getConstraints(d_var);
-
-  for (int i=1; i<w; i++) {
-    TaggedRef arg   = vec->getArg(i);
-    TaggedRef d_arg = oz_deref(arg);
-
-    int curcon  = getConstraints(d_arg);
-
-    if (curcon < maxcon)
-      continue;
-
-    if (curcon==maxcon) {
-      int cursize = getSize(d_arg);
-
-      if (cursize < minsize) {
-        minsize = cursize;
-      } else {
-        continue;
-      }
-    }
-
-    maxcon = curcon;
-    var    = arg;
-
-  }
-
-  OZ_RETURN(var);
-} OZ_BI_end
-
-
-#define NEW_DISTRIBUTORS
-
-#ifdef NEW_DISTRIBUTORS
-
-/*
- * New distributors
- *
- *
- */
-
 OZ_C_proc_proto(BIfdTellConstraint);
 
 TaggedRef BI_tell = makeTaggedConst(new  Builtin("FD.distributeTELL", 2, 0,
                                                  BIfdTellConstraint, OK));
+
 
 inline
 void tell_dom(Board * bb, const TaggedRef a, const TaggedRef b) {
@@ -261,12 +84,12 @@ class FdDistributor : public Distributor {
 protected:
   TaggedRef sync;
   int sel_var;
-  TaggedRef values[2];
+  TaggedRef sel_val;
   TaggedRef * vars;
   int size;
 public:
 
-  FdDistributor::FdDistributor(Board *bb, TaggedRef * vs, int n) {
+  FdDistributor(Board *bb, TaggedRef * vs, int n) {
     vars = vs;
     size = n;
     sync = oz_newVar(bb);
@@ -277,15 +100,31 @@ public:
   }
 
   void normalize(void);
-  void selectVarBySize(void);
-  void selectValMin(void);
 
-  virtual int getAlternatives(void);
+  void selectVarNaive(void);
+  void selectVarSize(void);
+  void selectVarMin(void);
+  void selectVarMax(void);
+  void selectVarNbSusps(void);
+
+  void selectValMin(void);
+  void selectValMid(void);
+  void selectValMax(void);
+  void selectValSplitMin(void);
+  void selectValSplitMax(void);
 
   virtual int commit(Board * bb, int l, int r) {
     if (size > 0) {
       if (l==r) {
-        tell_dom(bb,vars[sel_var],values[l-1]);
+        TaggedRef dom;
+        if (l == 1) {
+          dom = sel_val;
+        } else {
+          SRecord * st = SRecord::newSRecord(AtomCompl, 1);
+          st->setArg(0, sel_val);
+          dom = makeTaggedSRecord(st);
+        }
+        tell_dom(bb,vars[sel_var],dom);
         return 1;
       } else {
         return 2;
@@ -304,7 +143,7 @@ public:
     FdDistributor * t = (FdDistributor *)
       oz_hrealloc(this, sizeof(FdDistributor));
     OZ_collectHeapTerm(t->sync,t->sync);
-    OZ_collectHeapBlock(t->values,t->values,2);
+    OZ_collectHeapTerm(t->sel_val,t->sel_val);
     t->vars = OZ_copyOzTerms(size, t->vars);
     return t;
   }
@@ -312,7 +151,6 @@ public:
 };
 
 
-inline
 void FdDistributor::normalize(void) {
   // Discard all elements which are already bound
   int j = size;
@@ -322,52 +160,170 @@ void FdDistributor::normalize(void) {
       vars[--j]=vars[i];
   }
 
-  vars += j;
-  size -= j;
+  if (j > 0) {
+    freeListDispose(vars, j * sizeof(TaggedRef));
+    vars += j;
+    size -= j;
+  }
 
 }
 
-inline
-void FdDistributor::selectVarBySize(void) {
+/*
+ * Variable selection strategies
+ *
+ */
+
+void FdDistributor::selectVarNaive(void) {
+  Assert(size > 0);
+  sel_var = size-1;
+}
+
+void FdDistributor::selectVarSize(void) {
   Assert(size > 0);
   int minsize = getSize(oz_deref(vars[size-1]));
-
   sel_var = size-1;
-
   for (int i = size-1; i--; ) {
     int cursize = getSize(oz_deref(vars[i]));
-
     if (cursize < minsize) {
+      minsize = cursize; sel_var = i;
+    }
+  }
+}
+
+void FdDistributor::selectVarMin(void) {
+  Assert(size > 0);
+  int minmin = getMin(oz_deref(vars[size-1]));
+  sel_var = size-1;
+  for (int i = size-1; i--; ) {
+    int curmin = getMin(oz_deref(vars[i]));
+    if (curmin < minmin) {
+      minmin = curmin; sel_var = i;
+    }
+  }
+}
+
+void FdDistributor::selectVarMax(void) {
+  Assert(size > 0);
+  int maxmax = getMax(oz_deref(vars[size-1]));
+  sel_var = size-1;
+  for (int i = size-1; i--; ) {
+    int curmax = getMax(oz_deref(vars[i]));
+    if (curmax > maxmax) {
+      maxmax = curmax; sel_var = i;
+    }
+  }
+}
+
+void FdDistributor::selectVarNbSusps(void) {
+  Assert(size > 0);
+  TaggedRef d_var = oz_deref(vars[size-1]);
+  int minsize = getSize(d_var);
+  int maxnb   = getConstraints(d_var);
+  sel_var = size-1;
+  for (int i = size-1; i--; ) {
+    d_var = oz_deref(vars[i]);
+    int curnb = getConstraints(d_var);
+
+    if (curnb < maxnb)
+      continue;
+
+    int cursize = getSize(d_var);
+
+    if (curnb > maxnb || cursize < minsize) {
+      maxnb   = curnb;
       minsize = cursize;
       sel_var = i;
     }
   }
-
 }
 
-inline
+
+
+/*
+ * Value selection strategies
+ *
+ */
+
 void FdDistributor::selectValMin(void) {
-  Assert(size > 0);
-
-  int n = getMin(oz_deref(vars[sel_var]));
-
-  values[0] = makeTaggedSmallInt(n);
-  SRecord * st = SRecord::newSRecord(AtomCompl, 1);
-  st->setArg(0, makeTaggedSmallInt(n));
-  values[1] = makeTaggedSRecord(st);
-
+  sel_val = makeTaggedSmallInt(getMin(oz_deref(vars[sel_var])));
 }
 
-int FdDistributor::getAlternatives(void) {
-  normalize();
-  if (size > 0) {
-    selectVarBySize();
-    selectValMin();
-    return 2;
-  } else {
-    return 1;
-  }
+void FdDistributor::selectValMid(void) {
+  sel_val = makeTaggedSmallInt(getMid(oz_deref(vars[sel_var])));
 }
+
+void FdDistributor::selectValMax(void) {
+  sel_val = makeTaggedSmallInt(getMax(oz_deref(vars[sel_var])));
+}
+
+void FdDistributor::selectValSplitMin(void) {
+  SRecord * st = SRecord::newSRecord(AtomPair, 2);
+  st->setArg(0, makeTaggedSmallInt(0));
+  st->setArg(1, makeTaggedSmallInt(getMid(oz_deref(vars[sel_var]))));
+  sel_val = makeTaggedSRecord(st);
+}
+
+void FdDistributor::selectValSplitMax(void) {
+  SRecord * st = SRecord::newSRecord(AtomPair, 2);
+  st->setArg(0, makeTaggedSmallInt(getMid(oz_deref(vars[sel_var])) + 1));
+  st->setArg(1, makeTaggedSmallInt(fd_sup));
+  sel_val = makeTaggedSRecord(st);
+}
+
+
+
+/*
+ * Create class for different combinations
+ */
+
+#define DefFdDistClass(CLASS,VARSEL,VALSEL) \
+class CLASS : public FdDistributor {              \
+  public:                                         \
+  CLASS(Board * b, TaggedRef * v, int s) :        \
+    FdDistributor(b,v,s) {}                       \
+  virtual int getAlternatives(void) {             \
+    normalize();                                  \
+    if (size > 0) { VARSEL(); VALSEL(); return 2; \
+    } else {                            return 1; \
+    }                                             \
+  };                                              \
+}
+
+DefFdDistClass(FdDist_Naive_Min,selectVarNaive,selectValMin);
+DefFdDistClass(FdDist_Naive_Mid,selectVarNaive,selectValMid);
+DefFdDistClass(FdDist_Naive_Max,selectVarNaive,selectValMax);
+DefFdDistClass(FdDist_Naive_SplitMin,selectVarNaive,selectValSplitMin);
+DefFdDistClass(FdDist_Naive_SplitMax,selectVarNaive,selectValSplitMax);
+
+DefFdDistClass(FdDist_Size_Min,selectVarSize,selectValMin);
+DefFdDistClass(FdDist_Size_Mid,selectVarSize,selectValMid);
+DefFdDistClass(FdDist_Size_Max,selectVarSize,selectValMax);
+DefFdDistClass(FdDist_Size_SplitMin,selectVarSize,selectValSplitMin);
+DefFdDistClass(FdDist_Size_SplitMax,selectVarSize,selectValSplitMax);
+
+DefFdDistClass(FdDist_Min_Min,selectVarMin,selectValMin);
+DefFdDistClass(FdDist_Min_Mid,selectVarMin,selectValMid);
+DefFdDistClass(FdDist_Min_Max,selectVarMin,selectValMax);
+DefFdDistClass(FdDist_Min_SplitMin,selectVarMin,selectValSplitMin);
+DefFdDistClass(FdDist_Min_SplitMax,selectVarMin,selectValSplitMax);
+
+DefFdDistClass(FdDist_Max_Min,selectVarMax,selectValMin);
+DefFdDistClass(FdDist_Max_Mid,selectVarMax,selectValMid);
+DefFdDistClass(FdDist_Max_Max,selectVarMax,selectValMax);
+DefFdDistClass(FdDist_Max_SplitMin,selectVarMax,selectValSplitMin);
+DefFdDistClass(FdDist_Max_SplitMax,selectVarMax,selectValSplitMax);
+
+DefFdDistClass(FdDist_NbSusps_Min,selectVarNbSusps,selectValMin);
+DefFdDistClass(FdDist_NbSusps_Mid,selectVarNbSusps,selectValMid);
+DefFdDistClass(FdDist_NbSusps_Max,selectVarNbSusps,selectValMax);
+DefFdDistClass(FdDist_NbSusps_SplitMin,selectVarNbSusps,selectValSplitMin);
+DefFdDistClass(FdDist_NbSusps_SplitMax,selectVarNbSusps,selectValSplitMax);
+
+
+
+/*
+ * Vector processing
+ */
 
 #define TestElement(v) \
   {                                         \
@@ -383,9 +339,29 @@ int FdDistributor::getAlternatives(void) {
     }                                       \
   }
 
+#define iVarNaive   0
+#define iVarSize    1
+#define iVarMin     2
+#define iVarMax     3
+#define iVarNbSusps 4
 
-OZ_BI_define(fdd_test_ff, 1, 1) {
-  oz_declareNonvarIN(0,vv);
+#define iValMin      0
+#define iValMid      1
+#define iValMax      2
+#define iValSplitMin 3
+#define iValSplitMax 4
+
+#define PP(I,J) I*5+J
+
+#define PPCL(I,J)                                  \
+  case PP(iVar ## I,iVal ## J):                    \
+    fdd = new FdDist_ ## I ## _ ## J(bb, vars, n); \
+    break;
+
+OZ_BI_define(fdd_distribute, 3, 1) {
+  oz_declareIntIN(0,var_sel);
+  oz_declareIntIN(1,val_sel);
+  oz_declareNonvarIN(2,vv);
 
   int n = 0;
   TaggedRef * vars;
@@ -448,8 +424,41 @@ OZ_BI_define(fdd_test_ff, 1, 1) {
   {
     Board * bb = oz_currentBoard();
 
-    FdDistributor * fdd = new FdDistributor(bb, vars, n);
+    FdDistributor * fdd;
 
+    switch (PP(var_sel,val_sel)) {
+      PPCL(Naive,Min);
+      PPCL(Naive,Mid);
+      PPCL(Naive,Max);
+      PPCL(Naive,SplitMin);
+      PPCL(Naive,SplitMax);
+
+      PPCL(Size,Min);
+      PPCL(Size,Mid);
+      PPCL(Size,Max);
+      PPCL(Size,SplitMin);
+      PPCL(Size,SplitMax);
+
+      PPCL(Min,Min);
+      PPCL(Min,Mid);
+      PPCL(Min,Max);
+      PPCL(Min,SplitMin);
+      PPCL(Min,SplitMax);
+
+      PPCL(Max,Min);
+      PPCL(Max,Mid);
+      PPCL(Max,Max);
+      PPCL(Max,SplitMin);
+      PPCL(Max,SplitMax);
+
+      PPCL(NbSusps,Min);
+      PPCL(NbSusps,Mid);
+      PPCL(NbSusps,Max);
+      PPCL(NbSusps,SplitMin);
+      PPCL(NbSusps,SplitMax);
+    default:
+      Assert(0);
+    }
     bb->addToDistBag(fdd);
 
     OZ_RETURN(fdd->getSync());
@@ -459,7 +468,3 @@ OZ_BI_define(fdd_test_ff, 1, 1) {
   oz_typeError(0,"vector of finite domains");
 }
 OZ_BI_end
-
-
-
-#endif
