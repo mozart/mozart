0279f873f3cfbde3a4eb8e07beabe40e688b486c
schulte <schulte@ada56829-ad1f-0410-b00f-83cda6628aec>
1999-04-14 09:20:36 +0000

    I am conhow somefused

--- platform/emulator/bits.cc
+++ platform/emulator/bits.cc
@@ -42,13 +42,16 @@ static const char bits_in_byte[256] = {
   4, 5, 5, 6, 5, 6, 6, 7, 5, 6, 6, 7, 6, 7, 7, 8,
 };
 
-int get_num_of_bits(const int m, int * ia) {
+int get_num_of_bits(const int m, const int * ia) {
   int s = 0;
 
-  unsigned char * ca = (unsigned char *) ia;
-
-  for (int i = m << 2 ; i--; )
-    s += bits_in_byte[ca[i]];
+  for (int i = m ; i--; ) {
+    unsigned int iai = ia[i];
+    s += (bits_in_byte[(iai             ) >> 24] +
+          bits_in_byte[(iai & 0x00ff0000) >> 16] +
+          bits_in_byte[(iai & 0x0000ff00) >>  8] +
+          bits_in_byte[(iai & 0x000000ff)]);
+  }
 
   return s;
 }
