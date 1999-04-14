f6c425888868217a42a3d057173ec8e3ff022beb
schulte <schulte@ada56829-ad1f-0410-b00f-83cda6628aec>
1999-04-14 08:42:07 +0000

    Number of bits set in int[]
    git-svn-id: https://gforge.info.ucl.ac.be/svn/mozart@11216 ada56829-ad1f-0410-b00f-83cda6628aec

---
+++ trunk/mozart/platform/emulator/bits.hh
@@ -0,0 +1,30 @@
+/*
+ *  Authors:
+ *    Christian Schulte <schulte@ps.uni-sb.de>
+ * 
+ *  Copyright:
+ *    Christian Schulte, 1999
+ * 
+ *  Last change:
+ *    $Date$ by $Author$
+ *    $Revision$
+ * 
+ *  This file is part of Mozart, an implementation 
+ *  of Oz 3:
+ *     http://www.mozart-oz.org
+ * 
+ *  See the file "LICENSE" or
+ *     http://www.mozart-oz.org/LICENSE.html
+ *  for information on usage and redistribution 
+ *  of this file, and for a DISCLAIMER OF ALL 
+ *  WARRANTIES.
+ *
+ */
+
+#ifndef __BITS_HH__
+
+#define __BITS_HH__
+
+int get_num_of_bits(const int m, const int * ia);
+
+#endif
