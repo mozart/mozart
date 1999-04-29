f4927c59496fe930ef3ef36950ac9eb258d22d57
tmueller <tmueller@ada56829-ad1f-0410-b00f-83cda6628aec>
1999-04-29 16:20:43 +0000

    adaptation inprogress (tellBasicConstraint(FD))

--- platform/emulator/fddebug.hh
+++ platform/emulator/fddebug.hh
@@ -1,25 +1,25 @@
 /*
  *  Authors:
  *    Tobias Mueller (tmueller@ps.uni-sb.de)
- * 
+ *
  *  Contributors:
  *    optional, Contributor's name (Contributor's email address)
- * 
+ *
  *  Copyright:
  *    Organization or Person (Year(s))
- * 
+ *
  *  Last change:
  *    $Date$ by $Author$
  *    $Revision$
- * 
- *  This file is part of Mozart, an implementation 
+ *
+ *  This file is part of Mozart, an implementation
  *  of Oz 3:
  *     http://www.mozart-oz.org
- * 
+ *
  *  See the file "LICENSE" or
  *     http://www.mozart-oz.org/LICENSE.html
- *  for information on usage and redistribution 
- *  of this file, and for a DISCLAIMER OF ALL 
+ *  for information on usage and redistribution
+ *  of this file, and for a DISCLAIMER OF ALL
  *  WARRANTIES.
  *
  */
@@ -31,24 +31,26 @@
 
 //-----------------------------------------------------------------------------
 
+//#define DEBUG_CONSTRAINT_IR
 #ifdef DEBUG_CONSTRAINT_IR
 //#define DEBUG_FD_CONSTRREP
 #define DEBUG_FD
 #define DEBUG_FSET
-//#define DEBUG_FSET_CONSTRREP
+#define DEBUG_FSET_CONSTRREP
+
+//#define TO_FSET_FILE
+//#define TO_FD_FILE
 #endif
 
 extern FILE * _fdomn_file, * _fset_file;
 
-#define TO_FSET_FILE
-//#define TO_FD_FILE
 
 //-----------------------------------------------------------------------------
 #ifdef DEBUG_FD_CONSTRREP
 #define AssertFD(C) \
-if (!(C)) OZ_error("AssertFD '%s' failed at %s:%d.", #C, __FILE__, __LINE__); 
+if (!(C)) OZ_error("AssertFD '%s' failed at %s:%d.", #C, __FILE__, __LINE__);
 
-#define _DEBUG_FD_IR(CODE) { print_to_fdfile CODE ; } 
+#define _DEBUG_FD_IR(CODE) { print_to_fdfile CODE ; }
 #define DEBUG_FD_IR(CODE) _DEBUG_FD_IR(CODE)
 #define DebugCodeFD(C) C
 
@@ -66,10 +68,10 @@ if (!(C)) OZ_error("AssertFD '%s' failed at %s:%d.", #C, __FILE__, __LINE__);
 #ifdef DEBUG_FSET_CONSTRREP
 
 #define AssertFS(C) \
-if (!(C)) OZ_error("AssertFS '%s' failed at %s:%d.", #C, __FILE__, __LINE__); 
+if (!(C)) OZ_error("AssertFS '%s' failed at %s:%d.", #C, __FILE__, __LINE__);
 
 #define _DEBUG_FSET_IR(CODE) { print_to_fsfile CODE ; }
-#define DEBUG_FSET_IR(CODE) _DEBUG_FSET_IR(CODE) 
+#define DEBUG_FSET_IR(CODE) _DEBUG_FSET_IR(CODE)
 #define FSDEBUG(X) //{ X; }
 
 #else
