0e6f371bcd84e835ea6f26c0e3874c2a06b6ce28
tmueller <tmueller@ada56829-ad1f-0410-b00f-83cda6628aec>
1999-05-03 15:36:43 +0000

    adapted fd cpi to new bind/unifucation scheme
    git-svn-id: https://gforge.info.ucl.ac.be/svn/mozart@11310 ada56829-ad1f-0410-b00f-83cda6628aec

--- platform/emulator/cpi_fd.cc
+++ platform/emulator/cpi_fd.cc
@@ -1,25 +1,25 @@
 /*
  *  Authors:
  *    Tobias Mueller (tmueller@ps.uni-sb.de)
- * 
+ *
  *  Contributors:
  *    optional, Contributor's name (Contributor's email address)
- * 
+ *
  *  Copyright:
  *    Organization or Person (Year(s))
- * 
+ *
  *  Last change:
  *    $Date$ by $Author$
  *    $Revision$
- * 
- *  This file is part of Mozart, an implementation 
+ *
+ *  This file is part of Mozart, an implementation
  *  of Oz 3:
  *     http://www.mozart-oz.org
- * 
+ *
  *  See the file "LICENSE" or
  *     http://www.mozart-oz.org/LICENSE.html
- *  for information on usage and redistribution 
- *  of this file, and for a DISCLAIMER OF ALL 
+ *  for information on usage and redistribution
+ *  of this file, and for a DISCLAIMER OF ALL
  *  WARRANTIES.
  *
  */
@@ -30,7 +30,7 @@
 
 //-----------------------------------------------------------------------------
 
-void * OZ_FDIntVar::operator new(size_t s) 
+void * OZ_FDIntVar::operator new(size_t s)
 {
   return CpiHeap.alloc(s);
 }
@@ -41,7 +41,7 @@ void OZ_FDIntVar::operator delete(void * p, size_t s)
 }
 
 #ifdef __GNUC__
-void * OZ_FDIntVar::operator new[](size_t s) 
+void * OZ_FDIntVar::operator new[](size_t s)
 {
   return CpiHeap.alloc(s);
 }
@@ -52,10 +52,10 @@ void OZ_FDIntVar::operator delete[](void * p, size_t s)
 }
 #endif
 
-void OZ_FDIntVar::ask(OZ_Term v) 
+void OZ_FDIntVar::ask(OZ_Term v)
 {
   Assert(oz_isRef(v) || !oz_isVariable(v));
-  
+
   _DEREF(v, varPtr, vtag);
   var = v;
 
@@ -74,20 +74,20 @@ void OZ_FDIntVar::ask(OZ_Term v)
       setSort(bool_e);
     } else {
       Assert(cvar->getType() == OZ_VAR_FD);
-	
+
       domPtr = &((OzFDVariable *) cvar)->getDom();
       initial_size = domPtr->getSize();
       setSort(int_e);
-      
+
       Assert(initial_size > 1 && *domPtr != fd_bool);
     }
-  } 
+  }
 }
 
-int OZ_FDIntVar::read(OZ_Term v) 
+int OZ_FDIntVar::read(OZ_Term v)
 {
   Assert(oz_isRef(v) || !oz_isVariable(v));
-  
+
   _DEREF(v, varPtr, vtag);
   var = v;
 
@@ -109,12 +109,12 @@ int OZ_FDIntVar::read(OZ_Term v)
 
       if (cvar->testReifiedFlag()) {
       // may already be a reified var; then the type is incorrect
-	if (cvar->isBoolPatched()) goto local_bool; else goto local_fd; 
-      } else if (cvar->getType() == OZ_VAR_BOOL) {	
+	if (cvar->isBoolPatched()) goto local_bool; else goto local_fd;
+      } else if (cvar->getType() == OZ_VAR_BOOL) {
       local_bool:
 
 	setSort(bool_e);
-	
+
 	if (cvar->testStoreFlag()) {
 	// may already be entered as store var
 	  initial_size = 2;
@@ -128,9 +128,9 @@ int OZ_FDIntVar::read(OZ_Term v)
 	initial_width = 1;
       } else {
       local_fd:
-	
+
 	setSort(int_e);
-	
+
 	if (oz_onToplevel())
 	  dom = ((OzFDVariable *) cvar)->getDom();
 	domPtr = &((OzFDVariable *) cvar)->getDom();
@@ -139,20 +139,24 @@ int OZ_FDIntVar::read(OZ_Term v)
 
 	Assert(initial_size > 1 && *domPtr != fd_bool);
       }
-      
+
     } else {
       // don't know before hand if local or global
-      
+
       OzVariable * cvar = tagged2CVar(v);
       setState(oz_isLocalVar(cvar) ? loc_e : glob_e);
-      
+
       if (cvar->testReifiedFlag()) {
       // may already be a reified var; then the type is incorrect
-	if (cvar->isBoolPatched()) goto global_bool; else goto global_fd; 
+	if (cvar->isBoolPatched()) {
+	  goto global_bool;
+	} else {
+	  goto global_fd;
+	}
       } else if (cvar->getType() == OZ_VAR_BOOL) {
       global_bool:
 	setSort(bool_e);
-	
+
 	if (cvar->testStoreFlag()) {
 	// may already be entered as store var
 	  domPtr = ((OzBoolVariable *) cvar)->getStorePatchBool();
@@ -167,7 +171,7 @@ int OZ_FDIntVar::read(OZ_Term v)
 
       } else {
       global_fd:
-	
+
 	if (isState(glob_e) || oz_onToplevel())
 	  dom = ((OzFDVariable *) cvar)->getDom();
 	domPtr = &((OzFDVariable *) cvar)->getDom();
@@ -178,13 +182,13 @@ int OZ_FDIntVar::read(OZ_Term v)
 	Assert(initial_size > 1 && *domPtr != fd_bool);
       }
     }
- 
+
     setStoreFlag(v);
   }
   return domPtr->getSize();
 }
 
-int OZ_FDIntVar::readEncap(OZ_Term v) 
+int OZ_FDIntVar::readEncap(OZ_Term v)
 {
   Assert(oz_isRef(v) || !oz_isVariable(v));
 
@@ -202,7 +206,7 @@ int OZ_FDIntVar::readEncap(OZ_Term v)
 
     setState(encap_e);
     OzVariable * cvar = tagged2CVar(v);
-    
+
     if (cvar->testReifiedFlag()) {
     // var is already entered somewhere else
       setSort(cvar->isBoolPatched() ? bool_e : int_e);
@@ -236,18 +240,109 @@ int OZ_FDIntVar::readEncap(OZ_Term v)
   return domPtr->getSize();
 }
 
-#define CHECK_BOUNDS 							\
-   (initial_width > ((OZ_FiniteDomainImpl *)domPtr)->getWidth() 	\
+#define CHECK_BOUNDS                                                    \
+   (initial_width > ((OZ_FiniteDomainImpl *)domPtr)->getWidth()         \
      ? fd_prop_bounds : fd_prop_any)
 
+#ifdef TMUELLER
+OZ_Boolean OZ_FDIntVar::tell(void)
+{
+  // someone else has already determined it
+  if (!oz_isVariable(*varPtr)) {
+    return OZ_FALSE;
+  }
+  //
+  if (testReifiedFlag(var)) {
+    unpatchReifiedFD(var, isSort(bool_e));
+  }
+  //
+  if (!testResetStoreFlag(var)) {
+    // the constraint has already been told, i.e., there were at least
+    // two OZ_FDIntVar connected to the same store variable
+    return OZ_FALSE;
+  } else if(!isTouched()) {
+    // no constraints have been imposed by the current propagator run.
+    // note the cases caches integers too.
+    return OZ_TRUE;
+  } else if (isSort(int_e)) {
+    //
+    // there is a finite domain variable in the store
+    //
+    if (*domPtr == fd_singl) {
+      //
+      // propagation produced singleton
+      //
+      if (isState(loc_e)) {
+	// local variable
+	tagged2GenFDVar(var)->becomesSmallIntAndPropagate(varPtr);
+      } else {
+	// global variable
+	int int_val = domPtr->getSingleElem();
+	// restore the original constraint 
+	*domPtr = dom;
+	// wake up
+	tagged2GenFDVar(var)->propagate(fd_prop_singl);
+	bindGlobalVarToValue(varPtr, newSmallInt(int_val));
+      }
+    } else if (*domPtr == fd_bool) {
+      //
+      // propagation produced boolean domain
+      //
+      if (isState(loc_e)) {
+	// local variable
+	tagged2GenFDVar(var)->becomesBoolVarAndPropagate(varPtr);
+      } else {
+	// global variable
+	// restore the original constraint 
+	*domPtr = dom;
+	// wake up
+	tagged2GenFDVar(var)->propagate(CHECK_BOUNDS);
+	// cast store variable to boolean varaible
+	Board * fdvarhome = tagged2GenFDVar(var)->getBoardInternal();
+	OZ_Term * newboolvar = newTaggedCVar(new OzBoolVariable(fdvarhome));
+	castGlobalVar(varPtr, newboolvar);
+      }
+      return OZ_TRUE;
+    } else {
+      //
+      // propagation produced proper domain
+      //
+      tagged2GenFDVar(var)->propagate(CHECK_BOUNDS);
+      if (isState(glob_e)) {
+	// restore the original constraint 
+	*domPtr = dom;
+	// constrain global variable
+	constrainGlobalVar(varPtr, *domPtr);
+      }
+      return OZ_TRUE;
+    }
+  } else {
+    //
+    // there is a finite domain variable in the store
+    //
+    Assert(isSort(bool_e) && *domPtr == fd_singl); // boolean variable
+
+    if (isState(loc_e)) {
+      // local variable
+      tagged2GenBoolVar(var)->becomesSmallIntAndPropagate(varPtr, *domPtr);
+    } else {
+      // global variable
+      tagged2GenBoolVar(var)->propagate();
+      int int_val = domPtr->getSingleElem();
+      bindGlobalVarToValue(varPtr, newSmallInt(int_val));
+    }
+  }
+  return OZ_FALSE;
+}
+#else
 OZ_Boolean OZ_FDIntVar::tell(void)
 {
   if (!oz_isVariable(*varPtr))
     return OZ_FALSE;
 
-  if (testReifiedFlag(var)) 
+  if (testReifiedFlag(var))
     unpatchReifiedFD(var, isSort(bool_e));
-  
+
   if (!testResetStoreFlag(var)) {
     return OZ_FALSE;
   } else if(!isTouched()) {
@@ -283,12 +378,12 @@ OZ_Boolean OZ_FDIntVar::tell(void)
 	*domPtr = dom;
 	DoBindAndTrailAndIP(varPtr, makeTaggedRef(loctaggedfdvar),
 			    locfdvar, tagged2GenFDVar(var));
-      } 
+      }
       return OZ_TRUE;
     }
   } else {
     Assert(isSort(bool_e) && *domPtr == fd_singl); // boolean variable
-    
+
     if (isState(loc_e)) {
       tagged2GenBoolVar(var)->becomesSmallIntAndPropagate(varPtr, *domPtr);
     } else {
@@ -298,10 +393,11 @@ OZ_Boolean OZ_FDIntVar::tell(void)
   }
   return OZ_FALSE;
 }
+#endif
 
 void OZ_FDIntVar::fail(void)
 {
-  if (isSort(sgl_e)) 
+  if (isSort(sgl_e))
     return;
   if (isState(encap_e)) {
     unpatchReifiedFD(var, isSort(bool_e));
