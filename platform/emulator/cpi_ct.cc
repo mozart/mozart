06fb6bdec729d32bb26d158cf7994ccee86ffd32
tmueller <tmueller@ada56829-ad1f-0410-b00f-83cda6628aec>
1999-06-15 13:33:35 +0000

    adopted ct vars to new unification
    git-svn-id: https://gforge.info.ucl.ac.be/svn/mozart@11527 ada56829-ad1f-0410-b00f-83cda6628aec

--- platform/emulator/cpi_ct.cc
+++ platform/emulator/cpi_ct.cc
@@ -1,25 +1,25 @@
 /*
  *  Authors:
  *    Tobias Mueller (tmueller@ps.uni-sb.de)
- * 
+ *
  *  Contributors:
  *    optional, Contributor's name (Contributor's email address)
- * 
+ *
  *  Copyright:
  *    Organization or Person (Year(s))
- * 
+ *
  *  Last change:
  *    $Date$ by $Author$
  *    $Revision$
- * 
- *  This file is part of Mozart, an implementation 
+ *
+ *  This file is part of Mozart, an implementation
  *  of Oz 3:
  *     http://www.mozart-oz.org
- * 
+ *
  *  See the file "LICENSE" or
  *     http://www.mozart-oz.org/LICENSE.html
- *  for information on usage and redistribution 
- *  of this file, and for a DISCLAIMER OF ALL 
+ *  for information on usage and redistribution
+ *  of this file, and for a DISCLAIMER OF ALL
  *  WARRANTIES.
  *
  */
@@ -29,17 +29,17 @@
 
 //-----------------------------------------------------------------------------
 
-void * OZ_Ct::operator new(size_t s, int align) 
+void * OZ_Ct::operator new(size_t s, int align)
 {
   return alignedMalloc(s, align);
 }
 
-void OZ_Ct::operator delete(void * p, size_t s) 
+void OZ_Ct::operator delete(void * p, size_t s)
 {
   freeListDispose(p, s);
 }
 
-void * OZ_CtVar::operator new(size_t s) 
+void * OZ_CtVar::operator new(size_t s)
 {
   return CpiHeap.alloc(s);
 }
@@ -50,7 +50,7 @@ void OZ_CtVar::operator delete(void * p, size_t s)
 }
 
 #ifdef __GNUC__
-void * OZ_CtVar::operator new[](size_t s) 
+void * OZ_CtVar::operator new[](size_t s)
 {
   return CpiHeap.alloc(s);
 }
@@ -62,10 +62,10 @@ void OZ_CtVar::operator delete[](void * p, size_t s)
 #endif
 
 
-void OZ_CtVar::ask(OZ_Term v) 
+void OZ_CtVar::ask(OZ_Term v)
 {
   Assert(oz_isRef(v) || !oz_isVariable(v));
-  
+
   _DEREF(v, varPtr, vtag);
   var = v;
 
@@ -75,58 +75,69 @@ void OZ_CtVar::ask(OZ_Term v)
   } else {
     ctSetValue(v);
     setSort(val_e);
-  } 
+  }
 }
 
 
-void OZ_CtVar::read(OZ_Term v) 
+void OZ_CtVar::read(OZ_Term v)
 {
   Assert(oz_isRef(v) || !oz_isVariable(v));
-  
+
   _DEREF(v, varPtr, vtag);
   var = v;
-  
+
   if (oz_isVariable(vtag)) {
     // `v' designates a variable
-    
+
     Assert(isCVar(vtag));
-    
+
     if (Propagator::getRunningPropagator()->isLocal()) {
       // local variable per definition
-      
+
       // set flags
       setState(loc_e);
       setSort(var_e);
-      
+
       OzCtVariable * ctvar = tagged2GenCtVar(v);
       OZ_Ct * constr = ctvar->getConstraint();
-      
+
       if (oz_onToplevel())
 	ctSaveConstraint(constr);
-      
+
       ctSetLocalConstraint(constr);
-      
+
       ctSetConstraintProfile();
-      
+
     } else {
       // don't know before hand if local or global
-      
+
       // set flags
       OzCtVariable * ctvar = tagged2GenCtVar(v);
       setState(oz_isLocalVar(ctvar) ? loc_e : glob_e);
       setSort(var_e);
-      
+
       OZ_Ct * constr = ctvar->getConstraint();
 
+#ifdef CORRECT_UNIFY
+	if (isState(glob_e)) {
+	  ctRefConstraint(ctSaveConstraint(constr));
+	} else {
+	  if (oz_onToplevel()) {
+	    ctSaveConstraint(constr);
+	  }
+	  ctRefConstraint(constr);
+	}
+#else
       if (isState(glob_e) || oz_onToplevel()) {
 	ctSetGlobalConstraint(constr);
       } else {
 	ctSetLocalConstraint(constr);
       }
+#endif
 
       ctSetConstraintProfile();
     }
-    
+
     setStoreFlag(v); // tmueller
   } else {
     // `v' designates a value
@@ -141,7 +152,7 @@ void OZ_CtVar::read(OZ_Term v)
 }
 
 
-void OZ_CtVar::readEncap(OZ_Term v) 
+void OZ_CtVar::readEncap(OZ_Term v)
 {
   Assert(oz_isRef(v) || !oz_isVariable(v));
 
@@ -166,7 +177,7 @@ void OZ_CtVar::readEncap(OZ_Term v)
       ctSetConstraintProfile();
 
     } else {
-    // fs var entered first time
+    // ct var entered first time
 
       OZ_Ct * constr = ctvar->getConstraint();
 
@@ -184,32 +195,108 @@ void OZ_CtVar::readEncap(OZ_Term v)
     // set flags
     setSort(val_e);
     setState(loc_e); // TMUELLER: why, ought to be redundant
-    
+
     ctSetValue(v);
 
     ctSetConstraintProfile();
   }
 }
 
+#ifdef CORRECT_UNIFY
+//-----------------------------------------------------------------------------
+OZ_Boolean OZ_CtVar::tell(void)
+{
+  DEBUG_CONSTRAIN_CVAR(("OZ_CtVar::tell "));
 
+  // someone else has already determined it
+  if (!oz_isVariable(*varPtr)) {
+    return OZ_FALSE;
+  }
+  //
+  if (testReifiedFlag(var)) {
+    unpatchReifiedCt(var);
+  }
+  if (!testResetStoreFlag(var)) {
+    // the constraint has already been told, i.e., there were at least
+    // two OZ_FDIntVar connected to the same store variable
+    goto oz_false;
+  } else if(!isTouched()) {
+    // the constraint has already been told, i.e., there were at least
+    // two OZ_FDIntVar connected to the same store variable
+    goto oz_true;
+  } else {
+    // 
+    // there is a generic constraint variable in the store
+    //
+    Assert(isSort(var_e));
+    //
+    OzCtVariable * ctvar = tagged2GenCtVar(var);
+    OZ_Ct * constr       = ctGetConstraint();
+    //
+    if (constr->isValue()) {
+      //
+      // propagation produced a value
+      //
+      if (isState(loc_e)) {
+	// local variable
+	ctvar->propagate(OZ_WAKEUP_ALL, pc_propagator);
+	bindLocalVarToValue(varPtr, constr->toValue());
+      } else {
+	// global variable
+	// wake up
+	ctvar->propagate(OZ_WAKEUP_ALL, pc_propagator);
+	bindGlobalVarToValue(varPtr, constr->toValue());
+      }
+      goto oz_false;
+    } else {
+      // 
+      // propagation produced a set constraint
+      //
+      // wake up ...
+      OZ_CtWakeUp wakeup_descr = ctGetWakeUpDescrptor();
+      ctvar->propagate(wakeup_descr, pc_propagator);
+      //
+      if (isState(glob_e)) {
+	constrainGlobalVar(varPtr, constr);
+      }
+      goto oz_true;
+    }
+  }
+  //
+ oz_false:
+  //
+  // variable is determined
+  //
+  DEBUG_CONSTRAIN_CVAR(("FALSE\n"));
+  return OZ_FALSE;
+  //
+ oz_true:
+  //
+  // variable is still undetermined
+  //
+  DEBUG_CONSTRAIN_CVAR(("TRUE\n"));
+  return OZ_TRUE;
+}
+//-----------------------------------------------------------------------------
+#else
 OZ_Boolean OZ_CtVar::tell(void)
 {
   if (!oz_isVariable(*varPtr))
     return OZ_FALSE;
 
-  if (testReifiedFlag(var)) 
+  if (testReifiedFlag(var))
     unpatchReifiedCt(var);
-  
+
   if (!testResetStoreFlag(var)) {
     goto f;
   } else if(!isTouched()) {
     goto t;
-  } else { 
+  } else {
     Assert(isSort(var_e)); // must be constraint variable
-    
+
     OzCtVariable * ctvar = tagged2GenCtVar(var);
     OZ_Ct * constr = ctGetConstraint();
-    
+
     if (constr->isValue()) {
       // a variable has been constrained to a value
 
@@ -232,24 +319,24 @@ OZ_Boolean OZ_CtVar::tell(void)
     } else {
 
       OZ_CtWakeUp wakeup_descr = ctGetWakeUpDescrptor();
-      
+
       ctvar->propagate(wakeup_descr, pc_propagator);
 
       if (isState(glob_e)) {
-	OzCtVariable * locctvar = new OzCtVariable(constr, 
+	OzCtVariable * locctvar = new OzCtVariable(constr,
 						     ctvar->getDefinition(),
 						     oz_currentBoard());
 	OZ_Term * loctaggedctvar = newTaggedCVar(locctvar);
-	
+
 	ctRestoreConstraint();
 
 	DoBindAndTrailAndIP(varPtr, makeTaggedRef(loctaggedctvar),
 			    locctvar, ctvar);
-      } 
+      }
 
       goto t;
     }
-  } 
+  }
 
 t:
   return OZ_TRUE;
@@ -257,11 +344,11 @@ t:
 f:
   return OZ_FALSE;
 }
-
+#endif /* CORRECT_UNIFY */
 
 void OZ_CtVar::fail(void)
 {
-  if (isSort(val_e)) 
+  if (isSort(val_e))
     return;
   if (isState(encap_e)) {
     unpatchReifiedCt(var);
