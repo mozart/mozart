652e4931cdb7f1883ade08a20631d7d53a323d6d
kornstae <kornstae@ada56829-ad1f-0410-b00f-83cda6628aec>
1999-10-11 20:51:51 +0000

    Don't use the command line provided as argument to WinMain     since when passed by mingw32 it is gargled.
    git-svn-id: https://gforge.info.ucl.ac.be/svn/mozart@12149 ada56829-ad1f-0410-b00f-83cda6628aec

--- platform/mswindows/ozwrapperw.cc
+++ platform/mswindows/ozwrapperw.cc
@@ -1,12 +1,10 @@
 /*
  *  Authors:
- *    Ralf Scheidhauer (scheidhr@dfki.de)
- * 
- *  Contributors:
- *    optional, Contributor's name (Contributor's email address)
+ *    Ralf Scheidhauer <scheidhr@dfki.de>
+ *    Leif Kornstaedt <kornstae@ps.uni-sb.de>
  * 
  *  Copyright:
- *    Organization or Person (Year(s))
+ *    Ralf Scheidhauer and Leif Kornstaedt, 1999
  * 
  *  Last change:
  *    $Date$ by $Author$
@@ -35,14 +33,36 @@
 #include <process.h>
 #include "misc.cc"
 
+#define ISSPACE(a)	(a == ' ' || a == '\t')
+
 int WINAPI
 WinMain(HINSTANCE /*hInstance*/, HINSTANCE /*hPrevInstance*/,
-	LPSTR lpszCmdLine, int /*nCmdShow*/)
+	LPSTR /*lpszCmdLine*/, int /*nCmdShow*/)
 {
   char buffer[5000];
   sprintf(buffer,"ozenginew.exe \"");
   int len = strlen(buffer);
   GetModuleFileName(NULL, buffer+len, sizeof(buffer)-len);
+
+  //
+  // We don't use the command line we got as argument since
+  // this is not correctly preprocessed by mingw32.
+  //
+
+  char *lpszCmdLine = GetCommandLine();
+  // Skip whitespace and application name
+  if (lpszCmdLine) {
+    while (ISSPACE(*lpszCmdLine)) {
+      lpszCmdLine++;
+    }
+
+    int isQuoted = 0;
+    while (*lpszCmdLine != '\0' && (!ISSPACE(*lpszCmdLine) || isQuoted)) {
+      if (*lpszCmdLine == '\"')
+	isQuoted = !isQuoted;
+      lpszCmdLine++;
+    }
+  }
 
   strcat(buffer,"\" ");
   strcat(buffer,lpszCmdLine);
