c6155c12f4eb0b4684ba7e6b142ec15fbcc4e4ba
schulte <schulte@ada56829-ad1f-0410-b00f-83cda6628aec>
1999-12-09 15:19:52 +0000

    Fixed #461

--- platform/emulator/components.cc
+++ platform/emulator/components.cc
@@ -954,20 +954,36 @@ OZ_Return getURL(const char *url, TaggedRef out, URLAction act)
 
 #include <ctype.h>
 
-inline int toHex(char c) {
-  return
-    ('0' <= c && c <= '9')?(c-'0'):
-    (('a' <= c && c <= 'f')?(c-'a'):
-     (('A' <= c && c <= 'F')?(c-'A'):-1));
+inline unsigned char toHex(char c) {
+  switch (c) {
+  case '0': return 0;
+  case '1': return 1;
+  case '2': return 2;
+  case '3': return 3;
+  case '4': return 4;
+  case '5': return 5;
+  case '6': return 6;
+  case '7': return 7;
+  case '8': return 8;
+  case '9': return 9;
+  case 'a': case 'A': return 10;
+  case 'b': case 'B': return 11;
+  case 'c': case 'C': return 12;
+  case 'd': case 'D': return 13;
+  case 'e': case 'E': return 14;
+  case 'f': case 'F': return 15;
+  default:
+    return 16;
+  }
 }
-static void urlDecode(const char*s1,char*s2)
-{
+
+inline void urlDecode(const char*s1, char*s2) {
   while (*s1 != '\0') {
-    int i1,i2;
+    unsigned char i1,i2;
     if (*s1 == '%' &&
-        (i1=toHex(s1[1]))>=0 &&
-        (i2=toHex(s1[2]))>=0) {
-      *s2++ = i1*16+i2;
+        (i1 = toHex(s1[1]))<16 &&
+        (i2 = toHex(s1[2]))<16) {
+      *s2++ = (char) (i1*16 + i2);
       s1 += 3;
     }
     else
