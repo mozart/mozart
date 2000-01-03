dc583fdd037d624060ec81659049416353cc9118
tmueller <tmueller@ada56829-ad1f-0410-b00f-83cda6628aec>
2000-01-03 13:01:31 +0000

    fixed typo and removed extern "C" {}

--- doc/cpitut/programs/sync.nfi.cc
+++ doc/cpitut/programs/sync.nfi.cc
@@ -1,12 +1,9 @@
-extern "C"
+OZ_C_proc_interface *oz_init_module(void)
 {
-  OZ_C_proc_interface *oz_init_module(void)
-  {
-    static OZ_C_proc_interface i_table[] = {
-      {"connect", 2, 0, connect},
-      {0, 0, 0, 0}
-    };
+  static OZ_C_proc_interface i_table[] = {
+    {"connect", 2, 0, connect},
+    {0, 0, 0, 0}
+  };
 
-    return i_table;
-  }
+  return i_table;
 }
