fa95539c36cf86dfa709dc3c992985b9a9b97538
tmueller <tmueller@ada56829-ad1f-0410-b00f-83cda6628aec>
2000-01-03 13:01:31 +0000

    fixed typo and removed extern "C" {}
    git-svn-id: https://gforge.info.ucl.ac.be/svn/mozart@12435 ada56829-ad1f-0410-b00f-83cda6628aec

--- doc/cpitut/programs/ex_a.6.cc
+++ doc/cpitut/programs/ex_a.6.cc
@@ -2,19 +2,14 @@
 OZ_BI_proto(fd_init);
 OZ_BI_proto(fd_add);
 
-extern "C"
+OZ_C_proc_interface *oz_init_module(void)
 {
-  
-  OZ_C_proc_interface *oz_init_module(void)
-  {
-    static OZ_C_proc_interface i_table[] = {
-      {"init", 0, 0, fd_init},
-      {"add", 3, 0, fd_add},
-      {0,0,0,0}
-    };
-    
-    printf("addition propagator loaded\n");
-    return i_table;
-  }
-}
+  static OZ_C_proc_interface i_table[] = {
+    {"init", 0, 0, fd_init},
+    {"add", 3, 0, fd_add},
+    {0,0,0,0}
+  };
 
+  printf("addition propagator loaded\n");
+  return i_table;
+}
