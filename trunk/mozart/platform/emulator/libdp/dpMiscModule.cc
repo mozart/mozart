5324362fcbba0b3f571ac43a48066cc34f925767
schulte <schulte@ada56829-ad1f-0410-b00f-83cda6628aec>
2000-01-06 13:35:58 +0000

    Use socklen_t if detected by configure
    git-svn-id: https://gforge.info.ucl.ac.be/svn/mozart@12454 ada56829-ad1f-0410-b00f-83cda6628aec

--- trunk/mozart/platform/emulator/libdp/dpMiscModule.cc
+++ trunk/mozart/platform/emulator/libdp/dpMiscModule.cc
@@ -293,7 +293,7 @@ OZ_BI_define(BIsockoptBroadcast,1,0)
   int on = 1;
   //
   if (
-#if !defined(WINDOWS) && !defined(__FreeBSD__)
+#ifdef HAVE_SOCKLEN_T
       setsockopt(desc, SOL_SOCKET, SO_BROADCAST,
 		 (void *) &on, (socklen_t) sizeof(on))
 #else
