31b99480e264dea2f59935c1a2e34c7e9410ce28
tmueller <tmueller@ada56829-ad1f-0410-b00f-83cda6628aec>
2000-03-20 18:51:34 +0000

    updated to improved cpi

--- doc/cpitut/ri_class_rivar.hh
+++ doc/cpitut/ri_class_rivar.hh
@@ -1,41 +1,68 @@
 class RIVar : public OZ_CtVar {
 private:
-  RI * _ri_ref, _ri;
+
+  RI * _ref;
+  RI _copy, _encap;
 
   RIProfile _rip;
 
 protected:
-  virtual void ctSetValue(OZ_Term t) {
-    _ri.init(t);
-    _ri_ref = &_ri;
+
+  virtual void ctSetValue(OZ_Term t)
+  {
+    _copy.init(t);
+    _ref = &_copy;
   }
-  virtual OZ_Ct * ctRefConstraint(OZ_Ct * c) {
-    return _ri_ref = (RI *) c;
+
+  virtual OZ_Ct * ctRefConstraint(OZ_Ct * c)
+  {
+    return _ref = (RI *) c;
   }
-  virtual OZ_Ct * ctSaveConstraint(OZ_Ct * c) {
-    _ri = *(RI *) c;
-    return c;
+
+  virtual OZ_Ct * ctSaveConstraint(OZ_Ct * c)
+  {
+    _copy = *(RI *) c;
+    return &_copy;
   }
-  virtual void ctRestoreConstraint(void) {
-    *_ri_ref = _ri;
+
+  virtual OZ_Ct * ctSaveEncapConstraint(OZ_Ct * c)
+  {
+    _encap = *(RI *) c;
+    return &_encap;
   }
-  virtual void ctSetConstraintProfile(void) {
-    _rip = *_ri_ref->getProfile();
+
+  virtual void ctRestoreConstraint(void)
+  {
+    *_ref = _copy;
   }
-  virtual OZ_CtProfile * ctGetConstraintProfile(void) {
+
+  virtual void ctSetConstraintProfile(void)
+  {
+    _rip = *_ref->getProfile();
+  }
+
+  virtual OZ_CtProfile * ctGetConstraintProfile(void)
+  {
     return &_rip;
   }
-  virtual OZ_Ct * ctGetConstraint(void) {
-    return _ri_ref;
+
+  virtual OZ_Ct * ctGetConstraint(void)
+  {
+    return _ref;
   }
 
 public:
+
   RIVar(void) : OZ_CtVar() { }
+
   RIVar(OZ_Term t) : OZ_CtVar() { read(t); }
 
-  virtual OZ_Boolean isTouched(void) const  {
-    return _ri_ref->isTouched(_rip);
+  virtual OZ_Boolean isTouched(void) const
+  {
+    return _ref->isTouched(_rip);
   }
-  RI &operator * (void) { return *_ri_ref; }
-  RI * operator -> (void) { return _ri_ref; }
+
+  RI &operator * (void) { return *_ref; }
+  RI * operator -> (void) { return _ref; }
+
 };
