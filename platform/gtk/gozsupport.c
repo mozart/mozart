ff11743659bfa6daec32621b747f15fddf3d7ec5
schulte <schulte@ada56829-ad1f-0410-b00f-83cda6628aec>
2000-04-12 13:22:33 +0000

    On the way, bit Iam a chicken

---
+++ platform/gtk/gozsupport.c
@@ -0,0 +1,177 @@
+/*
+
+  $Id$
+
+  Author:
+  Andreas Simon <asimon@ps.uni-sb.de>
+
+  Copyright:
+  Andreas Simon (1999)
+
+*/
+
+#include <gtk/gtk.h>
+#include <mozart.h>
+/*#include <gozsupport.h>*/
+#define GOZ_DECLARE_GTKOBJECT(i, val)            OZ_declareForeignType (i, val, GtkObject*)
+
+static OZ_Term signal_port;
+
+OZ_BI_define (ozgtk_initialize_signal_port, 1, 0)
+{
+  OZ_declareTerm (0, port);
+  signal_port = port;
+  return OZ_ENTAILED;
+} OZ_BI_end
+
+/*
+  Process all events in the queue
+ */
+OZ_BI_define (ozgtk_handle_pending_events, 0, 0)
+{
+  while (gtk_events_pending())
+    gtk_main_iteration();
+  return OZ_ENTAILED;
+} OZ_BI_end
+
+/*
+  Put the signal id into the Oz stream
+  This function is not to be exported via the OZ/C-interface
+ */
+void
+signal_marshal (GtkObject * object,
+                gpointer    oz_id,  /* This pointer holds an guint */
+                guint       n_args,
+                GtkArg *    args)
+{
+  /* OZ_warning ("Marshaller: object: %p, n_args: %i; oz_id: %i, args: %p",
+     object, n_args, (guint) oz_id, args); */
+  /* insert signal into the Oz signal queue */
+  OZ_send (signal_port, OZ_int ((guint) oz_id));
+  OZ_warning ("Marshaller sent signal %i to port", (guint) oz_id);
+}
+
+OZ_BI_define (ozgtk_signal_connect, 3, 1)
+{
+  /*
+   * The callback function will allways be NULL,
+   * we only use our marshaller
+   *
+   */
+  guint id;
+  GOZ_DECLARE_GTKOBJECT (0, object);
+  OZ_declareTerm (1, _name); /* No strings for signals but atoms */
+  gchar * name = (gchar *) OZ_atomToC(_name);
+  OZ_declareInt (2, oz_id);
+  id = gtk_signal_connect_full (GTK_OBJECT (object),
+                                name,
+                                NULL,
+                                signal_marshal,
+                                (gpointer) oz_id,
+                                NULL, /* TODO: add a destroy notify function */
+                                FALSE, /* TODO: verify if defaults are correct */
+                                FALSE);
+  OZ_RETURN_INT (id);
+} OZ_BI_end
+
+OZ_BI_define (ozgtk_signal_emit_by_name, 2, 0)
+{
+  GOZ_DECLARE_GTKOBJECT (0, object);
+  OZ_declareTerm (1, _name); /* No strings for signals but atoms */
+  gchar * name = (gchar *) OZ_atomToC(_name);
+  gtk_signal_emit_by_name(object, name);
+  return OZ_ENTAILED;
+} OZ_BI_end
+
+GList *
+goz_oz_list_to_g_list(OZ_Term ozlist)
+{
+  guint len;
+  guint i;
+  GList * glist = NULL;
+
+  len = OZ_length(ozlist);
+  g_return_val_if_fail (len > 0, glist);
+  OZ_warning ("length:%i\n", len);
+
+  for (i=0; i < len; i++) {
+    glist = g_list_prepend (glist, (gpointer) OZ_getForeignPointer (OZ_head (ozlist)));
+    ozlist = OZ_tail (ozlist);
+  }
+
+  glist = g_list_reverse (glist); /* prevail order */
+
+  return glist;
+}
+
+/*****************************************************************************
+ * Gtk macros
+ *****************************************************************************/
+
+OZ_BI_define (ozgtk_widget_visible, 1, 1)
+{
+  GOZ_DECLARE_GTKOBJECT (0, wid);
+  OZ_RETURN_BOOL (GTK_WIDGET_VISIBLE(wid));
+} OZ_BI_end
+
+OZ_BI_define (ozgtk_widget_sensitive, 1, 1)
+{
+  GOZ_DECLARE_GTKOBJECT (0, wid);
+  OZ_RETURN_BOOL (GTK_WIDGET_SENSITIVE(wid));
+} OZ_BI_end
+
+OZ_BI_define (ozgtk_widget_set_flags, 2, 0)
+{
+  GOZ_DECLARE_GTKOBJECT (0, wid);
+  OZ_declareInt (1, flag);
+  GTK_WIDGET_SET_FLAGS(wid,flag);
+  return OZ_ENTAILED;
+} OZ_BI_end
+
+OZ_BI_define (ozgtk_widget_unset_flags, 2, 0)
+{
+  GOZ_DECLARE_GTKOBJECT (0, wid);
+  OZ_declareInt (1, flag);
+  GTK_WIDGET_UNSET_FLAGS(wid,flag);
+  return OZ_ENTAILED;
+} OZ_BI_end
+
+/*****************************************************************************
+ * Dialog
+ *****************************************************************************/
+
+OZ_BI_define (ozgtk_dialog_window, 1, 1)
+{
+  OZ_declareForeignType (0, dialog, GtkWidget *);
+  OZ_RETURN (OZ_makeForeignPointer(dialog->window));
+} OZ_BI_end
+
+OZ_BI_define (ozgtk_dialog_vbox, 1, 1)
+{
+  OZ_declareForeignType (0, dialog, GtkDialog *);
+  OZ_RETURN (OZ_makeForeignPointer(dialog->vbox));
+} OZ_BI_end
+
+OZ_BI_define (ozgtk_dialog_action_area, 1, 1)
+{
+  OZ_declareForeignType (0, dialog, GtkDialog *);
+  OZ_RETURN (OZ_makeForeignPointer(dialog->action_area));
+} OZ_BI_end
+
+OZ_BI_define (ozgtk_color_selection_dialog_colorsel, 1, 1)
+{
+  OZ_declareForeignType (0, color_selection_dialog, GtkColorSelectionDialog *);
+  OZ_RETURN (OZ_makeForeignPointer(color_selection_dialog->colorsel));
+} OZ_BI_end
+
+OZ_BI_define (ozgtk_color_selection_dialog_cancelButton, 1, 1)
+{
+  OZ_declareForeignType (0, color_selection_dialog, GtkColorSelectionDialog *);
+  OZ_RETURN (OZ_makeForeignPointer(color_selection_dialog->cancel_button));
+} OZ_BI_end
+
+OZ_BI_define (ozgtk_color_selection_dialog_ok_button, 1, 1)
+{
+  OZ_declareForeignType (0, color_selection_dialog, GtkColorSelectionDialog *);
+  OZ_RETURN (OZ_makeForeignPointer(color_selection_dialog->ok_button));
+} OZ_BI_end
---
+++ platform/gtk/gozsupport.h
@@ -0,0 +1,260 @@
+/*
+
+  $Id$
+
+  Some functions for converting between Oz and GTK+ values
+
+*/
+
+#ifndef __GOZ_SUPPORT_H__
+#define __GOZ_SUPPORT_H__
+
+#include <gtk/gtk.h>
+#include <mozart.h>
+
+/* This is for dummy variable identifiers used for casts */
+#define GOZ_(name) name ## _
+
+/*****************************************************************************
+ * Prototypes
+ *****************************************************************************/
+
+OZ_Term             GOZ_GLIST_TO_OZTERM             (GList * glist);
+OZ_Term             GOZ_GSLIST_TO_OZTERM            (GSList * gslist);
+
+/*****************************************************************************
+ * Declaration of GLib basic types
+ *****************************************************************************/
+
+#define GOZ_DECLARE_GBOOLEAN(i, val)        OZ_declareBool (i, GOZ_(val)); gboolean val = (gboolean) GOZ_(val)
+
+#define GOZ_DECLARE_GPOINTER(i, val)        OZ_declareForeignType (i, val, gpointer)
+#define GOZ_DECLARE_GCONSTPOINTER(i, val)   OZ_declareForeignType (i, val, gconstpointer)
+
+#define GOZ_DECLARE_GCHAR(i, val)           OZ_declareInt (i, GOZ_(val)); gchar val = (gchar) GOZ_(val)
+#define GOZ_DECLARE_GUCHAR(i, val)          OZ_declareInt (i, GOZ_(val)); guchar val = (gchar) GOZ_(val)
+
+#define GOZ_DECLARE_GINT(i, val)            OZ_declareInt (i, GOZ_(val)); gint val = (gint) GOZ_(val)
+#define GOZ_DECLARE_GUINT(i, val)           OZ_declareLong (i, GOZ_(val)); guint val = (guint) GOZ_(val)
+#define GOZ_DECLARE_GSHORT(i, val)          OZ_declareInt (i, GOZ_(val)); gshort val = (gshort) GOZ_(val)
+#define GOZ_DECLARE_GUSHORT(i, val)         OZ_declareInt (i, GOZ_(val)); gushort val = (gushort) GOZ_(val)
+#define GOZ_DECLARE_GLONG(i, val)           OZ_declareLong (i, GOZ_(val)); glong val = (glong) GOZ_(val)
+#define GOZ_DECLARE_GULONG(i, val)          OZ_declareLong (i, GOZ_(val)); gulong val = (gulong) GOZ_(val)
+
+#define GOZ_DECLARE_GINT8(i, val)           OZ_declareInt (i, GOZ_(val)); gint8 val = (gint8) GOZ_(val)
+#define GOZ_DECLARE_GUINT8(i, val)          OZ_declareInt (i, GOZ_(val)); guint16 val = (guint16) GOZ_(val)
+#define GOZ_DECLARE_GINT16(i, val)          OZ_declareInt (i, GOZ_(val)); gint16 val = (gint16) GOZ_(val)
+#define GOZ_DECLARE_GUINT16(i, val)         OZ_declareLong (i, GOZ_(val)); guint16 val = (guint16) GOZ_(val)
+#define GOZ_DECLARE_GINT32(i, val)          OZ_declareLong (i, GOZ_(val)); gint32 val = (gint32) GOZ_(val)
+#define GOZ_DECLARE_GUINT32(i, val)         OZ_declareLong (i, GOZ_(val)); guint32 val = (guint32) GOZ_(val)
+#ifdef G_HAVE_GINT64
+#define GOZ_DECLARE_GINT64(i, val)          OZ_declareLong (i, GOZ_(val)); gint64 val = (gint64) GOZ_(val)
+#define GOZ_DECLARE_GUINT64(i, val)         OZ_declareLong (i, GOZ_(val)); guint64 val = (guint64) GOZ_(val)
+#endif
+
+#define GOZ_DECLARE_GFLOAT(i, val)          OZ_declareFloat (i, GOZ_(val)); gfloat val = (gfloat) GOZ_(val)
+#define GOZ_DECLARE_GDOUBLE(i, val)         OZ_declareFloat (i, GOZ_(val)); gdouble val = (gdouble) GOZ_(val)
+
+#define GOZ_DECLARE_GSIZE(i, val)           OZ_declareLong (i, GOZ_(val)); gsize val = (gsize) GOZ_(val)
+#define GOZ_DECLARE_GSSIZE(i, val)          OZ_declareInt (i, GOZ_(val)); gssize val = (gssize) GOZ_(val)
+
+/*****************************************************************************
+ * Declaration of glib types
+ *****************************************************************************/
+
+GList * goz_oz_list_to_g_list(OZ_Term ozlist);
+
+#define GOZ_DECLARE_GLIST(i, val)           \
+  GList * val;                              \
+  OZ_declareTerm (i, GOZ_(val));            \
+  val = goz_oz_list_to_g_list (GOZ_(val))
+
+#define GOZ_DECLARE_GSLIST(i, val)            GSList * val = NULL;
+#define GOZ_DECLARE_GSLIST2(i, val)           GSList ** val = NULL;
+#define GOZ_DECLARE_GHASHTABLE(i, val)        GHashTable * val = NULL;
+#define GOZ_DECLARE_GQUARK(i, val)            OZ_declareLong (i, GOZ_(val)); GQuark val = (GQuark) GOZ_(val)
+#define GOZ_DECLARE_GSCANNER(i, val)          OZ_declareForeignType (i, val, GScanner *)
+
+#define GOZ_DECLARE_GDESTROYNOTIFY(i, val)    GDestroyNotify val = NULL;
+
+/*****************************************************************************
+ * Declaration of gdk types
+ *****************************************************************************/
+
+/* TODO: atom is unsigned long */
+#define GOZ_DECLARE_GDKATOM(i, val)           OZ_declareLong (i, GOZ_(val)); GdkAtom val = (GdkAtom) GOZ_(val)
+
+#define GOZ_DECLARE_GDKDESTROYNOTIFY(i, val)  GdkDestroyNotify val = NULL;
+#define GOZ_DECLARE_GDKEVENTFUNC(i, val)      GdkEventFunc val = NULL;
+#define GOZ_DECLARE_GDKFILTERFUNC(i, val)     GdkFilterFunc val = NULL;
+#define GOZ_DECLARE_GDKINPUTFUNC(i, val)      GdkInputFunction val = NULL;
+
+/*****************************************************************************
+ * Declaration of gtk types
+ *****************************************************************************/
+
+#define GOZ_DECLARE_GTKARGINFO2(i, val)          GtkArgInfo ** val = NULL;
+#define GOZ_DECLARE_GTKTYPE(i, val)              OZ_declareLong (i, GOZ_(val)); GtkType val = (GtkType) GOZ_(val)
+#define GOZ_DECLARE_GTKTYPE1(i, val)             GtkType * val = NULL;
+
+#define GOZ_DECLARE_GTKCALLBACK(i, val)          GtkCallback val = NULL;
+#define GOZ_DECLARE_GTKCALLBACKMARSHAL(i, val)   GtkCallbackMarshal val = NULL;
+#define GOZ_DECLARE_GTKDESTROYNOTIFY(i, val)     GtkDestroyNotify val = NULL;
+#define GOZ_DECLARE_GTKEMISSIONHOOK(i, val)      GtkEmissionHook val = NULL;
+#define GOZ_DECLARE_GTKFUNCTION(i, val)          GtkFunction val = NULL;
+#define GOZ_DECLARE_GTKIMAGELOADER(i, val)       GtkImageLoader val = NULL;
+#define GOZ_DECLARE_GTKSIGNALDESTROY(i, val)     GtkSignalDestroy val = NULL;
+#define GOZ_DECLARE_GTKSIGNALMARSHAL(i, val)     GtkSignalMarshal val = NULL;
+#define GOZ_DECLARE_GTKSIGNALMARSHALLER(i, val)  GtkSignalMarshaller val = NULL;
+
+#define GOZ_DECLARE_FUNCTION(i, val, func)       func val = NULL; /* TODO */
+
+/*
+  Build a gchar* array out of an Oz list with virtual string elements
+  TODO: Boy, you really have to check this!!!
+*/
+#define OZ_declareGCharArrayFromOzListOfVS(ARG, ARRAY)                        \
+  gchar   **ARRAY;                                                            \
+  {                                                                           \
+    gint    length;                                                           \
+    int     dummy;                                                            \
+    int     i;                                                                \
+    OZ_declareTerm (ARG, list);                                               \
+                                                                              \
+    length = OZ_length (list);                                                \
+    g_assert (length >= 0);                                                   \
+    ARRAY = g_new (gchar *, _length + 1);                                     \
+    for (i=0; i < length; i++) {                                              \
+      ARRAY[arg] = g_strdup (OZ_virtualStringToC (OZ_head (list), &_dummy))   \
+      list = OZ_tail (list);                                                  \
+    }                                                                         \
+    ARRAY[_length] = NULL;                                                    \
+  }
+
+#define OZ_declareArrayFromOzList(ARG, VAR, TYPE)                             \
+  TYPE *VAR;                                                                  \
+  {                                                                           \
+    int  i;                                                                   \
+    int len;                                                                  \
+    OZ_declareTerm (ARG, list);                                               \
+                                                                              \
+    len = OZ_length (list);                                                   \
+    g_assert (len >= 0);                                                      \
+    for (i=0; i < len; i++) {                                                 \
+    }                                                                         \
+  }
+
+#define OZ_declareArray(ARG, VAR, TYPE) \
+TYPE VAR;
+
+#define OZ_argInfo(ARG_INFO) \
+(OZ_nil())
+
+#define OZ_declareForeignArray(ARG, VAR, TYPE) \
+TYPE VAR;
+
+/*
+  Oz list of string elements to C array (gchar ***)
+*/
+#define OZ_declareStringArray(ARG, VAR) \
+gchar *** VAR; \
+{ \
+  int len; \
+  int i, dummy; \
+  char * string; \
+\
+  VAR = g_new (gchar **, len); \
+  OZ_declareTerm (ARG, term); \
+  len = OZ_length (term); \
+  for (i=0; i<len; i++) { \
+    string = OZ_stringToC (OZ_head (term), &dummy); \
+    term = OZ_tail (term); \
+    VAR[i] = &string; \
+  } \
+}
+
+#define OZ_declareGdkEvent(ARG, VAR) \
+GdkEvent * VAR;
+
+#define OZ_declareGdkFunc(ARG, VAR, TYPE) \
+TYPE VAR;
+
+#define OZ_declareGdkEventFunc(ARG, VAR) \
+OZ_declareGdkFunc(ARG, VAR, GdkEventFunc)
+
+#define OZ_declareGdkInputFunction(ARG, VAR) \
+OZ_declareGdkFunc(ARG, VAR, GdkInputFunction)
+
+#define OZ_declareGdkFilterFunc(ARG, VAR) \
+OZ_declareGdkFunc(ARG, VAR, GdkFilterFunc)
+
+#define OZ_declareGdkDestroyNotify(ARG, VAR) \
+OZ_declareGdkFunc(ARG, VAR, GdkDestroyNotify)
+
+#define OZ_declareGDestroyNotify(ARG, VAR) \
+OZ_declareGdkFunc(ARG, VAR, GDestroyNotify)
+
+#define OZ_declareGtkDestroyNotify(ARG, VAR) \
+OZ_declareGdkFunc(ARG, VAR, GtkDestroyNotify)
+
+/*****************************************************************************
+ * Conversions from C to Oz
+ *****************************************************************************/
+
+/* gdkEvent to Oz record (OZ_term *) */
+OZ_Term
+OZ_gdkEvent(GdkEvent *event) {
+  OZ_Term     record;
+  OZ_Term     features[2];
+  OZ_Term     arity;
+
+  features[0] = OZ_atom ("type");
+  features[1] = OZ_atom ("window");
+  features[2] = OZ_atom ("send_event");
+  arity = OZ_toList (3, features);
+
+  record = OZ_record (OZ_atom ("gdkEvent"), arity);
+
+  OZ_putSubtree (record, features[0], OZ_int ((int) (*(GdkEventAny *) event).type));
+  OZ_putSubtree (record, features[1], OZ_makeForeignPointer ((*(GdkEventAny *) event).window));
+  OZ_putSubtree (record, features[2], OZ_int ((*(GdkEventAny *) event).send_event));
+
+  return record;
+}
+
+/* Convert a GList to an Oz list of foreign elements */
+OZ_Term
+GOZ_GLIST_TO_OZTERM (GList * glist) {
+  OZ_Term    ozlist;
+  gpointer   element;
+
+  ozlist = OZ_nil ();
+
+  element = g_list_last (glist);
+  do {
+    ozlist = OZ_cons (OZ_makeForeignPointer (element) , ozlist);
+  } while (element = g_list_previous (glist));
+
+  g_list_free (glist);
+
+  return ozlist;
+}
+
+/* Convert a GSList to an Oz list of foreign elements */
+OZ_Term
+GOZ_GSLIST_TO_OZTERM (GSList * gslist) {
+  OZ_Term ozlist;
+
+  ozlist = OZ_nil ();
+  return ozlist;
+}
+
+/*****************************************************************************
+ * Return values
+ *****************************************************************************/
+
+#define GOZ_RETURN_GFLOAT(val)               OZ_RETURN (OZ_float (val))
+#define GOZ_RETURN_GCHAR2(val)               OZ_RETURN (OZ_nil ()) /* TODO */
+#define GOZ_RETURN_GNODE(val)                OZ_RETURN (OZ_nil ()) /* TODO */
+
+#endif /* __GOZ_SUPPORT_H__ */
