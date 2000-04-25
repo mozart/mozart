d41f8b8df71ecdb298f78bdb8c8b1741af0eba3e
asimon <asimon@ada56829-ad1f-0410-b00f-83cda6628aec>
2000-04-25 13:16:49 +0000

    Update
    git-svn-id: https://gforge.info.ucl.ac.be/svn/mozart@12759 ada56829-ad1f-0410-b00f-83cda6628aec

--- platform/gtk/gozwright.pl
+++ platform/gtk/gozwright.pl
@@ -1,16 +1,25 @@
 #!/usr/bin/perl -w
-###############################################################################
 #
-# $Id$
+# Authors:
+#   Andreas Simon (2000)
 #
-#                                Author:
-#                   Andreas Simon <asimon@ps.uni-sb.de>
+# Copyright:
+#   Andreas Simon (2000)
 #
-#                               Copyright:
-#                          Andreas Simon (1999-2000)
+# Last change:
+#   $Date$ by $Author$
+#   $Revision$
 #
+# This file is part of Mozart, an implementation
+# of Oz 3:
+#   http://www.mozart-oz.org
 #
-###############################################################################
+# See the file "LICENSE" or
+#   http://www.mozart-oz.org/LICENSE.html
+# for information on usage and redistribution
+# of this file, and for a DISCLAIMER OF ALL
+# WARRANTIES.
+#
 
 use Getopt::Long;
 use POSIX qw(strftime);
