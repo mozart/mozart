686f71a76a29cc5433e7e95d1987235fae68db06
tmueller <tmueller@ada56829-ad1f-0410-b00f-83cda6628aec>
2000-05-15 11:23:15 +0000

    compiles now with -Wall and Werror

--- platform/emulator/libfset/filter.hh
+++ platform/emulator/libfset/filter.hh
@@ -1,3 +1,29 @@
+/*
+ *  Authors:
+ *    Tobias Mueller (tmueller@ps.uni-sb.de)
+ *
+ *  Contributors:
+ *    optional, Contributor's name (Contributor's email address)
+ *
+ *  Copyright:
+ *    Organization or Person (Year(s))
+ *
+ *  Last change:
+ *    $Date$ by $Author$
+ *    $Revision$
+ *
+ *  This file is part of Mozart, an implementation
+ *  of Oz 3:
+ *     http://www.mozart-oz.org
+ *
+ *  See the file "LICENSE" or
+ *     http://www.mozart-oz.org/LICENSE.html
+ *  for information on usage and redistribution
+ *  of this file, and for a DISCLAIMER OF ALL
+ *  WARRANTIES.
+ *
+ */
+
 #ifndef __FILTER_HH_
 #define __FILTER_HH_
 
@@ -173,26 +199,7 @@ public:  //
   // have to be passed as arguments and the replacment react on the
   // same events at the respective parameters
   OZ_Service &replace_propagator(OZ_Propagator * prop,
-                                 int vars_drop = 0, /* (OZ_CPIVar *) */ ...)
-  {
-    DSP(("request replace\n"));
-    if (!_closed) {
-      _actions[_nb_actions]._what = _actions_t::_serv_replace;
-      _actions[_nb_actions]._action_params._replacement = prop;
-      _nb_actions += 1;
-      OZ_ASSERT(_nb_actions =< _max_actions);
-    }
-    //
-    va_list ap;
-    va_start(ap, vars_drop);
-    for (int i = vars_drop; i--; ) {
-      OZ_CPIVar * cpivar = va_arg(ap, OZ_CPIVar *);
-      cpivar->dropParameter();
-    }
-    //
-    _closed = 1;
-    return *this;
-  }
+                                 int vars_drop = 0, /* (OZ_CPIVar *) */ ...);
   // changes state of propagator, propagator shall
   // not be set `scheduled' (hence, `impose_propagator' does not work)
   void condens_vector(OZ_FDIntVarVector &);
