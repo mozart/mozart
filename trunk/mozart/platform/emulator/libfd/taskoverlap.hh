e9dbddbb6e15f0f15bd45e79b5c2374b42fb662a
tmueller <tmueller@ada56829-ad1f-0410-b00f-83cda6628aec>
2000-05-23 13:25:00 +0000

    taskoverlap via filterinterface
    git-svn-id: https://gforge.info.ucl.ac.be/svn/mozart@12864 ada56829-ad1f-0410-b00f-83cda6628aec

--- trunk/mozart/platform/emulator/libfd/taskoverlap.hh
+++ trunk/mozart/platform/emulator/libfd/taskoverlap.hh
@@ -31,11 +31,9 @@
 
 //-----------------------------------------------------------------------------
 
-class TasksOverlapPropagator : public Propagator_D_I_D_I_D {
-  friend INIT_FUNC(fdp_init);
-
+template <class SERVICE, class FDVAR>
+class FilterTasksOverlap : public OZ_StatefulFilter {
 private:
-  static OZ_PropagatorProfile profile;
   PEL_PersistentFDIntVar _cl1_t1, _cl1_t2, _cl1_o;
   PEL_PersistentFDIntVar _cl2_t1, _cl2_t2, _cl2_o;
   PEL_PersistentFDIntVar _cl3_t1, _cl3_t2, _cl3_o; 
@@ -47,44 +45,97 @@ private:
   int _first;
   //
 public:
-  TasksOverlapPropagator(OZ_Term x, OZ_Term xd, OZ_Term y, OZ_Term yd,
-			 OZ_Term o);
+  FilterTasksOverlap(int xd, int yd) {
+    _first = 0;
+    // clause 1
+    {
+      _engine_cl1.init();
+      make_PEL_GreaterOffset(_engine_cl1, _cl1_t1, xd, _cl1_t2);
+      make_PEL_GreaterOffset(_engine_cl1, _cl1_t2, yd, _cl1_t1);
+      
+      (*_cl1_t1).initFull();
+      (*_cl1_t2).initFull();
+      (*_cl1_o).initSingleton(1);
+    }
+    // clause 2
+    {
+      _engine_cl2.init();
+      make_PEL_LessEqOffset(_engine_cl2, _cl2_t1, xd, _cl2_t2);
+      (*_cl2_t1).initFull();
+      (*_cl2_t2).initFull();
+      (*_cl2_o).initSingleton(0);
+    }
+    // clause 3
+    {
+      _engine_cl3.init();
+      make_PEL_LessEqOffset(_engine_cl3, _cl3_t2, yd, _cl3_t1);
+      //
+      (*_cl3_t1).initFull();
+      (*_cl3_t2).initFull();
+      (*_cl3_o).initSingleton(0);
+    }
+  }
+  virtual void sClone(void) {
+    _engine_cl1.gCollect();
+    _engine_cl2.gCollect();
+    _engine_cl3.gCollect();
+    _cl1_t1.gCollect();
+    _cl1_t2.gCollect();
+    _cl1_o.gCollect();
+    _cl2_t1.gCollect();
+    _cl2_t2.gCollect();
+    _cl2_o.gCollect();
+    _cl3_t1.gCollect();
+    _cl3_t2.gCollect();
+    _cl3_o.gCollect();
+  }
+  virtual void gCollect(void) {
+    _engine_cl1.sClone();
+    _engine_cl2.sClone();
+    _engine_cl3.sClone();
+    _cl1_t1.sClone();
+    _cl1_t2.sClone();
+    _cl1_o.sClone();
+    _cl2_t1.sClone();
+    _cl2_t2.sClone();
+    _cl2_o.sClone();
+    _cl3_t1.sClone();
+    _cl3_t2.sClone();
+    _cl3_o.sClone();
+  }
+  SERVICE &filter(SERVICE & s, FDVAR &x, int xd, FDVAR &y, int yd, FDVAR &o);
+};
+
+//-----------------------------------------------------------------------------
+
+class TasksOverlapPropagator : public Propagator_D_I_D_I_D {
+  friend INIT_FUNC(fdp_init);
+  //
+private:
+  //
+  static OZ_PropagatorProfile profile;
+  //
+  FilterTasksOverlap<OZ_Service, OZ_FDIntVar> tasksoverlap;
+  //
+public:
+  //
+  TasksOverlapPropagator(OZ_Term x, OZ_Term xd,
+			 OZ_Term y, OZ_Term yd,
+			 OZ_Term o)
+    : Propagator_D_I_D_I_D(x, xd, y, yd, o), 
+    tasksoverlap(OZ_intToC(xd), OZ_intToC(yd)) { }
   //
   virtual OZ_Return propagate(void);
   virtual OZ_PropagatorProfile * getProfile(void) const { return &profile; }
   virtual void gCollect(void) {
     Propagator_D_I_D_I_D::gCollect();
     //
-    // here goes the additional stuff:
-    _engine_cl1.gCollect();
-    _engine_cl2.gCollect();
-    _engine_cl3.gCollect();
-    _cl1_t1.gCollect(); 
-    _cl1_t2.gCollect(); 
-    _cl1_o.gCollect();
-    _cl2_t1.gCollect();
-    _cl2_t2.gCollect();
-    _cl2_o.gCollect();
-    _cl3_t1.gCollect();
-    _cl3_t2.gCollect(); 
-    _cl3_o.gCollect();
+    tasksoverlap.gCollect();
   }
   virtual void sClone(void) {
     Propagator_D_I_D_I_D::sClone();
     //
-    // here goes the additional stuff:
-    _engine_cl1.sClone();
-    _engine_cl2.sClone();
-    _engine_cl3.sClone();
-    _cl1_t1.sClone(); 
-    _cl1_t2.sClone(); 
-    _cl1_o.sClone();
-    _cl2_t1.sClone();
-    _cl2_t2.sClone();
-    _cl2_o.sClone();
-    _cl3_t1.sClone();
-    _cl3_t2.sClone(); 
-    _cl3_o.sClone();
+    tasksoverlap.sClone();
   }
   virtual size_t sizeOf(void) { return sizeof(TasksOverlapPropagator); }
 };
