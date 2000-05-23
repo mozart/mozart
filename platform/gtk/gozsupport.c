8a65ab86d697820b5db55b50fac62609cbb2a625
asimon <asimon@ada56829-ad1f-0410-b00f-83cda6628aec>
2000-05-23 14:38:55 +0000

    - version check for GTK+ - minor bug fixes

--- platform/gtk/gozsupport.c
+++ platform/gtk/gozsupport.c
@@ -1,14 +1,25 @@
 /*
-
-  $Id$
-
-  Author:
-  Andreas Simon <asimon@ps.uni-sb.de>
-
-  Copyright:
-  Andreas Simon (1999)
-
-*/
+ * Authors:
+ *   Andreas Simon (1999-2000)
+ *
+ * Copyright:
+ *   Andreas Simon (1999-2000)
+ *
+ * Last change:
+ *   $Date$ by $Author$
+ *   $Revision$
+ *
+ * This file is part of Mozart, an implementation
+ * of Oz 3:
+ *   http://www.mozart-oz.org
+ *
+ * See the file "LICENSE" or
+ *   http://www.mozart-oz.org/LICENSE.html
+ * for information on usage and redistribution
+ * of this file, and for a DISCLAIMER OF ALL
+ * WARRANTIES.
+ *
+ */
 
 #include <gtk/gtk.h>
 #include <mozart.h>
