4dd3ff549c95c44decd78ee7772fd35837d55ba9
asimon <asimon@ada56829-ad1f-0410-b00f-83cda6628aec>
2000-05-23 14:38:55 +0000

    - version check for GTK+ - minor bug fixes
    git-svn-id: https://gforge.info.ucl.ac.be/svn/mozart@12866 ada56829-ad1f-0410-b00f-83cda6628aec

--- trunk/mozart/platform/gtk/gozsupport.h
+++ trunk/mozart/platform/gtk/gozsupport.h
@@ -1,10 +1,25 @@
 /*
-
-  $Id$
-
-  Some functions for converting between Oz and GTK+ values
-
-*/
+ * Authors:
+ *   Andreas Simon (1999-2000)
+ *
+ * Copyright:
+ *   Andreas Simon (1999-2000)
+ *
+ * Last change:
+ *   $Date$
+ *   $Revision$
+ *
+ * This file is part of Mozart, an implementation
+ * of Oz 3:
+ *   http://www.mozart-oz.org
+ *
+ * See the file "LICENSE" or
+ *   http://www.mozart-oz.org/LICENSE.html
+ * for information on usage and redistribution
+ * of this file, and for a DISCLAIMER OF ALL
+ * WARRANTIES.
+ *
+ */
 
 #ifndef __GOZ_SUPPORT_H__
 #define __GOZ_SUPPORT_H__
