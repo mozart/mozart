1c5380809c7044ec08a83c97d0dfc19c11eb9ef7
annan <annan@ada56829-ad1f-0410-b00f-83cda6628aec>
2000-05-29 09:00:35 +0000

    Commented

--- platform/emulator/libdp/timers.hh
+++ platform/emulator/libdp/timers.hh
@@ -3,6 +3,15 @@
 
 #include "base.hh"
 
+/*
+  class Timers contains an abstraction on top of the task manager to make
+  it easy to set and reset an arbitrary number of timers.
+  The users are responsible of storing a timerelement that is handled by Timers
+  and contains necesary information. This may not be alterd by the users, and
+  must be cleared by clearTimer before the user seises to exist.
+  Elapsed timers cause procedures provided when the timer is set to be invoked.
+*/
+
 // A wake-up-procedure should return FALSE if it does not wish to be reinvoked.
 // It must then be sure to call setTimer with a NULL ptr the next time
 typedef Bool (*TimerWakeUpProc)(void *);
@@ -19,11 +28,19 @@ public:
   Timers();
   ~Timers();
 
+  // methods to set and clear timers. te is a TimerElement handled only by
+  // Timers but stored by users. timeToWait is the desired timeout. proc is the
+  // procedure to be called at wakeup with argument arg. proc should return
+  // TRUE if it wishes to be awoken after timeToWait again and FALSE otherwise.
+  // arg may be anything and is not used inside Timers, just put in the call of
+  // proc.
+  //
   // If te!=NULL it must be a te set by this timers-object.
   void setTimer(TimerElement *&te,int timeToWait,
                TimerWakeUpProc proc,void *arg);
   void clearTimer(TimerElement *&te);
 
+  // Internal methods, public for technical reasons.
   Bool checkTimers(unsigned long time);
   Bool wakeUpTimers(unsigned long time);
 };
