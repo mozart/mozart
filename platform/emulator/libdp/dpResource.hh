03730dc9b0a76ae572bf434f57bf9f69e78bc6f3
popow <popow@ada56829-ad1f-0410-b00f-83cda6628aec>
2000-07-30 23:30:50 +0000

    hmm... no comments. The 'equality' bug seems to be fixed by now, though there is another (much more serious) problem with distributed resources: variables are not handled properly!
    git-svn-id: https://gforge.info.ucl.ac.be/svn/mozart@12968 ada56829-ad1f-0410-b00f-83cda6628aec

--- platform/emulator/libdp/dpResource.hh
+++ platform/emulator/libdp/dpResource.hh
@@ -79,24 +79,67 @@ public:
   ResourceHashTable(int i):GenHashTable(i){}
   
   void add(TaggedRef entity, int index){
+    // kost@ : this is what we can deal with:
+    Assert((!oz_isRef(entity) && !oz_isVariable(entity)) ||
+	   (oz_isRef(entity) && oz_isVariable(*tagged2Ref(entity))));
     Assert(find(entity)==RESOURCE_NOT_IN_TABLE);
     int hvalue = hash(entity);
     GenHashTable::htAdd(hvalue,(GenHashBaseKey*)entity,
 			(GenHashEntry*)index);}
-  
+
   int find(TaggedRef entity){
+    Assert((!oz_isRef(entity) && !oz_isVariable(entity)) ||
+	   (oz_isRef(entity) && oz_isVariable(*tagged2Ref(entity))));
     int hvalue = hash(entity);
     GenHashNode *aux = htFindFirst(hvalue);
     while(aux){
       int OTI = (int) aux->getEntry();
       OwnerEntry *oe=OT->getEntry(OTI);
-      if(oe && (!oe->isTertiary()) &&oe->getRef()==entity){
-	return OTI;}
-      else{
-	if(htSub(hvalue,aux));}
-      aux = htFindNext(aux, hvalue);}
+      // 
+      // kost@ : that's how it was looking before:
+      /* if(oe && (!oe->isTertiary()) && oe->getRef()==entity) { */
+      // kost@: i've introduced now this assertion:
+      // 'ref' owner entries are deallocated explicitly, so:
+      Assert(oe);
+      if(oe->isRef() && oe->getRef() == entity) {
+	return OTI;
+      } else {
+	// kost@ : what??! why remove!?! It cann't be removed since
+	// there can be multiple entries with the same hash value.
+	// So, i've uncommented it:
+	// if(htSub(hvalue,aux));
+	;
+	// I feel i know the motive for that: it was a sort of GC,
+	// wasn't it? Outdated RHT entries (that is, those that did
+	// not correspond to an alive&corrent OT entry anymore) were
+	// eventually purged;
+      }
+      aux = htFindNext(aux, hvalue);
+    }
     return RESOURCE_NOT_IN_TABLE;
   }
+
+  // kost@ : explicit destruction of entries;
+  void deleteFound(OZ_Term entity) {
+    Assert((!oz_isRef(entity) && !oz_isVariable(entity)) ||
+	   (oz_isRef(entity) && oz_isVariable(*tagged2Ref(entity))));
+    int hvalue = hash(entity);
+    GenHashNode *aux = htFindFirst(hvalue);
+    while (aux) {
+      OZ_Term te;
+      GenCast(aux->getBaseKey(), GenHashBaseKey*, te, OZ_Term);   
+      // Additionally, (all) bound variables can be discarded as well:
+      // nobody will ever try to find them;
+      if ((te == entity) ||
+	  (oz_isRef(te) && !oz_isVariable(*tagged2Ref(te)))) {
+	htSub(hvalue, aux);
+	break;
+      } else {
+	aux = htFindNext(aux, hvalue);
+      }
+    }
+  }
+    
   
   void gcResourceTable();
 };
