9a6cbe49595bb5fe487d6ecfe2f95f505b73c476
schulte <schulte@ada56829-ad1f-0410-b00f-83cda6628aec>
2000-10-18 20:15:41 +0000

    Hey you! You stay!
    git-svn-id: https://gforge.info.ucl.ac.be/svn/mozart@13287 ada56829-ad1f-0410-b00f-83cda6628aec

---
+++ platform/emulator/tagged.cc
@@ -0,0 +1,29 @@
+/*
+ *  Authors:
+ *    Ralf Scheidhauer (Ralf.Scheidhauer@ps.uni-sb.de)
+ * 
+ *  Copyright:
+ *    Ralf Scheidhauer 1999
+ * 
+ *  Last change:
+ *    $Date$ by $Author$
+ *    $Revision$
+ * 
+ *  This file is part of Mozart, an implementation 
+ *  of Oz 3:
+ *     http://www.mozart-oz.org
+ * 
+ *  See the file "LICENSE" or
+ *     http://www.mozart-oz.org/LICENSE.html
+ *  for information on usage and redistribution 
+ *  of this file, and for a DISCLAIMER OF ALL 
+ *  WARRANTIES.
+ *
+ */
+
+#if defined(INTERFACE)
+#pragma implementation "tagged.hh"
+#endif
+
+#include "tagged.hh"
+
