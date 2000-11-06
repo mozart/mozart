ca95f2b3217d80ecaccdcc8f5563f9fdc7ee8a94
annan <annan@ada56829-ad1f-0410-b00f-83cda6628aec>
2000-11-06 15:26:49 +0000

    Reset marshaling in messages that are to be resent.
    git-svn-id: https://gforge.info.ucl.ac.be/svn/mozart@13335 ada56829-ad1f-0410-b00f-83cda6628aec

--- platform/emulator/libdp/prioQueues.cc
+++ platform/emulator/libdp/prioQueues.cc
@@ -286,7 +286,9 @@ int PrioQueues::msgAcked(int num,Bool resend,Bool calcrtt) {
   while(cur!=NULL && cur->getMsgNum()>num) {
     prev=cur;
     if(resend) {
+      DebugCode(printf("resend\n");)
       unackedList=cur->next;
+      cur->resetMarshaling();
       u_addFirst(cur,&qs[4-1]);
       noMsgs++;
       cur=unackedList;
