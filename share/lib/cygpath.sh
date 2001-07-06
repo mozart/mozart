e6db3160e603bd366734f95dd592f74be17529a1
kornstae <kornstae@ada56829-ad1f-0410-b00f-83cda6628aec>
2001-07-06 11:18:51 +0000

    Mozart executables expect Windows style file names, not cygwin ones.    Unfortunately this looks a little messy.

---
+++ share/lib/cygpath.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+: ${SRCDIR=.}
+: ${OZPLATFORM=`$SRCDIR/../bin/ozplatform`}
+
+case $OZPLATFORM in
+    win32*)
+        cygpath -w -s $1 | sed 's|\\|/|g'
+        ;;
+    *)
+        echo $1
+        ;;
+esac
