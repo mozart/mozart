0c8846745ba84314d6b8a86a9472f69771e2d36e
duchier <duchier@ada56829-ad1f-0410-b00f-83cda6628aec>
2001-09-05 14:13:31 +0000

    some progress
    git-svn-id: https://gforge.info.ucl.ac.be/svn/mozart@14218 ada56829-ad1f-0410-b00f-83cda6628aec

--- platform/emulator/mozart_task.hh
+++ platform/emulator/mozart_task.hh
@@ -4,7 +4,8 @@
 #include "mozart.h"
 #include <pthread.h>
 
-class OzTaskManager;
+class OzTaskQueue;
+class OzTaskAdmin;
 
 // An OzTask represents some work to be done that may block
 // or require a long time to complete.  Since we don't want
@@ -14,15 +15,15 @@ class OzTaskManager;
 class OzTask {
 private:
   OzTask * _next;
+  friend class OzTaskQueue;
 public:
   OzTask():_next(0){}
-  virtual ~OzTask();
-  virtual void execute(void);
-  virtual void finish(void);
-  friend class OzTaskManager;
+  virtual ~OzTask() {}
+  virtual void execute(void) = 0;
+  virtual void finish(void) = 0;
 };
 
-extern OzTaskManager* OZ_newTaskManager(int,int);
-extern void OZ_taskManagerEnq(OzTaskManager*,OzTask*);
+extern void OZ_taskEnq(OzTask*);
+extern void OZ_taskEnqTiny(OzTask*);
 
 #endif
