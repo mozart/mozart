8791803295eaf364d6e03bda9096c6f0425a707e
duchier <duchier@ada56829-ad1f-0410-b00f-83cda6628aec>
2001-09-05 14:13:31 +0000

    some progress
    git-svn-id: https://gforge.info.ucl.ac.be/svn/mozart@14218 ada56829-ad1f-0410-b00f-83cda6628aec

--- branches/mozart-1-3-0-pthread/mozart/platform/emulator/mozart_task.cc
+++ branches/mozart-1-3-0-pthread/mozart/platform/emulator/mozart_task.cc
@@ -2,63 +2,86 @@
 #include <signal.h>
 #include <time.h>
 
-// an OzTaskManager
+// ===================================================================
+// OzTaskQueue
+//
+// provides the method enq(OzTask*) to enqueue a task for execution by
+// a system thread.  This class-based design makes it possible to have
+// several queues with possibly different characteristics: we use this
+// to provide a `tiny' queue as well as a `normal' queue.  The tiny
+// threads have a very small stack, which, hopefully makes them more
+// economical.
+// ===================================================================
 
-class OzTaskManager {
+static pthread_mutex_t mutex;
+
+class OzTaskQueue {
 public:
-  int total_max;		// at most these many threads may coexists
-  int idle_max;			// at most these many threads may be idle
+  const int total_max = 100;	// at most these many threads may coexists
+  const int idle_sec = 2;	// stays idle at most 2s
   int total;			// number of existing threads
   int idle;			// number of idle threads
-  pthread_mutex_t mutex;
   pthread_cond_t cond;
+  pthread_attr_t attr;
   OzTask * queue_first;		// queue of pending tasks
   OzTask * queue_last;
-  OzTaskManager(int tmax,int imax,int itime);
+  OzTaskQueue(void);
   void enq(OzTask*);
   void work(void);
   void finish(void);
 };
 
-OzTaskManager::OzTaskManager(int tmax,int imax)
-  : total_max(tmax),
-    idle_max(imax),
-    total(0),
+static sigset_t empty_mask;
+
+static OzTaskQueue* queue_tiny;
+static OzTaskQueue* queue_normal;
+
+static int initialized = 0;
+static void task_queues_init()
+{
+  initialized=1;
+  sigemptyset(&empty_mask);
+  pthread_mutex_init(&mutex,NULL);
+  queue_tiny   = new OzTaskQueue;
+  queue_normal = new OzTaskQueue;
+#ifdef _POSIX_THREAD_ATTR_STACKSIZE
+#ifndef OZ_TINY_STACK
+#define OZ_TINY_STACK PTHREAD_STACK_MIN*2
+#endif
+  pthread_attr_setstacksize(&queue_tiny->attr,OZ_TINY_STACK);
+#endif
+}
+
+OzTaskQueue::OzTaskQueue(void)
+  : total(0),
     idle(0),
     queue_first(0),
     queue_last(0)
 {
-  pthread_mutex_init(&mutex,NULL);
   pthread_cond_init(&cond,NULL);
-}
-
-OzTaskManager*
-OZ_newTaskManager(int tmax,int imax)
-{
-  new OzTaskManager(tmax,imax);
+  pthread_attr_init(&attr);
+  pthread_attr_setdetachstate(&attr,PTHREAD_CREATE_DETACHED);
 }
 
 void
-OZ_taskManagerEnq(OzTaskManager*m,OzTask*t)
-{
-  m->enq(t);
+OZ_taskEnqTiny(OzTask*t) {
+  if (!initialized) task_queues_init();
+  queue_tiny->enq(t);
 }
 
-static sigset_t empty_mask;
-static pthread_attr_t thread_attr;
+void
+OZ_taskEnq(OzTask*t) {
+  if (!initialized) task_queues_init();
+  queue_normal->enq(t);
+}
 
-class OzTaskDie : public OzTask {
-public:
-  void execute(void);
-};
-
-static void* OZ_taskExecute(void*x)
+static void* task_execute(void*x)
 {
-  ((OzTaskManager*)x)->work();
+  ((OzTaskQueue*)x)->work();
   return NULL;
 }
 
-void OzTaskManager::enq(OzTask* t)
+void OzTaskQueue::enq(OzTask* t)
 {
   pthread_mutex_lock(&mutex);
   t->_next=0;
@@ -69,24 +92,20 @@ void OzTaskManager::enq(OzTask* t)
     queue_last = t;
   }
   if (idle==0 && total<total_max) {
-    sigset_t new_mask;
     sigset_t old_mask;
-    sigemptyset(&new_mask);
     pthread_sigmask(SIG_SETMASK,&empty_mask,&old_mask);
     pthread_t id;
-    pthread_create(&id,&thread_attr,OZ_taskExecute,(void*)this);
+    pthread_create(&id,&thread_attr,task_execute,(void*)this);
     pthread_sigmask(SIG_SETMASK,&old_mask,NULL);
   }
   pthread_cond_signal(&cond);
   pthread_mutex_unlock(&mutex);
 }
 
-//////////////////////////////////////////////////////////////////////
-
 static OzTask* finish_first = 0;
 static OzTask* finish_last  = 0;
 
-void OzTaskManager::work(void)
+void OzTaskQueue::work(void)
 {
   pthread_mutex_lock(&mutex);
   int redo = 1;
@@ -95,12 +114,13 @@ void OzTaskManager::work(void)
   goto inner_loop;
 
  outer_loop:
-  // we use 2 seconds timeout
-  timeout.tv_sec = time(NULL) + 2;
-  ntimeout.tv_nsec = 0;
+  timeout.tv_sec = time(NULL) + idle_sec;
+  timeout.tv_nsec = 0;
+  // atomically: release lock, wait for condition, acquire lock
   if (pthread_cond_timedwait(&cond,&mutex,&timeout)==ETIMEOUT) redo=0;
 
  inner_loop:
+  // check for work to be done
   while (queue_first) {
     redo=1;
     OzTask* t = queue_first;
@@ -126,9 +146,11 @@ void OzTaskManager::work(void)
   pthread_mutex_unlock(&mutex);
 }
 
-//////////////////////////////////////////////////////////////////////
+// ===================================================================
+// perform task's synchronous fast 2nd step
+// ===================================================================
 
-void OzTaskManager::finish(void)
+void OzTaskQueue::finish(void)
 {
   pthread_mutex_lock(&mutex);
   if (finish_first) {
@@ -141,6 +163,7 @@ void OzTaskManager::finish(void)
       q = t->_next;
       t->_next = 0;
       t->finish();
+      delete t;
     }
   } else pthread_mutex_unlock(&mutex);
 }
