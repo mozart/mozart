ef7162c5c3f84c09312eb21ad9baaf995a27a074
duchier <duchier@ada56829-ad1f-0410-b00f-83cda6628aec>
2002-08-30 12:55:46 +0000

    merged from release branch: renamed port_t to oz_port_t to avoid the name clash on e.g. Darwin due to typedef in /usr/include/mach/port.h (reported by Marc-Antoine Parent)
    git-svn-id: https://gforge.info.ucl.ac.be/svn/mozart@15106 ada56829-ad1f-0410-b00f-83cda6628aec

--- platform/emulator/libdp/dsite.cc
+++ platform/emulator/libdp/dsite.cc
@@ -238,7 +238,7 @@ DSite* myDSite =  NULL;
 // kost@ : that's a part of the boot-up procedure ('perdioInit()');
 // Actually, it is used by 'initNetwork()' because ip, port, timestamp
 // are not known prior its initialization;
-DSite* makeMyDSite(ip_address a, port_t p, TimeStamp &t) {
+DSite* makeMyDSite(ip_address a, oz_port_t p, TimeStamp &t) {
   DSite *s = new DSite(a,p,t);
   s->setMyDSite();
   primarySiteTable->insert(s);
