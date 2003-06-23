d9587e989b44ecdcbc533cf8e51ec2b889b1b17b
duchier <duchier@ada56829-ad1f-0410-b00f-83cda6628aec>
2003-06-23 19:59:13 +0000

    changed 0 to oz_rootBoard(). hum... sorry. BTW I had already fixed PR 1008 in 2001.  I don't know why that wasn't in the release branch too.
    git-svn-id: https://gforge.info.ucl.ac.be/svn/mozart@15543 ada56829-ad1f-0410-b00f-83cda6628aec

--- branches/mozart-1-2-0-gcc-3-2/mozart/platform/emulator/bytedata.hh
+++ branches/mozart-1-2-0-gcc-3-2/mozart/platform/emulator/bytedata.hh
@@ -117,8 +117,8 @@ public:
   virtual void sCloneRecurseV(void) {}
   virtual void gCollectRecurseV(void) {}
   BitString operator=(const BitString&);
-  BitString() : OZ_Extension(0) {}
-  BitString(int w) : OZ_Extension(0), BitData(w) {}
+  BitString() : OZ_Extension(oz_rootBoard()) {}
+  BitString(int w) : OZ_Extension(oz_rootBoard()), BitData(w) {}
   BitString *clone();
 };
 
@@ -207,8 +207,8 @@ public:
   virtual void sCloneRecurseV(void) {}
   virtual void gCollectRecurseV(void) {}
   ByteString operator=(const ByteString&);
-  ByteString() : OZ_Extension(0) {}
-  ByteString(int w) : OZ_Extension(0), ByteData(w) {}
+  ByteString() : OZ_Extension(oz_rootBoard()) {}
+  ByteString(int w) : OZ_Extension(oz_rootBoard()), ByteData(w) {}
   ByteString *clone();
 };
 
