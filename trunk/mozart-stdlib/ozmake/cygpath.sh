cd52ac00455dde2c7604c2343fc5206019b2b4be
glynn <glynn@ada56829-ad1f-0410-b00f-83cda6628aec>
2004-03-31 11:47:23 +0000

    propogate new cygpath.sh from mozart to mozart-stdlib
    this version lower cases the drive letter if present
    
    
    git-svn-id: https://gforge.info.ucl.ac.be/svn/mozart@15851 ada56829-ad1f-0410-b00f-83cda6628aec

--- trunk/mozart-stdlib/ozmake/cygpath.sh
+++ trunk/mozart-stdlib/ozmake/cygpath.sh
@@ -2,7 +2,18 @@
 
 case `uname -s` in
     CYGWIN*)
-	cygpath -w "$1" | sed 's|\\|/|g'
+	pathname=`cygpath -w "$1" | sed 's|\\|/|g'`
+	case "$pathname" in
+	    ?:* )
+		device=`expr substr "$pathname" 1 1 | tr '[:upper:]' '[:lower:]'`
+		pathname=`expr "$pathname" : '.\(:.*\)$'`
+		pathname="$device$pathname"
+		echo "$pathname"
+	    ;;
+	    *)
+		echo "$pathname"
+	    ;;
+	esac
 	;;
     *)
 	echo "$1"
