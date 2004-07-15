f9c891baa16a5f2e3b812fffe15eb4637e161410
popow <popow@ada56829-ad1f-0410-b00f-83cda6628aec>
2004-07-15 18:56:08 +0000

    #1352 fixed;

--- platform/emulator/dictionary.cc
+++ platform/emulator/dictionary.cc
@@ -214,7 +214,7 @@ void DictHashTable::compactify()
   oldEntries = entries;
 
   // can be zero too:
-  int tableSize = (int) ((double) entries * GDT_IDEALENTRIES);
+  int tableSize = (int) ((double) entries / GDT_IDEALENTRIES);
   Assert(tableSize < oldSize);
   sizeIndex--;
   while (sizeIndex >= 0 && dictHTSizes[sizeIndex] >= tableSize)
@@ -345,7 +345,7 @@ DictNode* DictHashTable::getPairsInArray()
   DictNode* acc = out;
 
   //
-  for (int i = dictHTSizes[sizeIndex], ai = 0; i--; ) {
+  for (int i = dictHTSizes[sizeIndex]; i--; ) {
     DictNode* n = &table[i];
     if (!n->isEmpty()) {
       if (!n->isPointer()) {
@@ -429,6 +429,7 @@ OZ_Term DictHashTable::getArityList(OZ_Term tail)
         }
       }
     }
+    Assert(ai == entries);
 
     //
     Order_TaggedRef_By_Feat lt;
@@ -479,6 +480,28 @@ OZ_Term DictHashTable::toRecord(OZ_Term lbl)
     return (newrec->normalize());
   }
 }
+
+#if defined(DEBUG_CHECK)
+void DictHashTable::verify()
+{
+  int tableSize = dictHTSizes[sizeIndex];
+  int acc = 0;
+
+  for (int i = tableSize; i--; ) {
+    DictNode *n = &table[i];
+    if (!n->isEmpty()) {
+      if (!n->isPointer()) {
+        acc++;
+      } else {
+        DictNode *sptr = n->getDictNodeSPtr();
+        DictNode *eptr = n->getDictNodeEPtr();
+        acc += eptr - sptr;
+      }
+    }
+  }
+  Assert(acc == entries);
+}
+#endif
 
 //  OZ_Term DictHashTable::htFindOutline(OZ_Term key)
 //  {
