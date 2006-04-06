1d67271090f06c54d264553eabdcff3877ab461c
raph <raph@ada56829-ad1f-0410-b00f-83cda6628aec>
2006-04-06 16:11:33 +0000

    Rewrote the migratory protocol, and added the "chain" idea.  The manager is now able to diagnose the entity's failure when a proxy crash with the state.
    git-svn-id: https://gforge.info.ucl.ac.be/svn/mozart@16336 ada56829-ad1f-0410-b00f-83cda6628aec

--- platform/dss/src/protocol_migratory.cc
+++ platform/dss/src/protocol_migratory.cc
@@ -3,7 +3,7 @@
  *    Zacharias El Banna (zeb@sics.se)
  * 
  *  Contributors:
- *    optional, Contributor's name (Contributor's email address)
+ *    Raphael Collet (raph@info.ucl.ac.be)
  * 
  *  Copyright:
  *    Zacharias El Banna, 2002
@@ -33,54 +33,130 @@
 
 namespace _dss_internal{ //Start namespace
 
-// **************************  Migratory Token  ***************************
-  namespace { // We don't need to expose these outside the file so..
-
+  namespace {
+    // messages
     enum Migratory_Message {
-      MIGM_TOKEN_GET,
-      MIGM_NEED_NO_MORE,
-      MIGM_TOKEN,
-      MIGM_TOKEN_FORWARD,
-      MIGM_TOKEN_FORWARD_SPECIAL
+      MIGM_GET,          // PM: request access
+      MIGM_FORWARD,      // MP: send the successor of a proxy
+      MIGM_TOKEN,        // PP: pass to token from proxy to proxy
+      MIGM_TOKEN_HERE,   // PM: informs manager
+      MIGM_NEED_NO_MORE, // PM: please remove the token from here
+      MIGM_CHECK_SUCC,   // MP: manager inquiring to find token
+      MIGM_CHECK_PRED,   // MP: manager inquiring to find token
+      MIGM_FAILED_SUCC,  // PM: proxy will not forward to its successor
+      MIGM_FAILED_PRED,  // PM: proxy will not receive from this successor
+      MIGM_OLD_SUCC,     // PM: reply to an old request
+      MIGM_OLD_PRED,     // PM: reply to an old request
+      MIGM_PERMFAIL      // MP,PM: token is lost
     };
   }
 
 
 
-  ProtocolMigratoryProxy::ProtocolMigratoryProxy():
-    ProtocolProxy(PN_MIGRATORY_STATE),a_next(NULL), a_token(MIGT_HERE),a_operations(){}
-  
-  bool 
-  ProtocolMigratoryProxy::m_initRemoteProt(DssReadBuffer*)
-  {
-    a_token = MIGT_EMPTY;
-    return true;
+  /******************** ProtocolMigratoryManager ********************/
+
+  ProtocolMigratoryManager::ProtocolMigratoryManager(DSite* mysite) {
+    a_chain.append(makePair(mysite, 0));
+    a_last = mysite;    // the token is at home
   }
 
+
   void
-  ProtocolMigratoryManager::msgReceived(::MsgContainer* msg, DSite* sender){
-    Migratory_Message message = static_cast<Migratory_Message>(msg->popIntVal());
-    switch(message) {
-    case MIGM_TOKEN_GET:{
-      if(current != sender){
-	::MsgContainer *msgC = a_coordinator->m_createProxyProtMsg();
-	msgC->pushIntVal(MIGM_TOKEN_FORWARD);
-	msgC->pushDSiteVal(sender);
-	a_coordinator->m_sendToProxy(current, msgC); 
-	current = sender;
-      } else {
-	// it is already coming to the (home) proxy
-	dssLog(DLL_BEHAVIOR,"MigratoryManager::special gc case");
+  ProtocolMigratoryManager::sendMigrateInfo(::MsgContainer* msg) {
+    for (Position<Pair<DSite*, int> > p(a_chain); p(); p++) {
+      msg->pushDSiteVal((*p).first);
+      msg->pushIntVal((*p).second);
+    }
+  }
+
+  ProtocolMigratoryManager::ProtocolMigratoryManager(::MsgContainer* msg) {
+    a_last = NULL;
+    while (!msg->m_isEmpty()) {
+      a_last = msg->popDSiteVal();
+      a_chain.append(makePair(a_last, msg->popIntVal()));
+    }
+  }
+
+
+  void
+  ProtocolMigratoryManager::msgReceived(::MsgContainer* msg, DSite* sender) {
+    int msgType = msg->popIntVal();
+    switch (msgType) {
+    case MIGM_GET: {
+      if (a_last == NULL) sendToProxy(sender, MIGM_PERMFAIL);
+      else if (sender != a_last) {
+	int reqid = msg->popIntVal();
+	sendToProxy(a_last, MIGM_FORWARD, sender);
+	if (a_chain.front().find(sender)) {
+	  // remove everything up to the sender
+	  while (a_chain.pop().first != sender) ;
+	}
+	a_chain.append(makePair(sender, reqid));
+	a_last = sender;
+      }
+      break;
+    }
+    case MIGM_TOKEN_HERE: {
+      if (a_last && a_chain.front().find(sender)) {
+	Position<Pair<DSite*,int> > p(a_chain);
+	// remove all elements before sender
+	while ((*p).first != sender) p.pop();
       }
       break;
     }
     case MIGM_NEED_NO_MORE:{
-      if (current == sender){ // have not forwarded it to someone else 
-	::MsgContainer *msgC = a_coordinator->m_createProxyProtMsg();
-	current = a_coordinator->m_getEnvironment()->a_myDSite;
-	msgC->pushIntVal(MIGM_TOKEN_FORWARD);
-	msgC->pushDSiteVal(current);
-	a_coordinator->m_sendToProxy(sender, msgC); 
+      if (sender == a_last) {
+	// The sender has no successor; force the home proxy to
+	// request the state.
+	ProtocolProxy* pp = a_coordinator->m_getProxy()->m_getProtocol();
+	static_cast<ProtocolMigratoryProxy*>(pp)->requestToken();
+      }
+      break;
+    }
+    case MIGM_FAILED_SUCC: {
+      // a proxy notifies that its successor has failed
+      if (a_last) {
+	Position<Pair<DSite*,int> > p(a_chain);
+	p.find(sender); p++;     // p is on the sender's successor
+	Assert(p());
+	p.remove();              // remove the successor
+	if (p()) { // forward to the new successor
+	  sendToProxy(sender, MIGM_FORWARD, (*p).first);
+	} else { // we removed the last proxy in a_chain
+	  a_chain.check();
+	  a_last = sender;
+	}
+      }
+      break;
+    }
+    case MIGM_PERMFAIL:
+    case MIGM_FAILED_PRED: {
+      // The sender tells that it will not receive the token; it is
+      // therefore lost (we know the predecessor has had it).
+      if (a_last) {
+	// notify all proxies
+	while (!a_chain.isEmpty())
+	  sendToProxy(a_chain.pop().first, MIGM_PERMFAIL);
+	a_last = NULL;
+      }
+      break;
+    }
+    case MIGM_OLD_SUCC:
+    case MIGM_OLD_PRED: {
+      // The sender informs the manager that it no longer has the
+      // state (for the given request).
+      if (a_last) {
+	int reqid = msg->popIntVal();
+	Position<Pair<DSite*,int> > p(a_chain);
+	if (p.find(sender) && (*p).second == reqid) {
+	  // drop all elements up to sender
+	  while (a_chain.pop().first != sender) ;
+	  
+	  if (msgType == MIGM_OLD_SUCC) {
+	    // see whether a successor has had the token
+	    inquire(a_chain.front().element().first);
+	  }
+	}
       }
       break;
     }
@@ -90,71 +166,120 @@ namespace _dss_internal{ //Start namespace
   }
 
 
-  ProtocolMigratoryManager::ProtocolMigratoryManager(::MsgContainer* msg):current(NULL){
-    current = msg->popDSiteVal();
+  // inquiry protocol around a failed proxy
+  void ProtocolMigratoryManager::inquire(DSite* s) {
+    Position<Pair<DSite*,int> > cur(a_chain);
+    bool found = false;
+    Position<Pair<DSite*,int> > other;
+    
+    // first try to find the closest non-failed predecessor
+    while ((*cur).first != s) {
+      if ((*cur).first->m_getFaultState() <= DSite_TMP)
+	found = true, other = cur;
+      cur++;
+    }
+    if (found) { // we have a predecessor, inquire it
+      sendToProxy((*other).first, MIGM_CHECK_SUCC, (*other).second);
+      return;
+    }
+    
+    // try to find the closest non-failed successor, then
+    cur++;
+    while (cur()) {
+      if ((*cur).first->m_getFaultState() <= DSite_TMP)
+	found = true, other = cur;
+      cur++;
+    }
+    if (found) { // we have a successor, inquire it
+      sendToProxy((*other).first, MIGM_CHECK_PRED, (*other).second);
+      return;
+    }
+    
+    // all proxies in the chain failed; we've lost the state
+    while (!a_chain.isEmpty()) a_chain.pop();
+    a_last = NULL;
+    // notify home proxy (take the short cut...)
+    ProtocolProxy* pp = a_coordinator->m_getProxy()->m_getProtocol();
+    static_cast<ProtocolMigratoryProxy*>(pp)->lostToken();
   }
 
 
+  // check for failed proxies
+  void ProtocolMigratoryManager::m_siteStateChange(DSite* s,
+						   const DSiteState& state) {
+    if (a_last &&
+	(state == DSite_GLOBAL_PRM || state == DSite_LOCAL_PRM) &&
+	a_chain.front().find(s)) {
+      inquire(s);
+    }
+  }
+
+
+
+  /******************** ProtocolMigratoryProxy ********************/
+
+  ProtocolMigratoryProxy::ProtocolMigratoryProxy() :
+    ProtocolProxy(PN_MIGRATORY_STATE),
+    a_token(MIGT_HERE), a_successor(NULL), a_request(0) {}
+  
+  bool 
+  ProtocolMigratoryProxy::m_initRemoteProt(DssReadBuffer*) {
+    a_token = MIGT_EMPTY;
+    return true;
+  }
+
   void
-  ProtocolMigratoryManager::sendMigrateInfo(::MsgContainer* msg){
-    msg->pushDSiteVal(current);
+  ProtocolMigratoryProxy::makeGCpreps() {
+    if (a_successor) a_successor->m_makeGCpreps();
+    t_gcList(a_susps);
   }
 
 
   void
   ProtocolMigratoryProxy::requestToken(){
-    ::MsgContainer *msgC = a_proxy->m_createCoordProtMsg();
-    msgC->pushIntVal(MIGM_TOKEN_GET);
+    Assert(a_token == MIGT_EMPTY);
     dssLog(DLL_BEHAVIOR,"MigratoryProxy::requestToken");
+    sendToManager(MIGM_GET, a_request);
     a_token = MIGT_REQUESTED;
-    a_proxy->m_sendToCoordinator(msgC);
   }
 
   void
   ProtocolMigratoryProxy::forwardToken(){
-    ::PstOutContainerInterface* ans = a_proxy->retrieveEntityState();
-    dssLog(DLL_BEHAVIOR,"MigratoryProxy::forwardToken : To:%s",a_next->m_stringrep());
-    ::MsgContainer *msgC = a_proxy->m_createProxyProtMsg();
-    msgC->pushIntVal(MIGM_TOKEN);
-    gf_pushPstOut(msgC, ans);
+    Assert(a_token == MIGT_HERE && a_successor);
+    dssLog(DLL_BEHAVIOR,"MigratoryProxy::forwardToken : To:%s",
+	   a_successor->m_stringrep());
+    sendToProxy(a_successor, MIGM_TOKEN, a_proxy->retrieveEntityState());
     a_token = MIGT_EMPTY;
-    a_proxy->m_sendToProxy(a_next,msgC);
-    a_next  = NULL;
+    a_successor = NULL;
+    a_request++;
   }
-
 
   void
   ProtocolMigratoryProxy::resumeOperations(){
-    Assert(!a_operations.isEmpty() ||
-	   a_proxy->m_getProxyStatus() == PROXY_STATUS_HOME);
-    while (!a_operations.isEmpty()) {
-      Pair<Migratory_Operation, GlobalThread*> op = a_operations.pop();
-      dssLog(DLL_BEHAVIOR,"MigratoryProxy::resumeOperation: %d, Thread:%d",
-	     op.first, op.second);
-      switch (op.first) {
-      case MIGO_ACCESS:
-	(op.second)->resumeDoLocal(NULL); // ERIK!!
-	break;
-      case MIGO_FORWARD:
-	forwardToken(); // always forward token and...
-	// if Queue is not empty then request it again
-	if (!a_operations.isEmpty()) requestToken();
-	return;
-      default:
-	Assert(0);
-	a_proxy->m_getEnvironment()->a_map->GL_error("Migratory Proxy: error, unknown operation %d", op.first);
-	return;
-      }
-    }
-    if(a_next != NULL){ // How did we get here (no lock anymore)?
-      // Assert(0);
+    Assert(!a_susps.isEmpty() || a_proxy->m_isHomeProxy());
+    // wake up all suspended threads
+    while (!a_susps.isEmpty())
+      a_susps.pop()->resumeDoLocal(NULL);   // NULL is not cool...
+    // forward the token
+    if (a_successor)
       forwardToken();
-    }
+    else
+      sendToManager(MIGM_TOKEN_HERE);
+  }
+
+  void
+  ProtocolMigratoryProxy::lostToken() {
+    // we have lost the state, notify the upper layers
+    a_token = MIGT_LOST;
+    a_successor = NULL;
+    a_proxy->updateFaultState(FS_PROT_STATE_PRM_UNAVAIL);
+    // wake up all suspensions?
   }
 
 
   OpRetVal
-  ProtocolMigratoryProxy::protocol_Access(GlobalThread* const id, ::PstOutContainerInterface**& out){
+  ProtocolMigratoryProxy::protocol_Access(GlobalThread* const id,
+					  ::PstOutContainerInterface**& out){
     dssLog(DLL_BEHAVIOR,"MigratoryProxy::Access operation (%d)",a_token);
     out = NULL; // Never transmit
     switch(a_token){
@@ -164,32 +289,72 @@ namespace _dss_internal{ //Start namespace
       requestToken();
       // fall through
     case MIGT_REQUESTED:
-      a_operations.append(makePair(MIGO_ACCESS, id));
+      a_susps.append(id);
       return DSS_SUSPEND;
+    case MIGT_LOST:
+      return DSS_RAISE;
     default:
       Assert(0);
       return DSS_INTERNAL_ERROR_SEVERE; // Whatever....
     }
   }
+
+  // kill the entity
+  OpRetVal
+  ProtocolMigratoryProxy::protocol_Kill(GlobalThread* const th_id) {
+    if (a_token == MIGT_LOST) return DSS_SKIP;
+    if (!sendToManager(MIGM_PERMFAIL)) return DSS_RAISE;
+    a_susps.push(th_id);
+    return DSS_SUSPEND;
+  }
+
   
   void
   ProtocolMigratoryProxy::msgReceived(::MsgContainer* msg, DSite* sender){
-    Migratory_Message message = static_cast<Migratory_Message>(msg->popIntVal());
-    switch(message) {
-    case MIGM_TOKEN_FORWARD:{
-      Assert(a_next == NULL);
-      a_next = msg->popDSiteVal(); // The manager should never send it twice
-      if (a_token == MIGT_HERE)
-	forwardToken();
-      else
-	a_operations.append(makePair(MIGO_FORWARD, (GlobalThread*) NULL));
+    int msgType = msg->popIntVal();
+    switch (msgType) {
+    case MIGM_FORWARD:{
+      if (a_token == MIGT_LOST) break;   // should not happen
+      Assert(a_successor == NULL);
+      a_successor = msg->popDSiteVal();
+      if (a_token == MIGT_HERE) forwardToken();
       break;
     }
     case MIGM_TOKEN:{
+      if (a_token == MIGT_LOST) break;   // Yes, we have to drop it!
       a_token = MIGT_HERE;
-      ::PstInContainerInterface* buildcont =  gf_popPstIn(msg);
-      a_proxy->installEntityState(buildcont);
+      ::PstInContainerInterface* builder = gf_popPstIn(msg);
+      a_proxy->installEntityState(builder);
       resumeOperations();
+      break;
+    }
+    case MIGM_CHECK_SUCC: {
+      if (a_token == MIGT_LOST) break;
+      int reqid = msg->popIntVal();
+      if (a_request != reqid) { // this is an old request
+	sendToManager(MIGM_OLD_SUCC, reqid);
+      } else if (a_successor) { // ask for a new successor, then!
+	a_successor = NULL;
+	sendToManager(MIGM_FAILED_SUCC);
+      }
+      break;
+    }
+    case MIGM_CHECK_PRED: {
+      if (a_token == MIGT_LOST) break;
+      int reqid = msg->popIntVal();
+      if (a_request != reqid) { // this is an old request
+	sendToManager(MIGM_OLD_PRED, reqid);
+      } else if (a_token == MIGT_HERE) { // the token is here
+	sendToManager(MIGM_TOKEN_HERE);
+      } else { // we will never receive the token
+	sendToManager(MIGM_FAILED_PRED);
+	lostToken();     // early failure (beware!)
+      }
+      break;
+    }
+    case MIGM_PERMFAIL: {
+      if (a_token == MIGT_LOST) break;
+      lostToken();
       break;
     }
     default:
@@ -197,31 +362,44 @@ namespace _dss_internal{ //Start namespace
     }
   }
 
-
   bool
   ProtocolMigratoryProxy::clearWeakRoot(){
-    if(isWeakRoot()){ // Must check again since we don't know if something happened since last check
-      //if token == MIGT_REQUESTED then we should rely on the glue to mark it so skip it here
-      if(a_next == NULL){
-	dssLog(DLL_BEHAVIOR,"MigratoryProxy::clearing weak root\n");
-	::MsgContainer *msgC = a_proxy->m_createCoordProtMsg();
-	msgC->pushIntVal(MIGM_NEED_NO_MORE);
-	a_proxy->m_sendToCoordinator(msgC);
-      } else
-	forwardToken(); // How could this happen? (lock removed)
+    // Must check isWeakRoot() again since we don't know if something
+    // happened since last check
+    if (isWeakRoot()) {
+      if (a_token == MIGT_HERE) {
+	if (a_successor) forwardToken();
+	else sendToManager(MIGM_NEED_NO_MORE);
+      }
       return false; //Whatever happened we've only tried to clear it
     }
     return true;
   }
 
-
-  void
-  ProtocolMigratoryProxy::makeGCpreps() {
-    if (a_next) a_next->m_makeGCpreps();
-
-    Position<Pair<Migratory_Operation, GlobalThread*> > pos;
-    for (pos(a_operations); pos(); pos++)
-      if ((*pos).first == MIGO_ACCESS) (*pos).second->m_makeGCpreps();
+  // interpret a site failure
+  FaultState
+  ProtocolMigratoryProxy::siteStateChanged(DSite* s, const DSiteState& state) {
+    if (a_token != MIGT_LOST) {
+      if (a_proxy->m_getCoordinatorSite() == s) {
+	switch (state) {
+	case DSite_OK:
+	  return FS_PROT_STATE_OK;
+	case DSite_TMP:
+	  return FS_PROT_STATE_TMP_UNAVAIL;
+	case DSite_GLOBAL_PRM: case DSite_LOCAL_PRM:
+	  lostToken();
+	  return FS_PROT_STATE_PRM_UNAVAIL;
+	default:
+	  dssError("Unknown DSite state %d for %s",state,s->m_stringrep());
+	}
+      }
+      if (a_successor == s &&
+	  (state == DSite_GLOBAL_PRM || state == DSite_LOCAL_PRM)) {
+	a_successor = NULL;
+	sendToManager(MIGM_FAILED_SUCC);
+      }
+    }
+    return 0;
   }
 
 } //End namespace
