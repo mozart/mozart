70f4ef913f8a2ef3ed47389785ffc3b23681801a
raph <raph@ada56829-ad1f-0410-b00f-83cda6628aec>
2006-04-06 16:11:33 +0000

    Rewrote the migratory protocol, and added the "chain" idea.  The manager is now able to diagnose the entity's failure when a proxy crash with the state.
    git-svn-id: https://gforge.info.ucl.ac.be/svn/mozart@16336 ada56829-ad1f-0410-b00f-83cda6628aec

--- branches/mozdss-branch/mozart/platform/dss/src/protocol_migratory.hh
+++ branches/mozdss-branch/mozart/platform/dss/src/protocol_migratory.hh
@@ -3,7 +3,7 @@
  *    Zacharias El Banna (zeb@sics.se)
  * 
  *  Contributors:
- *    optional, Contributor's name (Contributor's email address)
+ *    Raphael Collet (raph@info.ucl.ac.be)
  * 
  *  Copyright:
  *    Zacharias El Banna, 2002
@@ -37,55 +37,68 @@
 
 namespace _dss_internal{ //Start namespace
 
-  // **************************  Migratory Token  ***************************
-  enum Migratory_Token {
-    MIGT_EMPTY,
-    MIGT_HERE,
-    MIGT_REQUESTED
-  };
-
-  enum Migratory_Operation {
-    MIGO_ACCESS,
-    MIGO_FORWARD
-  };
-
-
   class ProtocolMigratoryManager:public ProtocolManager {
-  public:
-    DSite *current;
   private:
-    ProtocolMigratoryManager(const ProtocolMigratoryManager&):current(NULL){}
-    ProtocolMigratoryManager& operator=(const ProtocolMigratoryManager&){ return *this; }
-  public:
+    SimpleQueue<Pair<DSite*, int> > a_chain;   // the chain of proxies
+    DSite*                          a_last;    // last proxy in chain
 
-    ProtocolMigratoryManager(DSite* mysite):current(mysite){};
+    // The chain contains pairs (proxy, request_id).  The proxies will
+    // receive the token in the order given by the chain.  The value
+    // request_id is used when the manager asks a proxy whether it has
+    // already received and forwarded the token.  It is required to
+    // avoid any ambiguity between requests.
+    //
+    // Invariants:
+    //  - a_last is the last proxy in the chain (if not empty);
+    //  - a proxy never appears twice in a_chain;
+    //  - a_chain is empty and a_last==NULL iff the entity is permfail.
+
+    // run the inquiry algorithm
+    void inquire(DSite*);
+
+    ProtocolMigratoryManager(const ProtocolMigratoryManager&) {}
+    ProtocolMigratoryManager& operator=(const ProtocolMigratoryManager&){ return *this; }
+
+  public:
+    ProtocolMigratoryManager(DSite*);
     ProtocolMigratoryManager(::MsgContainer*);
-    ~ProtocolMigratoryManager(){};
-    void makeGCpreps() { current->m_makeGCpreps(); }
+    ~ProtocolMigratoryManager() {}
+
+    void makeGCpreps() { t_gcList(a_chain); }
     void msgReceived(::MsgContainer*,DSite*);
     
     void sendMigrateInfo(::MsgContainer*); 
+
+    // check for failed proxies
+    void m_siteStateChange(DSite*, const DSiteState&);
+  };
+
+
+  // state of a migratory proxy
+  enum MigratoryToken {
+    MIGT_EMPTY,       // token not here
+    MIGT_REQUESTED,   // token not here, but has been requested
+    MIGT_HERE,        // token here
+    MIGT_LOST         // token lost (entity's state is thus permfail)
   };
   
 
   class ProtocolMigratoryProxy:public ProtocolProxy{
+    friend class ProtocolMigratoryManager;
   private:
-    DSite*    a_next;     // NULL
-    Migratory_Token a_token;  //empty
-    SimpleQueue<Pair<Migratory_Operation, GlobalThread*> > a_operations;
-
-    // a_operations contains pairs of the form <MIGO_ACCESS, tid> and
-    // <MIGO_FORWARD, NULL>.
+    MigratoryToken             a_token;       // where is the token?
+    DSite*                     a_successor;   // next proxy in chain
+    int                        a_request;     // request id
+    SimpleQueue<GlobalThread*> a_susps;       // suspended threads
 
     void requestToken();
     void forwardToken();
     void resumeOperations();
+    void lostToken();
 
     ProtocolMigratoryProxy(const ProtocolMigratoryProxy&):
-      ProtocolProxy(PN_MIGRATORY_STATE),a_next(NULL), a_token(MIGT_EMPTY),
-      a_operations()
+      ProtocolProxy(PN_MIGRATORY_STATE), a_token(MIGT_EMPTY), a_successor(NULL)
     {}
-
     ProtocolMigratoryProxy& operator=(const ProtocolMigratoryProxy&){ return *this; }
 
   public:
@@ -93,27 +106,29 @@ namespace _dss_internal{ //Start namespace
     bool m_initRemoteProt(DssReadBuffer*);
     ~ProtocolMigratoryProxy(){};
 
-    //
-    // WEAK protocol
-    //
+    // WEAK protocol (to get rid of the token)
     // 1) I send a "need_no_more" to manager
-    // 2) manager check if I am current, 
+    // 2) manager check if I am last, 
     //    if so it sends me a "forward" (to home proxy)
     //    else  it has already sent me a forward message and everething is ok
-    //
-
     bool isWeakRoot(){
-      return (a_token == MIGT_HERE || a_token == MIGT_REQUESTED);
-    };
-    
+      return (a_token == MIGT_REQUESTED || a_token == MIGT_HERE);
+    }
     bool clearWeakRoot();    
     
-    OpRetVal protocol_Access(GlobalThread* const, ::PstOutContainerInterface**&);
+    OpRetVal protocol_Access(GlobalThread* const,
+			     ::PstOutContainerInterface**&);
+    OpRetVal protocol_Kill(GlobalThread* const th_id);
+
     void makeGCpreps();
     void msgReceived(::MsgContainer*,DSite*);
     
-    virtual void remoteInitatedOperationCompleted(DssOperationId* opId,::PstOutContainerInterface* pstOut) {;} 
-    void localInitatedOperationCompleted(){Assert(0);} 
+    void remoteInitatedOperationCompleted(DssOperationId* opId,
+					  ::PstOutContainerInterface* pstOut){}
+    void localInitatedOperationCompleted(){ Assert(0); } 
+
+    // check fault state
+    virtual FaultState siteStateChanged(DSite*, const DSiteState&);
   };
 
 } //End namespace
