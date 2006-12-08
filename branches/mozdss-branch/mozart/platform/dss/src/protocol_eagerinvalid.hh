576496bb1a78594b719c823c951b04fc42b74021
raph <raph@ada56829-ad1f-0410-b00f-83cda6628aec>
2006-12-08 14:49:45 +0000

    refactored invalidation protocols
    git-svn-id: https://gforge.info.ucl.ac.be/svn/mozart@16494 ada56829-ad1f-0410-b00f-83cda6628aec

--- branches/mozdss-branch/mozart/platform/dss/src/protocol_eagerinvalid.hh
+++ branches/mozdss-branch/mozart/platform/dss/src/protocol_eagerinvalid.hh
@@ -37,64 +37,79 @@
 
 namespace _dss_internal{ //Start namespace
 
-  class ProtocolEagerInvalidManager:public ProtocolManager {
+  // generic classes for eager and lazy invalidation protocols
+
+  class ProtocolInvalidManager : public ProtocolManager {
   private:
-    SimpleList<DSite*> a_readers;
-    SimpleQueue<DSite*> a_writers;
+    // The status tells whether manager uses the lazy protocol.
 
-    // All proxies are registered in a_proxies.  a_readers contains
-    // all reader proxies that have not invalidated their state yet.
-    // They are asked to invalidate once a write request must be
-    // served; the write token is given once a_readers becomes empty.
-    //
-    // a_writers contains the proxies that made a write request.  The
-    // first element of a_writers has the write token if a_readers is
-    // empty.
+    SimpleList<DSite*> a_readers;     // reader proxies
+    unsigned int a_valid;             // how many proxies not invalid yet
 
-    ProtocolEagerInvalidManager(const ProtocolEagerInvalidManager&) {}
-    ProtocolEagerInvalidManager& operator=(const ProtocolEagerInvalidManager&){
+    // a_readers contains all reader proxies (all proxies if eager,
+    // strict readers if lazy).  Those proxies are asked to invalidate
+    // their state once a write request must be served.  a_valid
+    // proxies among a_readers have not notified invalidation.
+
+    struct Request {   // buffered write requests
+      GlobalThread* caller; PstInContainerInterface* arg;
+      Request(GlobalThread* t, PstInContainerInterface* a): caller(t), arg(a) {}
+      void makeGCpreps() { if (caller) caller->m_makeGCpreps(); }
+      void dispose() { if (arg) arg->dispose(); }
+    };
+
+    SimpleQueue<Request> a_requests;
+
+    // a_requests contains the pending write operations requested by
+    // proxies.  Those operations are performed by the manager once
+    // a_valid reaches zero.
+
+    ProtocolInvalidManager(const ProtocolInvalidManager&) {}
+    ProtocolInvalidManager& operator=(const ProtocolInvalidManager&){
       return *this; }
-								    
-  public:
-    ProtocolEagerInvalidManager(DSite *mysite);
-    void sendMigrateInfo(::MsgContainer*); 
-    ProtocolEagerInvalidManager(::MsgContainer*);
-    ~ProtocolEagerInvalidManager() {}
 
-    void msgReceived(::MsgContainer*,DSite*);
+  public:
+    ProtocolInvalidManager(DSite *mysite, bool isLazy);
+    void sendMigrateInfo(::MsgContainer*); 
+    ProtocolInvalidManager(::MsgContainer*);
+    ~ProtocolInvalidManager();
+
+    void msgReceived(::MsgContainer*, DSite*);
+    void makeGCpreps();
     void m_siteStateChange(DSite*, const DSiteState&);
 
   private: 
+    bool m_isLazy() const { return getStatus(); }
     void m_register(DSite* s);
-    void m_deregister(DSite* s);
-    void m_invalidateReaders();
-    void m_invalidated(DSite* s);
-    void m_sendWriteToken();
-    void m_updateOneReader(DSite*);
-    void m_updateAllReaders(DSite*);
+    void m_invalidate();
+    void m_invalid(DSite* s, bool remove = false);
+    static const bool REMOVE = true;
+    void m_checkOperations();
+    void m_commit(DSite*);
     void m_failed();
     void printStatus();
   };
 
 
-  class ProtocolEagerInvalidProxy:public ProtocolProxy{
+  class ProtocolInvalidProxy : public ProtocolProxy{
   private:
-    // The status of the proxy tells whether the state is valid.  The
-    // first a_reads elements of a_susps are read operations, and the
-    // remaining ones are write operations
-    int a_reads;
+    // The status of the proxy tells whether it uses the lazy
+    // protocol, whether the proxy is a reader, and whether the state
+    // is valid.  a_susps contains all pending operations; the first
+    // a_numRead elements are read operations, the remaining ones are
+    // write operations.  (The suspended writes operations are stored
+    // in order to keep the suspended thread alive in memory.)
+    unsigned int a_numRead;
 
-    ProtocolEagerInvalidProxy(const ProtocolEagerInvalidProxy&):
-      ProtocolProxy(PN_EAGER_INVALID), a_reads(0) {}
-    ProtocolEagerInvalidProxy& operator=(const ProtocolEagerInvalidProxy&){
+    ProtocolInvalidProxy(const ProtocolInvalidProxy&):
+      ProtocolProxy(PN_EAGER_INVALID) {}
+    ProtocolInvalidProxy& operator=(const ProtocolInvalidProxy&){
       return *this; }
 
   public:
-    ProtocolEagerInvalidProxy();
+    ProtocolInvalidProxy(bool isLazy);
     bool m_initRemoteProt(DssReadBuffer*);
-    ~ProtocolEagerInvalidProxy();
-
-    bool isWeakRoot() { return !isPermFail() && a_reads < a_susps.size(); }
+    ~ProtocolInvalidProxy();
 
     OpRetVal protocol_Read(GlobalThread* const th_id,
 			   PstOutContainerInterface**& msg);
@@ -109,6 +124,32 @@ namespace _dss_internal{ //Start namespace
     void localInitatedOperationCompleted(){Assert(0);} 
 
     virtual FaultState siteStateChanged(DSite*, const DSiteState&);
+
+  private:
+    bool m_isLazy() const { return getStatus() & 1; }
+    void m_setLazy() { setStatus(getStatus() | 1); }
+    bool m_isReader() const { return getStatus() & 2; }
+    void m_setReader(bool b) { setStatus((getStatus() & ~2) | (b ? 2 : 0)); }
+    bool m_isValid() const { return getStatus() & 4; }
+    void m_setValid(bool b) { setStatus((getStatus() & ~4) | (b ? 4 : 0)); }
+    void m_subscribe();
+  };
+
+
+
+  // manager and proxy for the eager protocol
+
+  class ProtocolEagerInvalidManager : public ProtocolInvalidManager {
+  public:
+    ProtocolEagerInvalidManager(DSite* s) :
+      ProtocolInvalidManager(s, false) {}
+    ProtocolEagerInvalidManager(MsgContainer* msg) :
+      ProtocolInvalidManager(msg) {}
+  };
+
+  class ProtocolEagerInvalidProxy : public ProtocolInvalidProxy {
+  public:
+    ProtocolEagerInvalidProxy() : ProtocolInvalidProxy(false) {}
   };
 
 } //End namespace
