e3f42d85d4e93ac822eb31265b9a1f64568df04e
ggutierrez <ggutierrez@ada56829-ad1f-0410-b00f-83cda6628aec>
2007-09-26 05:12:57 +0000

    Gecode 2.0.0 suport
    *) libcp-int is not buit because of some problems with mozart compatibility propagators
    *) libcp-bool is built an uses the new BoolVar variable type.
    
    
    git-svn-id: https://gforge.info.ucl.ac.be/svn/mozart@16660 ada56829-ad1f-0410-b00f-83cda6628aec

--- mozart/platform/emulator/libcp-bool/GeBoolVar.cc
+++ mozart/platform/emulator/libcp-bool/GeBoolVar.cc
@@ -51,8 +51,8 @@ OZ_Term GeBoolVar::statusV() {
 VarBase* GeBoolVar::clone(void) {
   GenericSpace* gs = getGSpace(); //extVar2Var(this)->getBoardInternal()->getGenericSpace(true);
   Assert(gs);
-  IntVar &v = getBoolVarInfo();
-  IntVar x;
+  BoolVar &v = getBoolVarInfo();
+  BoolVar x;
   x.update(gs,false,v);
   return x.variable();
 }
@@ -62,39 +62,40 @@ VarBase* GeBoolVar::clone(void) {
 //x is the local variable,  the one that its domain is modified
 inline
 bool GeBoolVar::intersect(TaggedRef x) {
-  IntVar& gv = getBoolVarInfo();
-  ViewRanges<IntView> gvr(gv);
+  BoolVar& gv = getBoolVarInfo();
+  ViewRanges<BoolView> gvr(gv);
 
-  IntVar& liv = get_BoolVarInfo(x);
-  IntView vw(liv);
+  BoolVar& liv = get_BoolVarInfo(x);
+  BoolView vw(liv);
   //Ask Alejandro about the use of getGSpace() instead of oz_currentBoard()
-  return (vw.inter(oz_currentBoard()->getGenericSpace(),gvr)==ME_GEN_FAILED ? false: true);
+  return (vw.inter_r(oz_currentBoard()->getGenericSpace(),gvr)==ME_GEN_FAILED ? false: true);
 }
 
 //(this) is the global variable
 //lx is the local value
 inline
 bool GeBoolVar::In(TaggedRef lx) {
-  IntVar gv = getBoolVarInfo();
-  IntView vw(gv);
+  BoolVar gv = getBoolVarInfo();
+  BoolView vw(gv);
   return vw.in(oz_intToC(lx));
 }
 
 TaggedRef GeBoolVar::clone(TaggedRef v) {
   Assert(OZ_isGeBoolVar(v));
   
-  OZ_Term lv = new_GeBoolVar(IntSet(0,1));
+  OZ_Term lv = new_GeBoolVar(0,1);
   get_GeBoolVar(v,false)->intersect(lv);
   return lv;
 }
 
+// can be simplified!!
 inline
 bool GeBoolVar::hasSameDomain(TaggedRef v) {
   
   Assert(OZ_isGeBoolVar(v));
-  IntVar v1 = get_BoolVarInfo(v);
-  ViewRanges< IntView > vr1 (v1);
-  ViewRanges< IntView > vr2 (getBoolVarInfo());
+  BoolVar v1 = get_BoolVarInfo(v);
+  ViewRanges< BoolView > vr1 (v1);
+  ViewRanges< BoolView > vr2 (getBoolVarInfo());
   
   while(true) {
     if(!vr1() && !vr2()) return true;
@@ -106,18 +107,19 @@ bool GeBoolVar::hasSameDomain(TaggedRef v) {
 
 inline
 TaggedRef GeBoolVar::newVar(void) {
-  return new_GeBoolVar(IntSet(0,1));
+  return new_GeBoolVar(0,1);
 }
 
 
+//can be simplified
 inline
 bool GeBoolVar::IsEmptyInter(TaggedRef* var1,  TaggedRef* var2) {
   
-  IntVar& v1 = get_BoolVarInfo(*var1);
-  IntVar& v2 = get_BoolVarInfo(*var2);
+  BoolVar& v1 = get_BoolVarInfo(*var1);
+  BoolVar& v2 = get_BoolVarInfo(*var2);
   
-  ViewRanges<IntView > vr1 (v1);
-  ViewRanges<IntView > vr2 (v2);
+  ViewRanges<BoolView > vr1 (v1);
+  ViewRanges<BoolView > vr2 (v2);
   
   while(true) {
     if(!vr1() || !vr2() ) return true;
