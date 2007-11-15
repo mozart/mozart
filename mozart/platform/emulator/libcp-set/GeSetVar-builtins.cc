72ff5950049ec4eec356d631efa1c1abce6161d0
ggutierrez <ggutierrez@ada56829-ad1f-0410-b00f-83cda6628aec>
2007-11-15 16:05:09 +0000

    adding some extra stuff to the FS constraint domain
    git-svn-id: https://gforge.info.ucl.ac.be/svn/mozart@16714 ada56829-ad1f-0410-b00f-83cda6628aec

--- mozart/platform/emulator/libcp-set/GeSetVar-builtins.cc
+++ mozart/platform/emulator/libcp-set/GeSetVar-builtins.cc
@@ -37,7 +37,7 @@ using namespace Gecode::Set;
 /** 
  * \brief Creates a new SetVar variable with bounds
  * 
- * @param 0 The space
+ * @param 0 Domain description
  * @param 1 Domain description
  * @param 2 The new variable
  */
@@ -51,6 +51,32 @@ OZ_BI_end
 
 
 /** 
+ * \brief Creates a new SetVar variable with the complement of an old one
+ * 
+ * @param 0 The old variable
+ * @param 1 The complement of the old variable as a new one
+ */
+OZ_BI_define(new_comp,1,1)
+{
+  OZ_RETURN(new_GeSetVarComp(OZ_in(0)));
+}
+OZ_BI_end
+
+/** 
+ * \brief Creates a new SetVar variable with the difference of two variables
+ * 
+ * @param 0 The old variable number 1,  V1
+ * @param 1 The old variable number 2,  V2
+ * @param 2 Variable = V1\V2
+ */
+OZ_BI_define(new_complin,2,1)
+{  
+  OZ_RETURN(new_GeSetVarComplIn(OZ_in(0),OZ_in(1)));
+}
+OZ_BI_end
+
+
+/** 
  * \brief Test whether \a OZ_in(0) is a GeSetVar
  * 
  */
@@ -59,6 +85,69 @@ OZ_BI_define(var_is,1,1)
   OZ_RETURN_BOOL(OZ_isGeSetVar(OZ_in(0)));
 }
 OZ_BI_end
+
+
+/** 
+ * \brief Update greatest lower bound to contain a value
+ * 
+ */
+OZ_BI_define(inc_val,2,0)
+{
+  DeclareInt(0,incValue,"The parameter has to be a value");
+  return inc_GeSetVarVal(OZ_in(1),incValue);
+  
+}
+OZ_BI_end
+
+/** 
+ * \brief Restrict least upper bound to not contain a value.
+ * 
+ */
+OZ_BI_define(exc_val,2,0)
+{
+  DeclareInt(0,incValue,"The parameter has to be a value");
+  return exc_GeSetVarVal(OZ_in(1),incValue);
+  
+}
+OZ_BI_end
+
+
+/** 
+ * \brief Restrict cardilatity to a value.
+ * 
+ */
+OZ_BI_define(card,2,0)
+{
+  DeclareInt(0,cardValue,"The parameter has to be a value");
+  return card_GeSetVarVal(OZ_in(1),cardValue);
+  
+}
+OZ_BI_end
+
+
+/** 
+ * \brief Restrict cardilatity to an interval of values.
+ * 
+ */
+OZ_BI_define(card_Int,3,0)
+{
+  DeclareInt(0,minValue,"The parameter has to be a value");
+  DeclareInt(1,maxValue,"The parameter has to be a value");
+  return cardInt_GeSetVar(OZ_in(1),minValue,maxValue);  
+}
+OZ_BI_end
+
+/** 
+ * \brief 
+ * 
+ */
+OZ_BI_define(is_In,2,1)
+{
+  DeclareInt(0,value,"The parameter has to be a value");
+  return isIn_GeSetVar(OZ_in(1),value);  
+}
+OZ_BI_end
+
 
 
 /** 
