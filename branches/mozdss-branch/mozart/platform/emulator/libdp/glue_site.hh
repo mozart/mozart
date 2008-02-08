39e44683c4041ecc4b098896c8b0a193b40b1503
raph <raph@ada56829-ad1f-0410-b00f-83cda6628aec>
2008-02-08 10:20:15 +0000

    merged files glue_siteRepresentation.* and glue_ozSite.* into glue_site.* for a future reworking of those classes
    git-svn-id: https://gforge.info.ucl.ac.be/svn/mozart@16771 ada56829-ad1f-0410-b00f-83cda6628aec

--- branches/mozdss-branch/mozart/platform/emulator/libdp/glue_site.hh
+++ branches/mozdss-branch/mozart/platform/emulator/libdp/glue_site.hh
@@ -97,4 +97,43 @@ void cleanStr(DssReadBuffer *buf, int len);
 
 
 
+#define OZ_E_SITE  4211
+
+class Oz_Site: public OZ_Extension
+{
+private:
+  Glue_SiteRep *a_gSite; 
+public: // From Oz_Extension 
+  virtual int           getIdV(void);
+
+  virtual OZ_Extension* gCollectV(void);
+  virtual void          gCollectRecurseV(void);
+  virtual OZ_Extension* sCloneV(void);
+  virtual void          sCloneRecurseV(void);
+
+  virtual OZ_Term       printV(int = 10) ;
+  virtual OZ_Term       printLongV(int depth = 10, int offset = 0);
+  virtual OZ_Term       typeV(void);
+  virtual OZ_Boolean    isChunkV(void) { return OZ_TRUE; }
+  virtual OZ_Term       getFeatureV(OZ_Term){;}
+  virtual OZ_Return	getFeatureV(OZ_Term,OZ_Term&) { return OZ_FAILED; }
+  virtual OZ_Return	putFeatureV(OZ_Term,OZ_Term ) { return OZ_FAILED; }
+  virtual OZ_Return     eqV(OZ_Term);
+  //
+  virtual OZ_Boolean    toBePickledV() { return (OZ_FALSE); }
+  virtual void          pickleV(MarshalerBuffer *mb, GenTraverser *gt) {}
+  virtual OZ_Boolean    toBeMarshaledV() { return (OZ_TRUE); }
+  virtual void          marshalSuspV(OZ_Term te,
+				     ByteBuffer *bs, GenTraverser *gt);
+  virtual int           minNeededSpace() { return (0); }
+  
+  void internalMarshal(DssWriteBuffer *buf);
+  
+  
+  void setGSR(Glue_SiteRep*);
+  Glue_SiteRep* getGSR(){ return a_gSite; }
+};
+
+void OzSite_init();
+
 #endif
