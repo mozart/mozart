a0502aab78ff8e5b689f10f8ad0310933e2fa45e
raph <raph@ada56829-ad1f-0410-b00f-83cda6628aec>
2008-03-04 13:52:19 +0000

    simplified variable patches for marshaling
    git-svn-id: https://gforge.info.ucl.ac.be/svn/mozart@16850 ada56829-ad1f-0410-b00f-83cda6628aec

--- platform/emulator/marshalerPatch.cc
+++ platform/emulator/marshalerPatch.cc
@@ -79,8 +79,7 @@ OzValuePatch* gcFinishOVP(OZ_Term ot)
     Assert(oz_isVarOrRef(ot));	// due to 'gcStartOVP()';
     Assert(oz_isExtVar(ot));	// ditto;
     DebugCode(ExtVarType evt = oz_getExtVar(ot)->getIdV(););
-    Assert(evt == OZ_EVAR_DISTRIBUTEDVARPATCH ||
-	   evt == OZ_EVAR_MARSHALEDVARPATCH);
+    Assert(evt == OZ_EVAR_DISTRIBUTEDVARPATCH);
 
     OzValuePatch *ovp = (OzValuePatch *) oz_getExtVar(ot);
     if (ovp->val) {
