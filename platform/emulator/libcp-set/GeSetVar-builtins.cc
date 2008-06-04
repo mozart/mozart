e9659aa2656677bd3339902cf25d05777bb0943c
ggutierrez <ggutierrez@ada56829-ad1f-0410-b00f-83cda6628aec>
2008-06-04 16:09:36 +0000

    *) fixed documentation mistake *) Misc changes *) reimplemented new_bounds built-in *) CardVal and CardInt builtins removed, added gfs_valueMake2
    git-svn-id: https://gforge.info.ucl.ac.be/svn/mozart@16980 ada56829-ad1f-0410-b00f-83cda6628aec

--- platform/emulator/libcp-set/GeSetVar-builtins.cc
+++ platform/emulator/libcp-set/GeSetVar-builtins.cc
@@ -6,6 +6,7 @@
  *  Contributing authors:
  *     Alejandro Arbelaez <aarbelaez@puj.edu.co>
  *     Andres Felipe Barco <anfelbar@univalle.edu.co>
+ *     Gustavo A. Gomez Farhat <gafarhat@univalle.edu.co>
  *
  *  Copyright:
  *     Gustavo Gutierrez, 2006
@@ -15,13 +16,15 @@
  *     $Date$
  *     $Revision$
  *
- *  This file is part of GeOz, a module for integrating gecode 
- *  constraint system to Mozart: 
- *     http://home.gna.org/geoz
- *
- *  See the file "LICENSE" for information on usage and
- *  redistribution of this file, and for a
- *     DISCLAIMER OF ALL WARRANTIES.
+ *  This file is part of Mozart, an implementation 
+ *  of Oz 3:
+ *     http://www.mozart-oz.org
+ * 
+ *  See the file "LICENSE" or
+ *     http://www.mozart-oz.org/LICENSE.html
+ *  for information on usage and redistribution 
+ *  of this file, and for a DISCLAIMER OF ALL 
+ *  WARRANTIES.
  *
  */
 
@@ -44,9 +47,9 @@ using namespace Gecode::Set;
  */
 OZ_BI_define(new_bounds,2,1)
 {
-  DECLARE_INT_SET3(dom, val, 0);   // the glb of the SetVar
-  DECLARE_INT_SET3(dom2, val1, 1);   // the lub of the SetVar
-  OZ_RETURN(new_GeSetVar(dom,dom2));
+  DECLARE_INT_SET3(dom1, val1, 0);   // the glb of the SetVar
+  DECLARE_INT_SET3(dom2, val2, 1);   // the lub of the SetVar
+  OZ_RETURN(new_GeSetVar(dom1,dom2));
 }
 OZ_BI_end
 
@@ -112,32 +115,6 @@ OZ_BI_define(exc_val,2,0)
 }
 OZ_BI_end
 
-
-/** 
- * \brief Restrict cardinality to a value.
- * 
- */
-// OZ_BI_define(card,2,0)
-// {
-//   DeclareInt(0,cardValue,"The parameter has to be a value");
-//   return card_GeSetVarVal(OZ_in(1),cardValue);
-  
-// }
-// OZ_BI_end
-
-
-/** 
- * \brief Restrict cardinality to an interval of values.
- * 
- */
-// OZ_BI_define(card_Int,3,0)
-// {
-//   DeclareInt(0,minValue,"The parameter has to be a value");
-//   DeclareInt(1,maxValue,"The parameter has to be a value");
-//   return cardInt_GeSetVar(OZ_in(1),minValue,maxValue);  
-// }
-// OZ_BI_end
-
 /** 
  * \brief 
  * 
@@ -174,5 +151,56 @@ OZ_BI_define(set_inf,0,1)
   OZ_RETURN_INT(Set::Limits::min);
 } 
 OZ_BI_end
+
+
+// this will be reflect.unknown
+
+// OZ_BI_define(gfs_reflectUnknown_2,2,0){
+//   DeclareGSpace(home);
+//   int result;
+
+//   if(OZ_isGeSetVar(OZ_in(0)) && OZ_isList(OZ_in(1))){
+//     DeclareGeSetVar(0, __s, home);
+//     DeclareGeBoolVar(1, __b, home);
+
+//     try{
+//       Gecode::dom(home, __s, Gecode::SRT_SUP, __i, __b);
+//     }
+//     catch(Exception e){
+//       RAISE_GE_EXCEPTION(e);
+//     }
+//   }
+//   else{
+//     return OZ_typeError(0, "Malformed Propagator");
+//   }
+
+//   CHECK_POST(home);
+// }OZ_BI_end
+
+// this will be value.make
+
+OZ_BI_define(gfs_valueMake_2,2,0){
+  DeclareGSpace(home);
+  
+  if(OZ_isGeIntVar(OZ_in(0)) && OZ_isGeSetVar(OZ_in(1))){
+    DeclareGeIntVar(0, __i, home);
+    DeclareGeSetVar(1, __s, home);
+    
+    try{
+      
+      printf("__i.min(), __i.max() = %d, %d\n", __i.min(), __i.max());
+      //Gecode::SetVar x(home, __i.min(), __i.max(), __i.min(), __i.max());
+      //__s.update(home, true, x);
+      // ???
+    }
+    catch(Exception e){
+      RAISE_GE_EXCEPTION(e);
+    }
+  }
+  //TODO: raise a more descriptive error
+  else{
+    return OZ_typeError(0, "Malformed Propagator");
+  }
+}OZ_BI_end
 
 #endif
