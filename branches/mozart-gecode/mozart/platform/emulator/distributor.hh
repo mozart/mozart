df2eef608ee33a7aff5186d968b5daca2f0d8504
ggutierrez <ggutierrez@ada56829-ad1f-0410-b00f-83cda6628aec>
2008-06-17 11:57:43 +0000

    Changed getChoice implementation to use a distributor to implement getChoice and to wait for stability. Distributors were slightly changed, a new method notifyStable was added to report space stability to the distributor. All other builtins were changed accordingly.
    git-svn-id: https://gforge.info.ucl.ac.be/svn/mozart@16992 ada56829-ad1f-0410-b00f-83cda6628aec

--- branches/mozart-gecode/mozart/platform/emulator/distributor.hh
+++ branches/mozart-gecode/mozart/platform/emulator/distributor.hh
@@ -39,6 +39,24 @@ public:
   virtual int commit(Board *, int) = 0;
   virtual int commit(Board *, int, int);
 
+	/*
+	 \brief Notify stability to the distributor. This method is called
+	 from checkStability when space stability is guaranted. Return values include
+	 -1 for old-style distributors, 0 for the distributor to report that is no
+	 longer needed in the space and 1 to ensure that distributor not should be
+	 removed from the space. Other values can be used in the future to report 
+	 errors.
+	 */
+	virtual int notifyStable(Board *);
+	
+	/*
+	 \brief Commit a branch to the distributor. Return values include 0 when the 
+	 distributor is no longer needed after the commit (i.e. it is done) and 1 to
+	 indicate the space that the distributor must be preserved. Other values can
+	 be used for early error detection.
+	 */
+	virtual int commitBranch(Board *, TaggedRef );
+	
   virtual Distributor * gCollect(void) = 0;
   virtual Distributor * sClone(void) = 0;
 
