d105400a95fc106ef7b2020c6659c1f8666ba212
raph <raph@ada56829-ad1f-0410-b00f-83cda6628aec>
2008-07-01 16:10:14 +0000

    Improved bundle construction for Mac OSX (platypus 3.4)

---
+++ misc/MacOsX/bundlelibs.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# This script copies the libraries used by emulator.exe into its
+# application bundle, and modifies the references to these libraries
+# inside emulator.exe accordingly.
+
+# path to the emulator
+EMULATOR=$1
+
+# path to the bundle
+BUNDLE=$2
+
+# relative path to the frameworks inside the bundle
+RELFWKS=@executable_path/../../../Frameworks
+
+# paths of imported shared libraries that do not match the patterns
+# shown below.  Add more grep -v's if necessary.
+BUNDLELIBS=`otool -LX $EMULATOR | sed 's/(.*$//' |
+    grep -v /usr/lib/libgcc |
+    grep -v /usr/lib/libstdc\+\+ |
+    grep -v /usr/lib/libz |
+    grep -v /usr/lib/libSystem`
+
+# copy libraries in the bundle, and patch the emulator
+for FILE in $BUNDLELIBS; do
+    NAME=`basename $FILE`
+    cp $FILE $BUNDLE/Contents/Frameworks/
+    install_name_tool -change $FILE $RELFWKS/$NAME $EMULATOR
+done
+
+# done!
