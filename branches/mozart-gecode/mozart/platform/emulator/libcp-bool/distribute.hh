4740461808cd012f9a6854ad7b3e926c2c9d6e39
ggutierrez <ggutierrez@ada56829-ad1f-0410-b00f-83cda6628aec>
2008-10-10 14:40:50 +0000

    I forgot to add the files in the preivous commit
    git-svn-id: https://gforge.info.ucl.ac.be/svn/mozart@17154 ada56829-ad1f-0410-b00f-83cda6628aec

---
+++ branches/mozart-gecode/mozart/platform/emulator/libcp-bool/distribute.hh
@@ -0,0 +1,31 @@
+/*
+ *  Main authors:
+ *     Andres Felipe Barco <anfelbar@univalle.edu.co>
+ *
+ *  Contributing authors:
+ *
+ *  Copyright:
+ *    Andres Barco, 2008
+ *
+ *  Last change:
+ *    $Date$ by $Author$
+ *    $Revision$
+ * 
+ *  This file is part of Mozart, an implementation 
+ *  of Oz 3:
+ *     http://www.mozart-oz.org
+ * 
+ *  See the file "LICENSE" or
+ *     http://www.mozart-oz.org/LICENSE.html
+ *  for information on usage and redistribution 
+ *  of this file, and for a DISCLAIMER OF ALL 
+ *  WARRANTIES.
+ *
+ */
+
+#ifndef __GECODE_BOOl_VAR_DIST_HH__
+#define __GECODE_BOOL_VAR_DIST_HH__
+
+//void gfd_dist_init(void);
+
+#endif
