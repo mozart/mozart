c6ea2f7bc981f278ec4628417abd3154ce41427f
ggutierrez <ggutierrez@ada56829-ad1f-0410-b00f-83cda6628aec>
2009-05-27 08:12:43 +0000

    Using Views instead of variable pointer when possible. This simplifies a lot the implementation. Space stability should be explicitly affected from propagators. (Change for boolean variables)
    git-svn-id: https://gforge.info.ucl.ac.be/svn/mozart@17221 ada56829-ad1f-0410-b00f-83cda6628aec

--- mozart/platform/emulator/libcp-bool/GeBoolVar.cc
+++ mozart/platform/emulator/libcp-bool/GeBoolVar.cc
@@ -51,7 +51,8 @@ OZ_Term GeBoolVar::statusV() {
 VarImpBase* GeBoolVar::clone(void) {
   GenericSpace* gs = getGSpace(); //extVar2Var(this)->getBoardInternal()->getGenericSpace(true);
   Assert(gs);
-  BoolVar *v = getBoolVarInfoPtr();
+  BoolView bv(static_cast<BoolVarImp*>(gs->getVar(index)));
+  BoolVar *v = new BoolVar(bv);
   BoolVar x;
   x.update(gs,false,*v);
   delete v;
@@ -63,25 +64,17 @@ VarImpBase* GeBoolVar::clone(void) {
 //x is the local variable,  the one that its domain is modified
 inline
 bool GeBoolVar::intersect(TaggedRef x) {
-  BoolVar *gv = getBoolVarInfoPtr();
-  ViewRanges<BoolView> gvr(*gv);
-  delete gv;
+  ViewRanges<BoolView> gvr(getBoolView());
   
-  BoolVar *liv = get_BoolVarInfoPtr(x);
-  BoolView vw(*liv);
-  delete liv;
   //Ask Alejandro about the use of getGSpace() instead of oz_currentBoard()
-  return (vw.inter_r(oz_currentBoard()->getGenericSpace(),gvr)==ME_GEN_FAILED ? false: true);
+  return (get_BoolView(x).inter_r(oz_currentBoard()->getGenericSpace(),gvr)==ME_GEN_FAILED ? false: true);
 }
 
 //(this) is the global variable
 //lx is the local value
 inline
 bool GeBoolVar::In(TaggedRef lx) {
-  BoolVar *gv = getBoolVarInfoPtr();
-  BoolView vw(*gv);
-  delete gv;
-  return vw.in(oz_intToC(lx));
+  return getBoolView().in(oz_intToC(lx));
 }
 
 TaggedRef GeBoolVar::clone(TaggedRef v) {
@@ -97,11 +90,8 @@ inline
 bool GeBoolVar::hasSameDomain(TaggedRef v) {
   
   Assert(OZ_isGeBoolVar(v));
-  BoolVar *v1 = get_BoolVarInfoPtr(v);
-  BoolVar *v2 = getBoolVarInfoPtr();
-  ViewRanges< BoolView > vr1 (*v1);
-  ViewRanges< BoolView > vr2 (*v2);
-  delete v1, v2;
+  ViewRanges< BoolView > vr1 (get_BoolView(v));
+  ViewRanges< BoolView > vr2 (getBoolView());
   
   while(true) {
     if(!vr1() && !vr2()) return true;
@@ -121,11 +111,8 @@ TaggedRef GeBoolVar::newVar(void) {
 inline
 bool GeBoolVar::IsEmptyInter(TaggedRef* var1,  TaggedRef* var2) {
   
-  BoolVar& v1 = get_BoolVarInfo(*var1);
-  BoolVar& v2 = get_BoolVarInfo(*var2);
-  
-  ViewRanges<BoolView > vr1 (v1);
-  ViewRanges<BoolView > vr2 (v2);
+  ViewRanges<BoolView > vr1 (get_BoolView(*var1));
+  ViewRanges<BoolView > vr2 (get_BoolView(*var2));
   
   while(true) {
     if(!vr1() || !vr2() ) return true;
@@ -140,11 +127,9 @@ bool GeBoolVar::IsEmptyInter(TaggedRef* var1,  TaggedRef* var2) {
 }
 
 void GeBoolVar::toStream(ostream &out) {
-  BoolVar *bv = getBoolVarInfoPtr();
   std::stringstream oss;
-  oss << *bv;
+  oss << getBoolView();
   out << "<GeBoolVar " << oss.str().c_str() << ">"; 
-  delete bv;
 }
   
 
