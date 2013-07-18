82492d779a7846aa99c80a91a1c26eff971e3395
Mon ouïe <mon.ouie@gmail.com>
2013-07-18 21:06:05 +0200

    Tcl_GetStringResult should be used instead of interp->result
    Direct access to interp->result has been deprecated and since then
    removed (it can be re-enabled by defining USE_INTERP_RESULT, but documentation
    recommends against it).

--- platform/wish/unixMain.cc
+++ platform/wish/unixMain.cc
@@ -1,4 +1,4 @@
-/* 
+/*
  * main.c --
  *
  *	This file contains the main program for "ozwish", a windowing
@@ -21,7 +21,7 @@
  * software and its documentation for any purpose, provided that the
  * above copyright notice and the following two paragraphs appear in
  * all copies of this software.
- * 
+ *
  * IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY FOR
  * DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING OUT
  * OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF THE UNIVERSITY OF
@@ -143,7 +143,8 @@ static void		StdinProc _ANSI_ARGS_((ClientData clientData,
 int
 main(int argc, char **argv)
 {
-    char *args, *p, *msg;
+    char *args, *p;
+    const char *msg;
     char buf[20];
     int code;
 
@@ -157,7 +158,7 @@ main(int argc, char **argv)
 
     if (Tk_ParseArgv(interp, (Tk_Window) NULL, &argc, CONST_CAST(CONST_FOR_ARGV char**,argv), argTable, 0)
 	    != TCL_OK) {
-	fprintf(stdout, "w %s\n.\n", interp->result);
+	fprintf(stdout, "w %s\n.\n", Tcl_GetStringResult(interp));
 	fflush(stdout); /* added mm */
 	exit(1);
     }
@@ -192,7 +193,7 @@ main(int argc, char **argv)
 #ifdef RS
     mainWindow = Tk_CreateMainWindow(interp, display, name, "Tk");
     if (mainWindow == NULL) {
-	fprintf(stdout, "w %s\n.\n", interp->result);
+	fprintf(stdout, "w %s\n.\n", Tcl_GetStringResult(interp));
 	fprintf(stdout, "s stop\n");
 	fflush(stdout); /* added mm */
 	exit(1);
@@ -232,7 +233,7 @@ main(int argc, char **argv)
      */
 
     if (Tcl_AppInit(interp) != TCL_OK) {
-	fprintf(stdout, "w Tcl_AppInit failed: %s\n.\n", interp->result);
+	fprintf(stdout, "w Tcl_AppInit failed: %s\n.\n", Tcl_GetStringResult(interp));
 	fflush(stdout); /* added mm */
     }
 
@@ -243,7 +244,7 @@ main(int argc, char **argv)
     if (geometry != NULL) {
 	code = Tcl_VarEval(interp, "wm geometry . ", geometry, (char *) NULL);
 	if (code != TCL_OK) {
-	    fprintf(stdout, "w %s\n.\n", interp->result);
+	    fprintf(stdout, "w %s\n.\n", Tcl_GetStringResult(interp));
 	    fflush(stdout); /* added mm */
 	}
     }
@@ -251,7 +252,7 @@ main(int argc, char **argv)
     /* mm: do not show the main window */
     code = Tcl_Eval(interp, "wm withdraw . ");
     if (code != TCL_OK) {
-      fprintf(stdout, "w %s\n.\n", interp->result);
+      fprintf(stdout, "w %s\n.\n", Tcl_GetStringResult(interp));
       fflush(stdout); /* added mm */
     }
 
@@ -301,7 +302,7 @@ main(int argc, char **argv)
 error:
     msg = CONST_CAST(char*,Tcl_GetVar(interp, CONST_CAST(CONST char*,"errorInfo"), TCL_GLOBAL_ONLY));
     if (msg == NULL) {
-	msg = interp->result;
+	msg = Tcl_GetStringResult(interp);
     }
     fprintf(stdout, "w %s\n.\n", msg);
     fflush(stdout);  /* added mm */
@@ -380,10 +381,10 @@ StdinProc(ClientData clientData, int mask)
     Tk_CreateFileHandler(0, 0, StdinProc, (ClientData) 0);
     code = Tcl_Eval(interp, cmd);
     Tk_CreateFileHandler(0, TK_READABLE, StdinProc, (ClientData) 0);
-    if (*interp->result != 0) {
+    if (*Tcl_GetStringResult(interp) != 0) {
 	if ((code != TCL_OK) || (tty)) {
 	  fprintf(stdout,"w --- %s", cmd);
-	  fprintf(stdout,"---  %s\n---\n.\n", interp->result);
+	  fprintf(stdout,"---  %s\n---\n.\n", Tcl_GetStringResult(interp));
 	  fflush(stdout); /* by mm */
 	}
     }
@@ -432,7 +433,7 @@ Prompt(Tcl_Interp *interp, int partial)
 	if (code != TCL_OK) {
 	    Tcl_AddErrorInfo(interp,
 		    "\n    (script that generates prompt)");
-	    fprintf(stdout, "w %s\n.\n", interp->result);
+	    fprintf(stdout, "w %s\n.\n", Tcl_GetStringResult(interp));
 	    fflush(stdout); /* added mm */
 	    goto defaultPrompt;
 	}
