b346cabb1cf7e44fcb2f6051e22dfc0871c3bead
Chris Double <chris.double@double.co.nz>
2018-09-14 18:14:43 +1200

    Avoid compiler warning about casting non-const string

--- platform/emulator/foreign.cc
+++ platform/emulator/foreign.cc
@@ -1361,7 +1361,7 @@ char* OZ_vsToC(OZ_Term t,int*n)
 {
   char * s;
   if (OZ_isNil(t)) {
-    static char *null = "";
+    static char *null = strdup("");
     if (n!=0) *n = 0;
     return null;
   }
